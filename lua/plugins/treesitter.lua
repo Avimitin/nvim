d944fb74d2c802cdc34eb925c8f22e7ab288ebdf
Avimitin <avimitin@gmail.com>
2021-07-20 17:16:52 +0800

    refactor: using tree file sturcture to manage plugins
    Former-commit-id: 077687aef512136406ffc3cfa10b70fe48160f98 [formerly 077687aef512136406ffc3cfa10b70fe48160f98 [formerly ea7641296e2d5e65f0fa3ded132053e45b0bc87c]]
    Former-commit-id: e0da4480b9ed72cab2a8968001b544fd835ced31
    Former-commit-id: 3dc1d522ff2306bb4c3846f37cba1c48ecdced67

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+require'nvim-treesitter.configs'.setup {
+	ensure_installed = {
+		"cpp",
+		"toml",
+		"rust",
+		"go",
+		"json",
+		"lua",
+		"comment",
+	},
+	highlight = {
+		enable = true,
+	}
+}
