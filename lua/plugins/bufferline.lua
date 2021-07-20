f4962586840a6fa2ac61467695c97a7cfba3d795
Avimitin <avimitin@gmail.com>
2021-07-20 22:52:13 +0800

    feat: added bufferline support
    Former-commit-id: a51d7d29430f659c61fd9cbd6a7b9d186ba8e4fe [formerly a51d7d29430f659c61fd9cbd6a7b9d186ba8e4fe [formerly 14be1b81b97046702860539c395f3114cc80f3d9]]
    Former-commit-id: 1011a6362ff24ec4a451cc706209ffcb95371892
    Former-commit-id: 4c98bdc5f21b716e6a380adc5954774ffc66225d

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,33 @@
+local diagnostic_function = function(count, level, diagnostics_dict, context)
+  local s = " "
+  for e, n in pairs(diagnostics_dict) do
+    local sym = e == "error" and " "
+      or (e == "warning" and " " or "" )
+    s = s .. n .. sym
+  end
+  return s
+end
+
+require('bufferline').setup {
+    options = {
+        offsets = {{filetype = "NvimTree", text = " Explorer", padding = 1}},
+        buffer_close_icon = "",
+        modified_icon = "",
+        close_icon = "",
+        left_trunc_marker = "",
+        right_trunc_marker = "",
+        max_name_length = 14,
+        max_prefix_length = 13,
+        tab_size = 20,
+        diagnostic = "nvim_lsp",
+        diagnostics_indicator = diagnostic_function,
+        show_tab_indicators = true,
+        enforce_regular_tabs = false,
+        view = "multiwindow",
+        show_buffer_close_icons = true,
+        separator_style = "thin",
+        mappings = true,
+        always_show_bufferline = true
+    },
+}
+
