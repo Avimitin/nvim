c8d91bc6f90e351e953755f5c5f960318bbfb04a
avimitin <avimitin@gmail.com>
2021-07-20 22:52:28 +0800

    feat: added lua implement autocomplete support
    Former-commit-id: 980c14684c94d077055414e53135a62337baeb1b
    Former-commit-id: 858054157217c9019a8b530932e5c476897f1baa

---
+++ lua/plugins/compe.lua
@@ -0,0 +1,69 @@
+local present, compe = pcall(require, "compe")
+if not present then
+    return
+end
+
+vim.o.completeopt = 'menuone,noinsert'
+
+compe.setup {
+    enabled = true,
+    autocomplete = true,
+    debug = false,
+    min_length = 1,
+    preselect = "enable",
+    throttle_time = 80,
+    source_timeout = 200,
+    incomplete_delay = 400,
+    max_abbr_width = 100,
+    max_kind_width = 100,
+    max_menu_width = 100,
+    documentation = true,
+    source = {
+        buffer = {kind = "﬘", true},
+        luasnip = {kind = "﬌", true},
+        nvim_lsp = true,
+        nvim_lua = true
+    }
+}
+
+-- Utility functions for compe and luasnip
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+  local col = vim.fn.col '.' - 1
+  if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
+    return true
+  else
+    return false
+  end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menu
+--- jump to prev/next snippet's placeholder
+local luasnip = require 'luasnip'
+
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-n>'
+  elseif luasnip.expand_or_jumpable() then
+    return t '<Plug>luasnip-expand-or-jump'
+  elseif check_back_space() then
+    return t '<Tab>'
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-p>'
+  elseif luasnip.jumpable(-1) then
+    return t '<Plug>luasnip-jump-prev'
+  else
+    return t '<S-Tab>'
+  end
+end
+
