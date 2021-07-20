0f80c9ecfae44d9a34a330de8f934990f69011a0
Avimitin <avimitin@gmail.com>
2021-07-20 23:41:19 +0800

    fix: fixed bad completion
    Former-commit-id: af33dc5d0cce1282186a23bed90631a850640136 [formerly af33dc5d0cce1282186a23bed90631a850640136 [formerly 749538ddd4725ed8ac4252b1321d38964ad4a0fc]]
    Former-commit-id: 3c85cc5a06cad1e96ad9398e5c7f9fb1ae57583a
    Former-commit-id: 8050bd5143cf915e67559c97ee9980072eb356ba

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -34,6 +34,47 @@ pcall(require, "plugins.lsp")
 
 local compe_stat = pcall(require, "plugins.compe")
 if compe_stat then
+  -- Utility functions for compe and luasnip
+  local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+  end
+
+  local check_back_space = function()
+    local col = vim.fn.col '.' - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
+      return true
+    else
+      return false
+    end
+  end
+
+  -- Use (s-)tab to:
+  --- move to prev/next item in completion menu
+  --- jump to prev/next snippet's placeholder
+  local snip_stat, luasnip = pcall(require, 'luasnip')
+
+  _G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+      return t '<C-n>'
+    elseif luasnip.expand_or_jumpable() and snip_stat then
+      return t '<Plug>luasnip-expand-or-jump'
+    elseif check_back_space() then
+      return t '<Tab>'
+    else
+      return vim.fn['compe#complete']()
+    end
+  end
+
+  _G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+      return t '<C-p>'
+    elseif luasnip.jumpable(-1) and snip_stat then
+      return t '<Plug>luasnip-jump-prev'
+    else
+      return t '<S-Tab>'
+    end
+  end
+
   -- Map tab to the above tab complete functions
   map('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
   map('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
