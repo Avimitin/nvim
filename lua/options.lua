80267707f9fa12b5a2bfd18a578ea2da9afa0620
Avimitin <avimitin@gmail.com>
2021-07-21 13:25:54 +0800

    refactor: using pure lua to execute options setting
    Former-commit-id: 4510def22a7943d644f1bf92a131c79a86d5ecf7 [formerly 4510def22a7943d644f1bf92a131c79a86d5ecf7 [formerly 46c86ce06e1aa6e8360189d4dffb28302a279dab]]
    Former-commit-id: 49b26c33ef1a6855e0c7ad9601e828137067bedd
    Former-commit-id: 3a5ce9cc5c763817d70466d2b5a40715fd9c7385

---
+++ lua/options.lua
@@ -0,0 +1,98 @@
+local opt = vim.opt
+
+opt.encoding='utf-8'
+
+-- auto change directory
+opt.autochdir=true
+
+-- enable number and relative line number
+opt.number=true
+opt.relativenumber=true
+
+-- line behind cursor
+opt.cursorline=true
+
+-- [[
+-- TAB SETTING
+-- Use a mix of tab and space to avoid mix up file
+-- ]]
+-- Use the appropriate number of spaces to insert a <Tab>.
+opt.expandtab=false
+-- Number of spaces that a <Tab> in the file counts for.
+opt.tabstop=2
+opt.shiftwidth=2
+opt.softtabstop=2
+
+-- Copy indent from current line when starting a new line
+opt.autoindent=true
+
+-- A List is an ordered sequence of items.
+opt.list=true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff=5
+
+-- Time in milliseconds to wait for a key code sequence to complete
+opt.ttimeoutlen=0
+-- no waiting for key combination
+opt.timeout=false
+
+-- remember where to recover cursor
+opt.viewoptions='cursor,folds,slash,unix'
+
+-- [[
+-- lines longer than the width of the window will wrap and displaying continues
+-- on the next line.
+-- ]]
+opt.wrap=true
+opt.tw=0
+opt.cindent=true
+opt.splitright=true
+opt.splitbelow=true
+opt.showmode=false
+opt.showcmd=true
+
+-- auto completion on command
+opt.wildmenu=true
+
+-- ignore case when searching and only on searching
+opt.ignorecase=true
+opt.smartcase=true
+
+vim.cmd("set shortmess+=c")
+opt.inccommand='split'
+opt.completeopt="longest,noinsert,menuone,noselect,preview"
+opt.ttyfast=true
+opt.lazyredraw=true
+opt.visualbell=true
+opt.updatetime=100
+opt.virtualedit='block'
+opt.colorcolumn='100'
+opt.lazyredraw=true
+opt.signcolumn="yes:1"
+opt.mouse='a'
+
+opt.foldmethod='indent'
+opt.foldlevel=99
+opt.foldenable=true
+opt.formatoptions='qj'
+
+opt.hidden=true
+
+-- Changed home directory here
+local backup_dir = HOME_DIR..".local/tmp/nvim/backup"
+local backup_stat = pcall(os.execute, "mkdir -p "..backup_dir)
+if backup_stat then
+  opt.backupdir=backup_dir
+  opt.directory=backup_dir
+end
+
+local undo_dir = HOME_DIR..".local/tmp/nvim/undo"
+local undo_stat = pcall(os.execute, "mkdir -p "..undo_dir)
+local has_persist = vim.fn.has("persistent_undo")
+if undo_stat and has_persist == 1 then
+  opt.undofile=true
+  opt.undodir=undo_dir
+end
+
+vim.api.nvim_command('autocmd TermOpen term://* startinsert')
