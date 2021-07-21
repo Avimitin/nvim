f6aed26fa6a29ba991c47b7710137460faccac1a
avimitin <avimitin@gmail.com>
2021-07-21 20:14:08 +0800

    refactor: added error handle
    Former-commit-id: 8d2059bea13df70d44073afa910f23636cb892df
    Former-commit-id: cf509cefc77d0db6618c9728ad9e5b36dac422bd

--- init.lua
+++ init.lua
@@ -34,5 +34,13 @@ vim.api.nvim_command 'filetype plugin indent on'
 
 require('options')
 require('mapping')
-pcall(require, 'plug-install')
-require('plug-init')
+
+local ok = pcall(require, 'plug-install')
+if not ok then
+	print("failed to install plugin")
+else
+	ok = pcall(require,'plug-init')
+	if not ok then
+		print("failed to initialize plugin configuration")
+	end
+end
