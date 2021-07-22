f22383c5f75c6efbbe1bc6904681dd78da79d355
avimitin <avimitin@gmail.com>
2021-07-22 17:28:14 +0800

    fix: fixed global value can't detect error
    Former-commit-id: 36530cb466484bf89eef664bc56cef6f7032d0cc
    Former-commit-id: 6eca14ef0b11efed8ba501354b0cea8e57fa6a29

--- init.lua
+++ init.lua
@@ -33,6 +33,7 @@ IN THE SOFTWARE.
 vim.api.nvim_command 'filetype plugin indent on'
 
 require('options')
+require('mapping')
 
 -- Copy from https://github.com/siduck76/NvChad
 local async
@@ -40,8 +41,6 @@ async =
 	vim.loop.new_async(
 	vim.schedule_wrap(
 		function()
-			require('mapping')
-
 			local ok, error = pcall(require, 'plug-install')
 			if not ok then
 				print("failed to install plugin")
