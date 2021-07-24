34aabb0c824916977a62e25de8e04d80a4f69496
Avimitin <avimitin@gmail.com>
2021-07-24 12:58:24 +0800

    refactor: changed nvimtree setting

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,6 +1,7 @@
-local present, tree_c = pcall(require, "nvim-tree.config")
-if not present then
-    return
+local ok, tree_c = pcall(require, "nvim-tree.config")
+if not ok then
+	print(tree_c)
+	return
 end
 
 local tree_cb = tree_c.nvim_tree_callback
@@ -8,22 +9,22 @@ local tree_cb = tree_c.nvim_tree_callback
 vim.g.nvim_tree_side = "left"
 vim.g.nvim_tree_width = 25
 vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
-vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_ignore_ft = {"dashboard"} -- don't open tree on specific fiypes.
+vim.g.nvim_tree_gitignore = 0
+vim.g.nvim_tree_auto_ignore_ft = {"startify","dashboard"} -- don't open tree on specific fiypes.
 vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 0 -- closes tree when it's the last window
+vim.g.nvim_tree_auto_close = 1 -- closes tree when it's the last window
 vim.g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_highlight_opened_files = 0
+vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_root_folder_modifier = ":t"
-vim.g.nvim_tree_tab_open = 0
+vim.g.nvim_tree_tab_open = 1
 vim.g.nvim_tree_allow_resize = 1
 vim.g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 vim.g.nvim_tree_disable_netrw = 1
-vim.g.nvim_tree_hijack_netrw = 0
+vim.g.nvim_tree_hijack_netrw = 1
 vim.g.nvim_tree_update_cwd = 1
 
 vim.g.nvim_tree_show_icons = {
