002846ef2948d76602c78f55ab14022de313aace
Avimitin <avimitin@gmail.com>
2021-07-25 17:46:35 +0800

    fix: fixed wrong statup plugin for dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,6 +3,8 @@ vim.g.dashboard_preview_pipeline = 'lolcat'
 vim.g.dashboard_preview_file = "~/.config/nvim/preview.cat"
 vim.g.dashboard_preview_file_height = 11
 vim.g.dashboard_preview_file_width = 50
+vim.g.dashboard_disable_statusline = 1
+vim.g.dashboard_default_executive = "telescope"
 
 vim.cmd[[
 autocmd FileType dashboard set showtabline=0 laststatus=0
