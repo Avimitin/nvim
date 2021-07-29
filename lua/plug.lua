8579026ec0b0b6dcfb3512b1845982f0be297001
avimitin <avimitin@gmail.com>
2021-07-29 17:59:12 +0800

    feat: added clang-format for cpp
    Former-commit-id: 636d908d090fc834a02416ab9dd99f6f2ebebdf2
    Former-commit-id: d32023acdac71664d4b952aa6ee7840fc5c7fe56

--- lua/plug.lua
+++ lua/plug.lua
@@ -255,6 +255,14 @@ return require('packer').startup(function(use)
 	use {
 		'sbdchd/neoformat',
 		cmd="Neoformat",
+		config=function ()
+			vim.g.neoformat_cpp_clangformat = {
+			 exe='clang-format',
+			 args={'-style=file'},
+		 }
+			vim.g.neoformat_enabled_cpp = {'clangformat'}
+			vim.g.neoformat_enabled_c = {'clangformat'}
+		end
 	}
 
 	-- show color at words
