f06c4e9e119de83e637d5b39f6a6fdcffecd720f
Avimitin <avimitin@gmail.com>
2021-08-30 23:58:24 +0800

    fix(gitsigns): Replaced deprecated settings
    Former-commit-id: 514ee02d570a217c78a9a4c3da2853ccef9af1e5 [formerly 514ee02d570a217c78a9a4c3da2853ccef9af1e5 [formerly e8f272602773b2fa7557c946271279c9829ff38b]]
    Former-commit-id: 75e8f9d5ea9822de2262f772ab014b763e560920
    Former-commit-id: 0555dfd18976b34d1f2b72acc2a0fa1377d22fdb

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -25,8 +25,11 @@ require('gitsigns').setup {
     follow_files = true
   },
   current_line_blame = false,
-  current_line_blame_delay = 1000,
-  current_line_blame_position = 'eol',
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+  },
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
