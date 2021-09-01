f9f4f1fdf90004095a9f44da71a29f1f0bf4812a
Avimitin <avimitin@gmail.com>
2021-09-01 17:15:02 +0800

    refactor(rust): Activated rust-tools after telescope
    Former-commit-id: 212d2bf6b85b810733beca592325b72a6f37e88c [formerly 212d2bf6b85b810733beca592325b72a6f37e88c [formerly 44171c9201f12ff0de451ef3b4922c839c931630]]
    Former-commit-id: 3095095fb5ca9b18460a88a7166aa8d8c4ebab1a
    Former-commit-id: fc246d81d3f3702993a6296270174d70a4d328ee

--- lua/plug.lua
+++ lua/plug.lua
@@ -320,7 +320,7 @@ return require('packer').startup(function(use)
 
 	use {
 		'simrat39/rust-tools.nvim',
-		after={"nvim-lspconfig"},
+		after={"nvim-lspconfig", "telescope.nvim"},
 		config=function()
 			require("plugins.rust")
 		end,
