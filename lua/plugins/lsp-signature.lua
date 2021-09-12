9e75464fe2f24d49d6cf9cd6ff0b9658501cba77
Avimitin <avimitin@gmail.com>
2021-09-12 22:18:20 +0800

    refactor(lsp-signature): Code format
    Former-commit-id: 375afe4bd30c69c62ec70edeadf99cc6ec00c723 [formerly 375afe4bd30c69c62ec70edeadf99cc6ec00c723 [formerly 8139c7e27960a93c4513303222d5a00ac040e177]]
    Former-commit-id: 79e13ca2759ccff749aa0da4ef562b8c0146fdd7
    Former-commit-id: 94ef6336717360fc2125e2b033c08f81d33d056a

--- lua/plugins/lsp-signature.lua
+++ lua/plugins/lsp-signature.lua
@@ -1,24 +1,22 @@
 -- Copy from: https://github.com/siduck76/NvChad/commit/d4193727bd474d4454fa8fdec6764bf2064aaf5b
 local present, lspsignature = pcall(require, "lsp_signature")
 if present then
-	lspsignature.setup(
-			{
-					bind = true,
-					doc_lines = 2,
-					floating_window = true,
-					fix_pos = true,
-					hint_enable = true,
-					hint_prefix = " ",
-					hint_scheme = "String",
-					use_lspsaga = false,
-					hi_parameter = "Search",
-					max_height = 22,
-					max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
-					handler_opts = {
-							border = "single" -- double, single, shadow, none
-					},
-					zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
-					padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
-			}
-	)
+    lspsignature.setup({
+        bind = true,
+        doc_lines = 2,
+        floating_window = true,
+        fix_pos = true,
+        hint_enable = true,
+        hint_prefix = " ",
+        hint_scheme = "String",
+        use_lspsaga = false,
+        hi_parameter = "Search",
+        max_height = 22,
+        max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+        handler_opts = {
+            border = "single" -- double, single, shadow, none
+        },
+        zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
+        padding = "" -- character to pad on left and right of signature can be ' ', or '|'  etc
+    })
 end
