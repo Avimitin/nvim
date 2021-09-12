1893546f57888909d36d6e92ab57434ea3da95fe
Avimitin <avimitin@gmail.com>
2021-09-12 22:22:38 +0800

    refactor: Code format
    Former-commit-id: 467c4d902c3375e711a960124e704af4708769b8 [formerly 467c4d902c3375e711a960124e704af4708769b8 [formerly aa99d5f027d9f8777e3596efba68a62fdb5b0f73]]
    Former-commit-id: fb507527298b5f00f347d1557b963c24d6a7a611
    Former-commit-id: 43aad3fefc297cf5b4b565e47ccaa4ec1ab6017d

--- lua/plugins/galaxyline.lua
+++ lua/plugins/galaxyline.lua
@@ -3,15 +3,8 @@ local gls = gl.section
 local diagnostic = require('galaxyline.provider_diagnostic')
 
 gl.short_line_list = {
-	'LuaTree',
-	'vista',
-	'dbui',
-	'startify',
-	'term',
-	'nerdtree',
-	'fugitive',
-	'fugitiveblame',
-	'plug'
+    'LuaTree', 'vista', 'dbui', 'startify', 'term', 'nerdtree', 'fugitive',
+    'fugitiveblame', 'plug'
 }
 
 -- VistaPlugin = extension.vista_nearest
@@ -30,214 +23,221 @@ local colors = {
     purple   = '#C678DD',
     magenta  = '#C858E9',
     blue     = '#73BA9F',
-    red      = '#D54E53',
+    red      = '#D54E53'
 }
 
 local function has_file_type()
     local f_type = vim.bo.filetype
-    if not f_type or f_type == '' then
-        return false
-    end
+    if not f_type or f_type == '' then return false end
     return true
 end
 
 local buffer_not_empty = function()
-  if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-    return true
-  end
-  return false
+    if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then return true end
+    return false
 end
 
 -- insert_left insert item at the left panel
-local function insert_left(element)
-  table.insert(gls.left, element)
-end
+local function insert_left(element) table.insert(gls.left, element) end
 
 -- insert_blank_line_at_left insert blank line with
 -- line_bg color.
 local function insert_blank_line_at_left()
-insert_left {
-  Space = {
-    provider = function () return ' ' end,
-    highlight = {colors.line_bg,colors.line_bg}
-  }
-}
+    insert_left {
+        Space = {
+            provider = function() return ' ' end,
+            highlight = {colors.line_bg, colors.line_bg}
+        }
+    }
 end
 
 -- insert_right insert given item into galaxyline.right
-local function insert_right(element)
-  table.insert(gls.right, element)
-end
+local function insert_right(element) table.insert(gls.right, element) end
 
 -- insert_blank_line_at_left insert blank line with
 -- line_bg color.
 local function insert_blank_line_at_right()
-insert_right {
-  Space = {
-    provider = function () return ' ' end,
-    highlight = {colors.line_bg,colors.line_bg}
-  }
-}
+    insert_right {
+        Space = {
+            provider = function() return ' ' end,
+            highlight = {colors.line_bg, colors.line_bg}
+        }
+    }
 end
 
 -----------------------------------------------------
 ----------------- start insert ----------------------
 -----------------------------------------------------
 
---{ mode panel start
-insert_left{
-  Start = {
-    provider = function() return ' ' end,
-    highlight = {colors.line_bg,}
-  }
+-- { mode panel start
+insert_left {
+    Start = {
+        provider = function() return ' ' end,
+        highlight = {colors.line_bg}
+    }
 }
 
 insert_blank_line_at_left()
 
-insert_left{
-  ViMode = {
-    icon = function()
-        local icons = {
-            n      = ' ',
-            i      = ' ',
-            c      = 'ﲵ ',
-            V      = ' ',
-            [''] = ' ',
-            v      = ' ',
-            C      = 'ﲵ ',
-            R      = '﯒ ',
-            t      = ' ',
-        }
-        return icons[vim.fn.mode()]
-      end,
-    provider = function()
-      -- auto change color according the vim mode
-      local alias = {
-          n      = 'N',
-          i      = 'I',
-          c      = 'C',
-          V      = 'VL',
-          [''] = 'V',
-          v      = 'V',
-          C      = 'C',
-          ['r?'] = ':CONFIRM',
-          rm     = '--MORE',
-          R      = 'R',
-          Rv     = 'R&V',
-          s      = 'S',
-          S      = 'S',
-          ['r']  = 'HIT-ENTER',
-          [''] = 'SELECT',
-          t      = 'T',
-          ['!']  = 'SH',
-      }
+insert_left {
+    ViMode = {
+        icon = function()
+            local icons = {
+                n = ' ',
+                i = ' ',
+                c = 'ﲵ ',
+                V = ' ',
+                [''] = ' ',
+                v = ' ',
+                C = 'ﲵ ',
+                R = '﯒ ',
+                t = ' '
+            }
+            return icons[vim.fn.mode()]
+        end,
+        provider = function()
+            -- auto change color according the vim mode
+            local alias = {
+                n = 'N',
+                i = 'I',
+                c = 'C',
+                V = 'VL',
+                [''] = 'V',
+                v = 'V',
+                C = 'C',
+                ['r?'] = ':CONFIRM',
+                rm = '--MORE',
+                R = 'R',
+                Rv = 'R&V',
+                s = 'S',
+                S = 'S',
+                ['r'] = 'HIT-ENTER',
+                [''] = 'SELECT',
+                t = 'T',
+                ['!'] = 'SH'
+            }
 
-      local mode_color = {
-				n = colors.yellow,      i = colors.green,   v=colors.blue,
-        [''] = colors.blue,   V=colors.blue,      c = colors.magenta,
-				no = colors.red,        s = colors.orange,  S=colors.orange,
-				[''] = colors.orange, ic = colors.yellow, R = colors.purple,
-				Rv = colors.purple,     cv = colors.red,    ce=colors.red,
-				r = colors.cyan,        rm = colors.cyan,   ['r?'] = colors.cyan,
-        ['!'] = colors.red,     t = colors.red
-			}
+            local mode_color = {
+                n = colors.yellow,
+                i = colors.green,
+                v = colors.blue,
+                [''] = colors.blue,
+                V = colors.blue,
+                c = colors.magenta,
+                no = colors.red,
+                s = colors.orange,
+                S = colors.orange,
+                [''] = colors.orange,
+                ic = colors.yellow,
+                R = colors.purple,
+                Rv = colors.purple,
+                cv = colors.red,
+                ce = colors.red,
+                r = colors.cyan,
+                rm = colors.cyan,
+                ['r?'] = colors.cyan,
+                ['!'] = colors.red,
+                t = colors.red
+            }
 
-      local vim_mode = vim.fn.mode()
-      vim.api.nvim_command('hi GalaxyViMode guifg='..mode_color[vim_mode])
-      return alias[vim_mode]
-    end,
-    highlight = {colors.line_bg, colors.line_bg},
-  },
+            local vim_mode = vim.fn.mode()
+            vim.api.nvim_command('hi GalaxyViMode guifg=' ..
+                                     mode_color[vim_mode])
+            return alias[vim_mode]
+        end,
+        highlight = {colors.line_bg, colors.line_bg}
+    }
 }
 
 insert_blank_line_at_left()
 
-insert_left{
-  Separa = {
-    provider = function() return ' ' end,
-    highlight = {colors.line_bg, },
-  }
+insert_left {
+    Separa = {
+        provider = function() return ' ' end,
+        highlight = {colors.line_bg}
+    }
 }
 
---mode panel end}
+-- mode panel end}
 
 -- {information panel start
-insert_left{
-  Start = {
-    provider = function() return ' ' end,
-    highlight = {colors.line_bg,}
-  }
+insert_left {
+    Start = {
+        provider = function() return ' ' end,
+        highlight = {colors.line_bg}
+    }
 }
 
-insert_left{
-  FileIcon = {
-    provider = 'FileIcon',
-    condition = buffer_not_empty,
-    highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,colors.line_bg},
-  },
+insert_left {
+    FileIcon = {
+        provider = 'FileIcon',
+        condition = buffer_not_empty,
+        highlight = {
+            require('galaxyline.provider_fileinfo').get_file_icon_color,
+            colors.line_bg
+        }
+    }
 }
 
-insert_left{
-  BufferType = {
-    provider = 'FileTypeName',
-    condition = has_file_type,
-    highlight = {colors.fg,colors.line_bg}
-  }
+insert_left {
+    BufferType = {
+        provider = 'FileTypeName',
+        condition = has_file_type,
+        highlight = {colors.fg, colors.line_bg}
+    }
 }
 
 insert_blank_line_at_left()
 
 insert_left {
-  GitIcon = {
-    provider = function() return '  ' end,
-    condition = require('galaxyline.provider_vcs').check_git_workspace,
-    highlight = {colors.orange,colors.line_bg},
-  }
+    GitIcon = {
+        provider = function() return '  ' end,
+        condition = require('galaxyline.provider_vcs').check_git_workspace,
+        highlight = {colors.orange, colors.line_bg}
+    }
 }
 
 insert_left {
-  GitBranch = {
-    provider = 'GitBranch',
-    condition = require('galaxyline.provider_vcs').check_git_workspace,
-    highlight = {'#8FBCBB',colors.line_bg},
-  }
+    GitBranch = {
+        provider = 'GitBranch',
+        condition = require('galaxyline.provider_vcs').check_git_workspace,
+        highlight = {'#8FBCBB', colors.line_bg}
+    }
 }
 
 insert_blank_line_at_left()
 
 local checkwidth = function()
-  local squeeze_width  = vim.fn.winwidth(0) / 2
-  if squeeze_width > 40 then
-    return true
-  end
-  return false
+    local squeeze_width = vim.fn.winwidth(0) / 2
+    if squeeze_width > 40 then return true end
+    return false
 end
 
 insert_left {
-  DiffAdd = {
-    provider = 'DiffAdd',
-    condition = checkwidth,
-    icon = '  ',
-    highlight = {colors.green,colors.line_bg},
-  }
+    DiffAdd = {
+        provider = 'DiffAdd',
+        condition = checkwidth,
+        icon = '  ',
+        highlight = {colors.green, colors.line_bg}
+    }
 }
 
 insert_left {
-  DiffModified = {
-    provider = 'DiffModified',
-    condition = checkwidth,
-    icon = '  ',
-    highlight = {colors.orange,colors.line_bg},
-  }
+    DiffModified = {
+        provider = 'DiffModified',
+        condition = checkwidth,
+        icon = '  ',
+        highlight = {colors.orange, colors.line_bg}
+    }
 }
 
 insert_left {
-  DiffRemove = {
-    provider = 'DiffRemove',
-    condition = checkwidth,
-    icon = '  ',
-    highlight = {colors.red,colors.line_bg},
-  }
+    DiffRemove = {
+        provider = 'DiffRemove',
+        condition = checkwidth,
+        icon = '  ',
+        highlight = {colors.red, colors.line_bg}
+    }
 }
 
 DiagnosticError = diagnostic.get_diagnostic_error
@@ -246,101 +246,101 @@ DiagnosticHint = diagnostic.get_diagnostic_hint
 DiagnosticInfo = diagnostic.get_diagnostic_info
 
 insert_left {
-  DiagnosticError = {
-    provider = 'DiagnosticError',
-    icon = '  ',
-    highlight = {colors.red,colors.line_bg}
-  }
+    DiagnosticError = {
+        provider = 'DiagnosticError',
+        icon = '  ',
+        highlight = {colors.red, colors.line_bg}
+    }
 }
 
 insert_left {
-  DiagnosticWarn = {
-    provider = 'DiagnosticWarn',
-		condition = checkwidth,
-    icon = '  ',
-    highlight = {colors.yellow,colors.line_bg},
-  }
+    DiagnosticWarn = {
+        provider = 'DiagnosticWarn',
+        condition = checkwidth,
+        icon = '  ',
+        highlight = {colors.yellow, colors.line_bg}
+    }
 }
 
 insert_left {
-	DiagnosticInfo = {
-	 provider = DiagnosticInfo,
-	 condition = checkwidth,
-	 highlight = {colors.green,colors.line_bg},
-	 icon = '  ',
-	}
+    DiagnosticInfo = {
+        provider = DiagnosticInfo,
+        condition = checkwidth,
+        highlight = {colors.green, colors.line_bg},
+        icon = '  '
+    }
 }
 
 insert_left {
-	DiagnosticHint = {
-	 provider = DiagnosticHint,
-	 condition = checkwidth,
-	 highlight = {colors.white,colors.line_bg},
-	 icon = '  ',
-	}
+    DiagnosticHint = {
+        provider = DiagnosticHint,
+        condition = checkwidth,
+        highlight = {colors.white, colors.line_bg},
+        icon = '  '
+    }
 }
 
-insert_left{
-  Separa = {
-    provider = function() return ' ' end,
-    highlight = {colors.line_bg, },
-  }
+insert_left {
+    Separa = {
+        provider = function() return ' ' end,
+        highlight = {colors.line_bg}
+    }
 }
 -- left information panel end}
 
-insert_right{
-  Start = {
-    provider = function() return ' ' end,
-    highlight = {colors.line_bg,}
-  }
+insert_right {
+    Start = {
+        provider = function() return ' ' end,
+        highlight = {colors.line_bg}
+    }
 }
 
 insert_blank_line_at_right()
 
-insert_right{
-  FileFormat = {
-    provider = 'FileFormat',
-		condition=checkwidth,
-    highlight = {colors.fg,colors.line_bg,'bold'},
-  }
+insert_right {
+    FileFormat = {
+        provider = 'FileFormat',
+        condition = checkwidth,
+        highlight = {colors.fg, colors.line_bg, 'bold'}
+    }
 }
 
 insert_blank_line_at_right()
 
-insert_right{
-  LineInfo = {
-    provider = 'LineColumn',
-    separator = ' ',
-		condition=checkwidth,
-    separator_highlight = {colors.green, colors.line_bg},
-    highlight = {colors.fg,colors.line_bg},
-  },
+insert_right {
+    LineInfo = {
+        provider = 'LineColumn',
+        separator = ' ',
+        condition = checkwidth,
+        separator_highlight = {colors.green, colors.line_bg},
+        highlight = {colors.fg, colors.line_bg}
+    }
 }
 
-insert_right{
-  PerCent = {
-    provider = 'LinePercent',
-    separator = '',
-    separator_highlight = {colors.blue,colors.line_bg},
-    highlight = {colors.cyan, colors.line_bg,'bold'},
-  }
+insert_right {
+    PerCent = {
+        provider = 'LinePercent',
+        separator = '',
+        separator_highlight = {colors.blue, colors.line_bg},
+        highlight = {colors.cyan, colors.line_bg, 'bold'}
+    }
 }
 
-insert_right{
-  Encode = {
-    provider = 'FileEncode',
-    separator = '',
-		condition=checkwidth,
-    separator_highlight = {colors.blue,colors.line_bg},
-    highlight = {colors.cyan, colors.line_bg,'bold'},
-  }
+insert_right {
+    Encode = {
+        provider = 'FileEncode',
+        separator = '',
+        condition = checkwidth,
+        separator_highlight = {colors.blue, colors.line_bg},
+        highlight = {colors.cyan, colors.line_bg, 'bold'}
+    }
 }
 
 insert_blank_line_at_right()
 
-insert_right{
-  Separa = {
-    provider = function() return ' ' end,
-    highlight = {colors.line_bg, },
-  }
+insert_right {
+    Separa = {
+        provider = function() return ' ' end,
+        highlight = {colors.line_bg}
+    }
 }
--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,14 +1,10 @@
---use fterm to open the lazygit
+-- use fterm to open the lazygit
 local term = require("FTerm.terminal")
 local lazygit = term:new():setup({
     cmd = "lazygit",
-    dimensions = {
-        height = 0.9,
-        width = 0.9
-    }
+    dimensions = {height = 0.9, width = 0.9}
 })
-function _G.fterm_lazygit()
-    lazygit:toggle()
-end
+function _G.fterm_lazygit() lazygit:toggle() end
 
-vim.api.nvim_set_keymap('n', '<C-g>', ':lua _G.fterm_lazygit()<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('n', '<C-g>', ':lua _G.fterm_lazygit()<CR>',
+                        {noremap = true, silent = true})
