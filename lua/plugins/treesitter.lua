70a6cebfd1edce0f3d682c5b5e07328631fa9330
Avimitin <avimitin@gmail.com>
2021-09-12 22:24:12 +0800

    refactor: Code format
    Former-commit-id: 29bfd40ee51965fd564d977e584000f41b4cc3aa [formerly 29bfd40ee51965fd564d977e584000f41b4cc3aa [formerly ce8b8460621b696b4c860baadce8383363d26b13]]
    Former-commit-id: e42e38d81f970ac68def1e48e43289c922b9a334
    Former-commit-id: fc818e22fcca9f565c493c2dfa40259e00c46a5f

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,4 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = {
-		"cpp",
-		"toml",
-		"rust",
-		"go",
-		"json",
-		"lua",
-		"comment",
-	},
-	highlight = {
-		enable = true,
-	}
+    ensure_installed = {"cpp", "toml", "rust", "go", "json", "lua", "comment"},
+    highlight = {enable = true}
 }
