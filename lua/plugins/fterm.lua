aa99d5f027d9f8777e3596efba68a62fdb5b0f73
Avimitin <avimitin@gmail.com>
2021-09-12 22:22:38 +0800

    refactor: Code format

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,17 +1,17 @@
 if not pcall(require, 'FTerm') then
-	print("Failed to call FTerm")
-	return
+    print("Failed to call FTerm")
+    return
 end
 
 require('FTerm').setup {
-	dimensions = {
-		height = 0.9,
-		width = 0.9,
-	},
+    dimensions = {height = 0.9, width = 0.9},
 
-	border = 'double'
+    border = 'double'
 }
 
-local option = {noremap=true, silent=true}
-vim.api.nvim_set_keymap("n", "<C-\\>", [[<CMD>lua require("FTerm").toggle()<CR>]], option)
-vim.api.nvim_set_keymap("t", "<C-\\>", [[<C-\><C-n><CMD>lua require("FTerm").toggle()<CR>]], option)
+local option = {noremap = true, silent = true}
+vim.api.nvim_set_keymap("n", "<C-\\>",
+                        [[<CMD>lua require("FTerm").toggle()<CR>]], option)
+vim.api.nvim_set_keymap("t", "<C-\\>",
+                        [[<C-\><C-n><CMD>lua require("FTerm").toggle()<CR>]],
+                        option)
