31d1d41d4c576b4998cecdf077773edd59f361c4
Avimitin <avimitin@gmail.com>
2021-09-26 19:57:46 +0800

    refactor(hexokinase): Load hexokinase at startup

--- lua/plug.lua
+++ lua/plug.lua
@@ -235,7 +235,6 @@ return require('packer').startup(function(use)
     use {
         'RRethy/vim-hexokinase',
         run = 'make',
-        cmd = "Hexokinase Toggle",
         config = function()
             vim.g.Hexokinase_highlighters = {'backgroundfull'}
             vim.g.Hexokinase_optInPatterns = {
