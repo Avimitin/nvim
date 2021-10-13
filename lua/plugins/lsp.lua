9a3c988dbcd1a44638512720201acda6fd4e57cc
Avimitin <avimitin@gmail.com>
2021-10-13 08:59:21 +0800

    lsp: append lsp handlers for text/documentation
    This settings are copied from the repository NvChad. Also, I've update
    the virtual text indicator icon and do some code format.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,20 +57,21 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
+capabilities.textDocument.completion.completionItem.documentationFormat = {
+    "markdown", "plaintext"
+}
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.preselectSupport = true
 capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
 capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
 capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.commitCharactersSupport =
+    true
+capabilities.textDocument.completion.completionItem.tagSupport = {
+    valueSet = {1}
+}
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-   properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
-   },
+    properties = {"documentation", "detail", "additionalTextEdits"}
 }
 
 -- lspInstall + lspconfig stuff
@@ -129,9 +130,19 @@ vim.fn.sign_define("LspDiagnosticsSignInformation",
 vim.fn.sign_define("LspDiagnosticsSignHint",
                    {text = "", numhl = "LspDiagnosticsDefaultHint"})
 
+local lsp_publish_diagnostics_options = {
+    virtual_text = {prefix = "", spacing = 0},
+    signs = true,
+    underline = true,
+    update_in_insert = false -- update diagnostics insert mode
+}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = {prefix = "", spacing = 2},
-        signs = true,
-        underline = true
-    })
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
+                 lsp_publish_diagnostics_options)
+
+vim.lsp.handlers["textDocument/hover"] =
+    vim.lsp.with(vim.lsp.handlers.hover, {border = "single"})
+
+vim.lsp.handlers["textDocument/signatureHelp"] =
+    vim.lsp.with(vim.lsp.handlers.signature_help, {border = "single"})
