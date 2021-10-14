f95bc7407a2f66412cc755b80f30c988045e3cf2
Avimitin <avimitin@gmail.com>
2021-10-14 14:41:22 +0800

    format: format Lua code
    Run LuaFormatter for options.lua, mapping.lua, plug.lua.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,95 +1,91 @@
 function Map(mode, lhs, rhs, opts)
     local options = {noremap = true, silent = true}
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
+    if opts then options = vim.tbl_extend("force", options, opts) end
     local stat, error = pcall(vim.api.nvim_set_keymap, mode, lhs, rhs, options)
-		if not stat then
-			print(error)
-		end
+    if not stat then print(error) end
 end
 
 Map('n', 'J', '<C-D>')
 Map('n', 'K', '<C-U>')
-Map("",  "L",     "$")
-Map("",  "H",     "0")
-Map("",  "X",     "Vx")
-Map("",  "W",     "5w")
-Map("",  "B",     "5b")
-Map("",  "vw",    "viw")
-Map("",  "<C-z>", "u")
-Map("n", "<",     "<<")
-Map("n", ">",     ">>")
-Map("",  "s",     "<nop>")
-Map("",  "-",     "N")
-Map("",  "=",     "n")
-Map("n",  ";",     ":")
+Map("", "L", "$")
+Map("", "H", "0")
+Map("", "X", "Vx")
+Map("", "W", "5w")
+Map("", "B", "5b")
+Map("", "vw", "viw")
+Map("", "<C-z>", "u")
+Map("n", "<", "<<")
+Map("n", ">", ">>")
+Map("", "s", "<nop>")
+Map("", "-", "N")
+Map("", "=", "n")
+Map("n", ";", ":")
 
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 
-Map("n", ";w",        ":w<CR>")
+Map("n", ";w", ":w<CR>")
 
 Map("n", "<LEADER>q", ":wq<CR>")
 
-Map("n", "<C-A-q>",     ":qa<CR>")
+Map("n", "<C-A-q>", ":qa<CR>")
 
 Map("v", "<LEADER>y", [["+y]])
 
-Map("",  "<LEADER>p", [["+p]])
+Map("", "<LEADER>p", [["+p]])
 
-Map("n", "<ESC>",     ":nohlsearch<CR>")
+Map("n", "<ESC>", ":nohlsearch<CR>")
 
-Map("i", "jj",        "<ESC>")
+Map("i", "jj", "<ESC>")
 
-Map("n", "spv",       "<C-w>t<C-w>H")
+Map("n", "spv", "<C-w>t<C-w>H")
 
-Map("n", "srr",       "<C-w>b<C-w>K")
-Map("n", "srv",       "<C-w>b<C-w>H")
+Map("n", "srr", "<C-w>b<C-w>K")
+Map("n", "srv", "<C-w>b<C-w>H")
 
-Map("n", "<up>",      ":res +5<CR>")
-Map("n", "<down>",    ":res -5<CR>")
-Map("n", "<left>",    ":vertical resize-5<CR>")
-Map("n", "<right>",   ":vertical resize+5<CR>")
+Map("n", "<up>", ":res +5<CR>")
+Map("n", "<down>", ":res -5<CR>")
+Map("n", "<left>", ":vertical resize-5<CR>")
+Map("n", "<right>", ":vertical resize+5<CR>")
 
-Map("n", "sk",        "<C-w>k")
-Map("n", "sj",        "<C-w>j")
-Map("n", "sh",        "<C-w>h")
-Map("n", "sl",        "<C-w>l")
+Map("n", "sk", "<C-w>k")
+Map("n", "sj", "<C-w>j")
+Map("n", "sh", "<C-w>h")
+Map("n", "sl", "<C-w>l")
 
---center line
-Map("i", "<C-c>",     "<ESC>zzi")
+-- center line
+Map("i", "<C-c>", "<ESC>zzi")
 
---nnn
+-- nnn
 Map('n', '<Leader>o', ':NnnPicker %:p:h<CR>')
 
---hop
+-- hop
 Map('n', 'f', ':HopChar2<CR>')
 Map('v', 'f', ':HopChar2<CR>')
 Map('n', '<C-J>', ':HopLine<CR>')
 Map('n', '<C-K>', ':HopLine<CR>')
 
---telescope
+-- telescope
 Map('n', '<leader>ff', [[<cmd>Telescope find_files<cr>]])
 Map('n', '<leader>fp', [[<cmd>Telescope media_files<cr>]])
 Map('n', '<leader>fg', [[<cmd>Telescope live_grep<cr>]])
 Map('n', '<leader>fb', [[<cmd>Telescope buffers<cr>]])
 Map('n', '<leader>fh', [[<cmd>Telescope help_tags<cr>]])
 
---SymbolsOutline
+-- SymbolsOutline
 Map("n", "<leader>l", ":SymbolsOutline<CR>")
 
---EasyAlign
+-- EasyAlign
 Map("v", "<leader>e", ":EasyAlign<CR>")
 
---AnyJump
+-- AnyJump
 Map('n', '<leader>aj', ':AnyJump<CR>')
 Map('n', '<leader>ab', ':AnyJumpBack<CR>')
 
---vim-go
+-- vim-go
 Map('n', 'got', ':GoTestFunc<CR>')
 Map('n', 'gor', ':GoRun<CR>')
 
---nvim-tree
+-- nvim-tree
 Map("n", "tt", ":NvimTreeToggle<CR>")
 Map("n", "tr", ":NvimTreeRefresh<CR>")
 
--- lua/options.lua
+++ lua/options.lua
@@ -1,120 +1,120 @@
 local opt = vim.opt
 
-opt.encoding='utf-8'
+opt.encoding = 'utf-8'
 
 -- enable number and relative line number
-opt.number=true
+opt.number = true
 
 -- line behind cursor
-opt.cursorline=true
+opt.cursorline = true
 
 -- [[
 -- TAB SETTING
 -- Use a mix of tab and space to avoid mix up file
 -- ]]
 -- Use the appropriate number of spaces to insert a <Tab>.
-opt.expandtab=false
+opt.expandtab = false
 -- Number of spaces that a <Tab> in the file counts for.
-opt.tabstop=8
-opt.shiftwidth=2
-opt.softtabstop=2
+opt.tabstop = 8
+opt.shiftwidth = 2
+opt.softtabstop = 2
 
 -- Copy indent from current line when starting a new line
---opt.autoindent=true
+-- opt.autoindent=true
 
 -- A List is an ordered sequence of items.
-opt.list=true
-opt.listchars="tab:-->,trail:·"
+opt.list = true
+opt.listchars = "tab:-->,trail:·"
 
 -- Minimal number of screen lines to keep above and below the cursor.
-opt.scrolloff=5
+opt.scrolloff = 5
 
 -- Time in milliseconds to wait for a key code sequence to complete
-opt.ttimeoutlen=0
+opt.ttimeoutlen = 0
 -- no waiting for key combination
-opt.timeout=false
+opt.timeout = false
 
 -- remember where to recover cursor
-opt.viewoptions='cursor,folds,slash,unix'
+opt.viewoptions = 'cursor,folds,slash,unix'
 
 -- [[
 -- lines longer than the width of the window will wrap and displaying continues
 -- on the next line.
 -- ]]
-opt.wrap=true
-opt.tw=0
-opt.cindent=true
-opt.splitright=true
-opt.splitbelow=true
-opt.showmode=false
-opt.showcmd=true
+opt.wrap = true
+opt.tw = 0
+opt.cindent = true
+opt.splitright = true
+opt.splitbelow = true
+opt.showmode = false
+opt.showcmd = true
 
 -- auto completion on command
-opt.wildmenu=true
+opt.wildmenu = true
 
 -- ignore case when searching and only on searching
-opt.ignorecase=true
-opt.smartcase=true
+opt.ignorecase = true
+opt.smartcase = true
 
 vim.cmd("set shortmess+=cm")
 vim.cmd("set shortmess-=F")
-opt.inccommand='split'
-opt.completeopt={"menuone", "noselect", "menu"}
-opt.ttyfast=true
-opt.lazyredraw=true
-opt.visualbell=true
-opt.updatetime=100
-opt.virtualedit='block'
-opt.colorcolumn='100'
-opt.lazyredraw=true
-opt.signcolumn="yes:1"
-opt.mouse='a'
+opt.inccommand = 'split'
+opt.completeopt = {"menuone", "noselect", "menu"}
+opt.ttyfast = true
+opt.lazyredraw = true
+opt.visualbell = true
+opt.updatetime = 100
+opt.virtualedit = 'block'
+opt.colorcolumn = '100'
+opt.lazyredraw = true
+opt.signcolumn = "yes:1"
+opt.mouse = 'a'
 
-opt.foldmethod='indent'
-opt.foldlevel=99
-opt.foldenable=true
-opt.formatoptions='qj'
+opt.foldmethod = 'indent'
+opt.foldlevel = 99
+opt.foldenable = true
+opt.formatoptions = 'qj'
 
-opt.hidden=true
+opt.hidden = true
 
 -- Changed home directory here
-local backup_dir = vim.fn.stdpath("cache").."/backup"
-local backup_stat = pcall(os.execute, "mkdir -p "..backup_dir)
+local backup_dir = vim.fn.stdpath("cache") .. "/backup"
+local backup_stat = pcall(os.execute, "mkdir -p " .. backup_dir)
 if backup_stat then
-  opt.backupdir=backup_dir
-  opt.directory=backup_dir
+    opt.backupdir = backup_dir
+    opt.directory = backup_dir
 end
 
-local undo_dir = vim.fn.stdpath("cache").."/undo"
-local undo_stat = pcall(os.execute, "mkdir -p "..undo_dir)
+local undo_dir = vim.fn.stdpath("cache") .. "/undo"
+local undo_stat = pcall(os.execute, "mkdir -p " .. undo_dir)
 local has_persist = vim.fn.has("persistent_undo")
 if undo_stat and has_persist == 1 then
-  opt.undofile=true
-  opt.undodir=undo_dir
+    opt.undofile = true
+    opt.undodir = undo_dir
 end
 
 vim.api.nvim_command('autocmd TermOpen term://* startinsert')
-vim.cmd[[set guifont=FiraCode\ Nerd\ Font\ Mono:h12]]
+vim.cmd [[set guifont=FiraCode\ Nerd\ Font\ Mono:h12]]
 
---neovide specific settings
+-- neovide specific settings
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
---neoterm specific settings
+-- neoterm specific settings
 vim.g.neoterm_default_mod = "botright"
 vim.g.neoterm_size = "10"
 vim.g.neoterm_automap_keys = "<leader>tt"
 
---nvui specific settings
+-- nvui specific settings
 if vim.g.nvui then
-  vim.cmd [[NvuiCmdFontFamily FiraCode Nerd Font Mono]]
-  vim.cmd [[NvuiCmdFontSize 12]]
-  vim.cmd [[NvuiAnimationsEnabled 1]]
-  vim.cmd [[NvuiCmdCenterXPos 0.5]]
-  vim.cmd [[NvuiCmdCenterYPos 0.5]]
-  vim.cmd [[NvuiCmdBorderWidth 3]]
-  vim.cmd [[NvuiCmdBorderColor #6E6C6A]]
-  vim.cmd [[NvuiCmdBigFontScaleFactor 1.3]]
-  vim.cmd [[NvuiCmdPadding 13]]
-  vim.cmd [[NvuiPopupMenuBorderWidth 4]]
-  vim.cmd [[NvuiPopupMenuBorderColor #6E6C6A]]
+    vim.cmd [[NvuiCmdFontFamily FiraCode Nerd Font Mono]]
+    vim.cmd [[NvuiCmdFontSize 12]]
+    vim.cmd [[NvuiAnimationsEnabled 1]]
+    vim.cmd [[NvuiCmdCenterXPos 0.5]]
+    vim.cmd [[NvuiCmdCenterYPos 0.5]]
+    vim.cmd [[NvuiCmdBorderWidth 3]]
+    vim.cmd [[NvuiCmdBorderColor #6E6C6A]]
+    vim.cmd [[NvuiCmdBigFontScaleFactor 1.3]]
+    vim.cmd [[NvuiCmdPadding 13]]
+    vim.cmd [[NvuiPopupMenuBorderWidth 4]]
+    vim.cmd [[NvuiPopupMenuBorderColor #6E6C6A]]
 end
