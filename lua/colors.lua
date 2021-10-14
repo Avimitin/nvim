d7351f2d3e0762431eead43a47d999cda6e1aba1
Avimitin <avimitin@gmail.com>
2021-10-14 11:13:09 +0800

    colors: remove custom settings detect
    Since I am using only one colorscheme, there is no reason to keep
    custom.lua.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,28 +1,7 @@
-local ok, custom = pcall(require, "custom")
-
-local settings = {
-  color_scheme = "deus",
-  background = "dark",
-}
-
-if ok and custom ~= nil then
-  if custom.background ~= nil then
-    settings.background = custom.background
-  end
-  if custom.color_scheme ~= nil then
-    settings.color_scheme = custom.color_scheme
-  end
-end
-
 -- theme
 vim.opt.termguicolors=true
-vim.opt.background=settings.background
+vim.opt.background="dark"
+vim.g.deus_background = "mid"
+vim.g.everforest_background = "hard"
 
-local loaded = pcall(vim.cmd, "colorscheme "..settings.color_scheme)
-if not loaded then
-  if settings.color_scheme == nil then
-    print("Unknow empty colorscheme")
-  end
-
-  print("failed to load colorscheme: "..settings.color_scheme)
-end
+vim.cmd("colorscheme deus")
