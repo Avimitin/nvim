7f7b1ce1aa29e855934d10fbfbb09cfa046adad4
Avimitin <avimitin@gmail.com>
2021-10-15 15:09:52 +0800

    plugins/lsp: set lspconfig to open definition in... ...split window
    It is really unconvenient to go back if open the definition in the same
    page. Now lspconfig will open a split window below for the definition
    file.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: dbcf9440c97c5c61ce36a9009f24dc03cd081004 [formerly dbcf9440c97c5c61ce36a9009f24dc03cd081004 [formerly 0624943db121e18f5023d515b92d94504215c48f]]
    Former-commit-id: 30c06a255f37e354f300e7526645331122d8eaa3
    Former-commit-id: c6f20d3f8fe152b1edbb348f6275de3922b729ae

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -144,3 +144,37 @@ vim.lsp.handlers["textDocument/hover"] =
 
 vim.lsp.handlers["textDocument/signatureHelp"] =
     vim.lsp.with(vim.lsp.handlers.signature_help, {border = "single"})
+
+local function goto_definition(split_cmd)
+  local util = vim.lsp.util
+  local log = require("vim.lsp.log")
+  local api = vim.api
+
+  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
+  local handler = function(_, result, ctx)
+    if result == nil or vim.tbl_isempty(result) then
+      local _ = log.info() and log.info(ctx.method, "No location found")
+      return nil
+    end
+
+    if split_cmd then
+      vim.cmd(split_cmd)
+    end
+
+    if vim.tbl_islist(result) then
+      util.jump_to_location(result[1])
+
+      if #result > 1 then
+        util.set_qflist(util.locations_to_items(result))
+        api.nvim_command("copen")
+        api.nvim_command("wincmd p")
+      end
+    else
+      util.jump_to_location(result)
+    end
+  end
+
+  return handler
+end
+
+vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
