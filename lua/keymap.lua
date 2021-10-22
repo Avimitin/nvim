b9c9cde33b3294e94eea6a6fa4c451ef3a721c44
Avimitin <avimitin@gmail.com>
2021-10-22 21:10:34 +0800

    keymap: clean up keymap
    These are some of the keys that are not commonly used and can easily
    conflit with other plugins. So it is time to delete them.
    
    DEPRECATED: keymaps for spliting windows and plugins
    
    * Delete `srv`, `s+jkhl`
    * Delete anyjump keymap
    * Delete hop line keymap
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -25,8 +25,6 @@ vim.g.mapleader = " "
 
 Map("n", ";w", ":w<CR>")
 
-Map("n", "<LEADER>q", ":wq<CR>")
-
 Map("n", "<C-A-q>", ":qa<CR>")
 
 Map("v", "<LEADER>y", [["+y]])
@@ -37,20 +35,10 @@ Map("n", "<ESC>", ":nohlsearch<CR>")
 
 Map("i", "jj", "<ESC>")
 
-Map("n", "spv", "<C-w>t<C-w>H")
-
-Map("n", "srr", "<C-w>b<C-w>K")
-Map("n", "srv", "<C-w>b<C-w>H")
-
 Map("n", "<up>", ":res +5<CR>")
 Map("n", "<down>", ":res -5<CR>")
 Map("n", "<left>", ":vertical resize-5<CR>")
 Map("n", "<right>", ":vertical resize+5<CR>")
-
-Map("n", "sk", "<C-w>k")
-Map("n", "sj", "<C-w>j")
-Map("n", "sh", "<C-w>h")
-Map("n", "sl", "<C-w>l")
 
 -- center line
 Map("i", "<C-c>", "<ESC>zzi")
@@ -62,7 +50,6 @@ Map('n', '<Leader>o', ':NnnPicker %:p:h<CR>')
 Map('n', 'f', ':HopChar2<CR>')
 Map('v', 'f', ':HopChar2<CR>')
 Map('n', '<C-J>', ':HopLine<CR>')
-Map('n', '<C-K>', ':HopLine<CR>')
 
 -- telescope
 Map('n', '<leader>ff', [[<cmd>Telescope find_files<cr>]])
@@ -71,15 +58,8 @@ Map('n', '<leader>fg', [[<cmd>Telescope live_grep<cr>]])
 Map('n', '<leader>fb', [[<cmd>Telescope buffers<cr>]])
 Map('n', '<leader>fh', [[<cmd>Telescope help_tags<cr>]])
 
--- SymbolsOutline
-Map("n", "<leader>l", ":SymbolsOutline<CR>")
-
 -- EasyAlign
 Map("v", "<leader>e", ":EasyAlign<CR>")
-
--- AnyJump
-Map('n', '<leader>aj', ':AnyJump<CR>')
-Map('n', '<leader>ab', ':AnyJumpBack<CR>')
 
 -- vim-go
 Map('n', 'got', ':GoTestFunc<CR>')
