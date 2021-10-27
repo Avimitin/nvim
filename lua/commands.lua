dc7757339bc47658a9a9dcc65f32b425b5e8b02c
Avimitin <avimitin@gmail.com>
2021-10-27 09:18:30 +0800

    commands: create new commands
    Create commands LuaFormat to call the LuaFormat function. Alias the
    FocusSplitNicely with command FSplit.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,7 +2,15 @@ local function new_cmd(cmd, repl)
   vim.cmd("command "..cmd.." "..repl)
 end
 
+-- plugin neoclip
 new_cmd("ClipRec", [[lua require('neoclip').start()]])
 new_cmd("ClipView", [[Telescope neoclip]])
 
+-- plugin neovim-session-manager
 new_cmd("ViewSession", [[Telescope sessions]])
+
+-- plugin lua formmater
+new_cmd("LuaFormat", [[call LuaFormat()]])
+
+-- plugin focus
+new_cmd("FSplit", [[FocusSplitNicely]])
