1f357f8de7448071e4f92d9e60eba815250195ca
Avimitin <avimitin@gmail.com>
2021-10-27 13:29:50 +0800

    keymaps: report error with vim.notify
    Message in print will not appear when shortmess contains F.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 0b277069e53d618238351b5fb9cfd182c53119b8 [formerly 0b277069e53d618238351b5fb9cfd182c53119b8 [formerly 5f3300db3719749403645e070de23b5f0b3b93c0]]
    Former-commit-id: fbf9bd048a9df0127c95cce4aaf3d9e703e4ffaf
    Former-commit-id: 6ca5342034ce4d8a6a0b4ee6d6245b157b8f34df

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,7 +2,7 @@ function Map(mode, lhs, rhs, opts)
     local options = {noremap = true, silent = true}
     if opts then options = vim.tbl_extend("force", options, opts) end
     local stat, error = pcall(vim.api.nvim_set_keymap, mode, lhs, rhs, options)
-    if not stat then print(error) end
+    if not stat then vim.notify(error, vim.log.levels.ERROR, {title='keymap'}) end
 end
 
 Map('n', 'J', '5j')
