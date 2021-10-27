3fe640ef8e110c9b29619bfa937efe7c1cef36db
Avimitin <avimitin@gmail.com>
2021-10-27 11:00:18 +0800

    commands: delete ViewSession command
    This command is setting for neovim-session-manager which is already
    deleted.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: bfd6524bc54762313ce328cd251572f5cc518564 [formerly bfd6524bc54762313ce328cd251572f5cc518564 [formerly d23019b25878a7a49b7c3a0efb502380effc4397]]
    Former-commit-id: 475fd60c31e619f3dec95a8d2df98ea855e1e112
    Former-commit-id: cd061b544fdf00352cdd9a4d5a239675336ca435

--- lua/commands.lua
+++ lua/commands.lua
@@ -6,9 +6,6 @@ end
 new_cmd("ClipRec", [[lua require('neoclip').start()]])
 new_cmd("ClipView", [[Telescope neoclip]])
 
--- plugin neovim-session-manager
-new_cmd("ViewSession", [[Telescope sessions]])
-
 -- plugin lua formmater
 new_cmd("LuaFormat", [[call LuaFormat()]])
 
