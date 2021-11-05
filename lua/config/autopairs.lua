f9f5a4fc5e4b7d794c649ba58be83568facb5ca8
Avimitin <avimitin@gmail.com>
2021-11-05 18:00:03 +0800

    plugins/autopairs: clean up rules and add new rule
    Delete rule for %% and $$. Add new rule for assignment.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/config/autopairs.lua
+++ lua/config/autopairs.lua
@@ -2,6 +2,7 @@ require('nvim-autopairs').setup({enable_check_bracket_line = false})
 
 local npairs = require("nvim-autopairs")
 local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
 
 npairs.setup({
     check_ts = true,
@@ -21,23 +22,56 @@ npairs.setup({
     }
 })
 
-require('nvim-treesitter.configs').setup {autopairs = {enable = true}}
-
-local ts_conds = require('nvim-autopairs.ts-conds')
-
--- press % => %% is only inside comment or string
 npairs.add_rules({
-    Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({'string', 'comment'})),
-    Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({'function'})),
-    Rule(' ', ' '):with_pair(function(opts)
+    Rule(' ', ' ')
+      :with_pair(function (opts)
         local pair = opts.line:sub(opts.col - 1, opts.col)
-        return vim.tbl_contains({'()', '[]', '{}'}, pair)
-    end), Rule('( ', ' )'):with_pair(function() return false end):with_move(
-        function(opts) return opts.prev_char:match('.%)') ~= nil end):use_key(
-        ')'),
-    Rule('{ ', ' }'):with_pair(function() return false end):with_move(
-        function(opts) return opts.prev_char:match('.%}') ~= nil end):use_key(
-        '}'), Rule('[ ', ' ]'):with_pair(function() return false end):with_move(
-        function(opts) return opts.prev_char:match('.%]') ~= nil end):use_key(
-        ']')
+        return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+      end),
+    Rule('( ', ' )')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+            return opts.prev_char:match('.%)') ~= nil
+        end)
+        :use_key(')'),
+    Rule('{ ', ' }')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+            return opts.prev_char:match('.%}') ~= nil
+        end)
+        :use_key('}'),
+    Rule('[ ', ' ]')
+        :with_pair(function() return false end)
+        :with_move(function(opts)
+            return opts.prev_char:match('.%]') ~= nil
+        end)
+        :use_key(']'),
+    Rule('=', '')
+      :with_pair(cond.not_filetypes({'sh', 'bash'}))
+      :with_pair(cond.not_inside_quote())
+      :with_pair(function(opts)
+          local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+          if last_char:match('[%w%=%s]') then
+              return true
+          end
+          return false
+      end)
+      :replace_endpair(function(opts)
+          local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+          local next_char = opts.line:sub(opts.col, opts.col)
+          next_char = next_char == ' ' and '' or ' '
+          if prev_2char:match('%w$') then
+              return '<bs> =' .. next_char
+          end
+          if prev_2char:match('%=$') then
+              return next_char
+          end
+          if prev_2char:match('=') then
+              return '<bs><bs>=' .. next_char
+          end
+          return ''
+      end)
+      :set_end_pair_length(0)
+      :with_move(cond.none())
+      :with_del(cond.none())
 })
