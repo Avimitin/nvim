e3e2626a588f3aed4a32d10eb873bee050a78f48
Avimitin <avimitin@gmail.com>
2021-11-07 17:30:56 +0800

    plugins/anyjump: disable the default keybind
    This can release the <LEADER>j key.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 062ddaee30dfb374332241cff89f07e475ef4468 [formerly 062ddaee30dfb374332241cff89f07e475ef4468 [formerly 2e04c1bde1c7fc34820a7a78ffd1c7dcee72398e]]
    Former-commit-id: 837d99042c41d9bd1b8597ebf8769dc40c877468
    Former-commit-id: b928eb1d8aaf313da56784fed4aa96567e5f298f

--- lua/plug.lua
+++ lua/plug.lua
@@ -250,6 +250,7 @@ return require('packer').startup(function(use)
         setup = function()
             vim.g.any_jump_window_width_ratio = 0.8
             vim.g.any_jump_window_height_ratio = 0.9
+            vim.g.any_jump_disable_default_keybindings = 1
         end,
         cmd = {'AnyJump', 'AnyJumpBack'}
     }
