e3b0425ccc28cabc15407e17174489bb2b7f9eec
Avimitin <avimitin@gmail.com>
2021-11-08 20:06:32 +0800

    plugin/lsp: move keymap to utils.lua
    The lsp keymap is set to registered buffer. The rust-tools.nvim also
    need this keymap. I don't want to write it twice so I moved the keymap
    to utils.lua and expose it by function M.lsp_attach.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 28db8638ef66b322e2ed8107b52e12ed525166be [formerly 28db8638ef66b322e2ed8107b52e12ed525166be [formerly a787647ed48b9ed42d7d3527e03b0ee356e17d70]]
    Former-commit-id: 571bc13cced4677274e09e81e7585ed991d4f39c
    Former-commit-id: 29b374c712fb44f51a2c69831ee6200f0d9d9c1b

--- lua/config/rust.lua
+++ lua/config/rust.lua
@@ -5,26 +5,12 @@ if not ok then
 end
 
 local opts = {
-    tools = { -- rust-tools options
-        -- automatically set inlay hints (type hints)
-        -- There is an issue due to which the hints are not applied on the first
-        -- opened file. For now, write to the file to trigger a reapplication of
-        -- the hints or just run :RustSetInlayHints.
-        -- default: true
+    tools = {
         autoSetHints = true,
-
-        -- whether to show hover actions inside the hover window
-        -- this overrides the default hover handler so something like lspsaga.nvim's hover would be overriden by this
-        -- default: true
         hover_with_actions = true,
-
-        -- These apply to the default RustRunnables command
         runnables = {
-            -- whether to use telescope for selection menu or not
-            -- default: true
             use_telescope = true,
 
-            -- rest of the opts are forwarded to telescope
             prompt_prefix = "  ",
             selection_caret = "  ",
             entry_prefix = "  ",
@@ -36,64 +22,36 @@ local opts = {
                 width = 0.3,
                 height = 0.50,
                 preview_cutoff = 0,
-                prompt_position = 'bottom',
+                prompt_position = 'bottom'
             }
         },
 
         -- These apply to the default RustSetInlayHints command
         inlay_hints = {
-            -- wheter to show parameter hints with the inlay hints or not
-            -- default: true
             show_parameter_hints = true,
-
-            -- prefix for parameter hints
-            -- default: "<-"
             parameter_hints_prefix = "<- ",
-
-            -- prefix for all the other hints (type, chaining)
-            -- default: "=>"
             other_hints_prefix = "=> ",
-
-            -- whether to align to the lenght of the longest line in the file
             max_len_align = false,
-
-            -- padding from the left if max_len_align is true
             max_len_align_padding = 1,
-
-            -- whether to align to the extreme right or not
             right_align = false,
-
-            -- padding from the right if right_align is true
             right_align_padding = 7
         },
 
         hover_actions = {
-            -- the border that is used for the hover window
-            -- see vim.api.nvim_open_win()
             border = {
                 {"╭", "FloatBorder"}, {"─", "FloatBorder"},
                 {"╮", "FloatBorder"}, {"│", "FloatBorder"},
                 {"╯", "FloatBorder"}, {"─", "FloatBorder"},
                 {"╰", "FloatBorder"}, {"│", "FloatBorder"}
             },
-
-            -- whether the hover action window gets automatically focused
-            -- default: false
             auto_focus = true
         }
     },
-
-    -- all the opts to send to nvim-lspconfig
-    -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = {} -- rust-analyer options
+    server = {on_attach = require('utils').lsp_attach} -- rust-analyer options
 }
 
 require('rust-tools').setup(opts)
 
-vim.api.nvim_set_keymap('n', '<Leader>ra',
-                        ':lua require("rust-tools.hover_actions").hover_actions()<CR>',
-                        {noremap = true, silent = false})
-vim.api.nvim_set_keymap('n', '<Leader>ri',
-                        ':lua require("rust-tools.inlay_hints").set_inlay_hints()<CR>',
-                        {noremap = true, silent = false})
+local map = require('utils').map
+map('n', '<Leader>ra',
+    ':lua require("rust-tools.hover_actions").hover_actions()<CR>')
