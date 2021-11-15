0ce1d10111321ad2fa4c615c77eb690083011065
Avimitin <avimitin@gmail.com>
2021-11-15 17:19:14 +0800

    R!/nvim-cmp: refactor the keymap setting
    Delete ctrl-p and ctrl-n, as the <tab> can serve all the function. Set
    keymap for specific mode.
    
    BREAKING CHANGE:
    * ctrl-p and ctrl-n is no more mapped in nvim-cmp
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -26,12 +26,12 @@ cmp.setup({
       })}),
   },
    mapping = {
-      ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-n>"] = cmp.mapping.select_next_item(),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.close(),
+      ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
       ["<CR>"] = cmp.mapping.confirm {
          behavior = cmp.ConfirmBehavior.Replace,
          select = true,
