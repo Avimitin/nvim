55dd8e40a52734ce459e7d621be74671e1704b58
Avimitin <avimitin@gmail.com>
2021-11-15 17:06:18 +0800

    C/nvim-cmp: expand the tab inside config
    Align all the contents.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 9f591885d536fecf5c1286d86464003393d1d484 [formerly 9f591885d536fecf5c1286d86464003393d1d484 [formerly a8bcb9e4d318f5b0f95c12c9e70df3dfc60a0145]]
    Former-commit-id: 0b160a06efddec563162cd6ce81a709a51e534dd
    Former-commit-id: 74cf9dd0abae8d802f18a839562eab92cade398a

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -18,11 +18,11 @@ cmp.setup({
   },
   formatting = {
     format = require("lspkind").cmp_format({with_text = true, menu = ({
-	buffer = "[Buffer]",
-	nvim_lsp = "[LSP]",
-	luasnip = "[LuaSnip]",
-	nvim_lua = "[Lua]",
-	latex_symbols = "[Latex]",
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[Latex]",
       })}),
   },
    mapping = {
@@ -37,24 +37,24 @@ cmp.setup({
          select = true,
       },
       ["<Tab>"] = cmp.mapping(function(fallback)
-	if cmp.visible() then
-	  cmp.select_next_item()
-	elseif vim.fn["vsnip#available"]() == 1 then
-	  feedkey("<Plug>(vsnip-expand-or-jump)", "")
-	elseif has_words_before() then
-	  cmp.complete()
-	else
-	  fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-	end
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif vim.fn["vsnip#available"]() == 1 then
+          feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        end
       end, { "i", "s" }),
 
 
       ["<S-Tab>"] = cmp.mapping(function()
-	if cmp.visible() then
-	  cmp.select_prev_item()
-	elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-	  feedkey("<Plug>(vsnip-jump-prev)", "")
-	end
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+          feedkey("<Plug>(vsnip-jump-prev)", "")
+        end
       end, { "i", "s" }),
    },
   sources = {
