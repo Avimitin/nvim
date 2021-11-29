a8ccb16414ac18d1cb58612c45c5c47365417402
Avimitin <avimitin@gmail.com>
2021-11-29 09:41:53 +0800

    N!/escape: use plugins for better escape
    I've used `jj` as my keymap for a long time. But it is annoying that
    when you press j for some word, neovim will wait for another key to
    press. This let the cursor stop at j key and waiting for other key. If
    you actually need to input `jj` in this case, you must first insert a
    space than insert j. It is not convenient, so I use `<Alt-;>` to replace
    it in commit 3fadf94a3bfcb24658d9c487dd6a9e4cce76017c [formerly 447f652538d43416b7543af719b0ceea2de732be] [formerly 1199d41b5bdbc3f3f1b61767bb81d2b90935a101] [formerly 156e3b4ec88bce874171a00948a7fa546e1e484a [formerly 156e3b4ec88bce874171a00948a7fa546e1e484a [formerly 24d673c5ccf5fbca46abf270601cb39fe8d8763e]]].
    
    This plugin can help avoid the above case and also make the `j` key
    input experience smoother.
    
    BREAKING CHANGE: delete the `<Alt-;>` key.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 37d7a2105138c79b84978feb51f8fa0fcd1b808b [formerly 37d7a2105138c79b84978feb51f8fa0fcd1b808b [formerly 5c4e22ea03f381de379ffe7012b881f450c54099]]
    Former-commit-id: c8723df67aa9c4148f54a1c6e18f2ead52391a55
    Former-commit-id: dd679162ff9301bdfa16605292d96122b01ff25c

--- lua/plug.lua
+++ lua/plug.lua
@@ -336,4 +336,16 @@ return require('packer').startup(function(use)
     use {
       "ggandor/lightspeed.nvim"
     }
+
+    use {
+      "max397574/better-escape.nvim",
+      config = function()
+        require("better_escape").setup {
+            mapping = {"jj"}, -- a table with mappings to use
+            timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+            clear_empty_lines = true, -- clear line after escaping if there is only whitespace
+            keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+        }
+      end
+    }
 end)
