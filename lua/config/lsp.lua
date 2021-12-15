616dd09f2fd55576e52f060ff374661176e98078
Avimitin <avimitin@gmail.com>
2021-12-15 08:38:53 +0800

    C: add lua-format file to specify format options
    Usage: `lua-format -i {SOURCE_FILE} -c .lua-format`
    
    Reference:
    * LuaFormatter: https://github.com/Koihik/LuaFormatter
    * Format Setting:
        https://github.com/Koihik/LuaFormatter/blob/master/docs/Style-Config.md
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 6f17146f6364156a4f01b550d2c8b1f54cbf300d [formerly 6f17146f6364156a4f01b550d2c8b1f54cbf300d [formerly f2009fe148555d3c48914cdff161f8a9349d8a75]]
    Former-commit-id: 7acdb4f1063bdb07ff53713a8b71f7994c6c0752
    Former-commit-id: 0a7289dd3e69788ac95d42a87ce1ebbea1e360bd

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,15 +1,18 @@
 local present1, _ = pcall(require, "lspconfig")
 local present2, installer = pcall(require, "nvim-lsp-installer")
 if not (present1 or present2) then
-    vim.notify("Fail to setup LSP", vim.log.levels.ERROR, {title = 'plugins'})
-    return
+  vim.notify("Fail to setup LSP", vim.log.levels.ERROR, {
+    title = 'plugins'
+  })
+  return
 end
 
 local on_attach = require('utils').lsp_attach
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.documentationFormat = {
-    "markdown", "plaintext"
+  "markdown",
+  "plaintext"
 }
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.preselectSupport = true
@@ -19,79 +22,107 @@ capabilities.textDocument.completion.completionItem.deprecatedSupport = true
 capabilities.textDocument.completion.completionItem.commitCharactersSupport =
     true
 capabilities.textDocument.completion.completionItem.tagSupport = {
-    valueSet = {1}
+  valueSet = {1}
 }
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {"documentation", "detail", "additionalTextEdits"}
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits"
+  }
 }
 
 -- lspInstall + lspconfig stuff
 
 local lua_setting = {
-    Lua = {
-        diagnostics = {globals = {"vim"}},
-        workspace = {
-            library = {
-                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
-            },
-            maxPreload = 100000,
-            preloadFileSize = 10000
-        },
-        telemetry = {enable = false}
+  Lua = {
+    diagnostics = {
+      globals = {
+        "vim"
+      }
+    },
+    workspace = {
+      library = {
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+      },
+      maxPreload = 100000,
+      preloadFileSize = 10000
+    },
+    telemetry = {
+      enable = false
     }
+  }
 }
 
 if installer.settings then
-    installer.settings {
-        ui = {
-            icons = {
-                server_installed = "✓",
-                server_pending = "➜",
-                server_uninstalled = "✗"
-            }
-        }
+  installer.settings {
+    ui = {
+      icons = {
+        server_installed = "✓",
+        server_pending = "➜",
+        server_uninstalled = "✗"
+      }
+    }
+  }
+
+  local servers = {
+    "clangd",
+    "gopls",
+    "sumneko_lua"
+  }
+
+  for _, lang in pairs(servers) do
+    local ok, server = installer.get_server(lang)
+    if ok then
+      if not server:is_installed() then
+        print("Installing " .. lang)
+        server:install()
+      end
+    end
+  end
+
+  installer.on_server_ready(function(server)
+    local opts = {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      root_dir = vim.loop.cwd
     }
 
-    local servers = {"clangd", "gopls", "sumneko_lua"}
-
-    for _, lang in pairs(servers) do
-        local ok, server = installer.get_server(lang)
-        if ok then
-            if not server:is_installed() then
-                print("Installing " .. lang)
-                server:install()
-            end
-        end
+    if server.name == "sumneko_lua" then
+      opts.settings = lua_setting
     end
 
-    installer.on_server_ready(function(server)
-        local opts = {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            root_dir = vim.loop.cwd
-        }
-
-        if server.name == "sumneko_lua" then opts.settings = lua_setting end
-
-        -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-        server:setup(opts)
-        vim.cmd [[ do User LspAttachBuffers ]]
-    end)
+    -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
+    server:setup(opts)
+    vim.cmd [[ do User LspAttachBuffers ]]
+  end)
 end
 
-local signs = {Error = " ", Warn = " ", Hint = " ", Info = " "}
+local signs = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " "
+}
 
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, {
+    text = icon,
+    texthl = hl,
+    numhl = ""
+  })
 end
 
 local lsp_publish_diagnostics_options = {
-    virtual_text = {prefix = "", spacing = 0},
-    signs = true,
-    underline = true,
-    update_in_insert = false -- update diagnostics insert mode
+  virtual_text = {
+    prefix = "",
+    spacing = 0
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false -- update diagnostics insert mode
 }
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
@@ -99,39 +130,45 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
                  lsp_publish_diagnostics_options)
 
 vim.lsp.handlers["textDocument/hover"] =
-    vim.lsp.with(vim.lsp.handlers.hover, {border = "single"})
+    vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "single"
+    })
 
 vim.lsp.handlers["textDocument/signatureHelp"] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, {border = "single"})
+    vim.lsp.with(vim.lsp.handlers.signature_help, {
+      border = "single"
+    })
 
 local function goto_definition(split_cmd)
-    local util = vim.lsp.util
-    local log = require("vim.lsp.log")
-    local api = vim.api
+  local util = vim.lsp.util
+  local log = require("vim.lsp.log")
+  local api = vim.api
 
-    -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-    local handler = function(_, result, ctx)
-        if result == nil or vim.tbl_isempty(result) then
-            local _ = log.info() and log.info(ctx.method, "No location found")
-            return nil
-        end
-
-        if split_cmd then vim.cmd(split_cmd) end
-
-        if vim.tbl_islist(result) then
-            util.jump_to_location(result[1])
-
-            if #result > 1 then
-                util.set_qflist(util.locations_to_items(result))
-                api.nvim_command("copen")
-                api.nvim_command("wincmd p")
-            end
-        else
-            util.jump_to_location(result)
-        end
+  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
+  local handler = function(_, result, ctx)
+    if result == nil or vim.tbl_isempty(result) then
+      local _ = log.info() and log.info(ctx.method, "No location found")
+      return nil
     end
 
-    return handler
+    if split_cmd then
+      vim.cmd(split_cmd)
+    end
+
+    if vim.tbl_islist(result) then
+      util.jump_to_location(result[1])
+
+      if #result > 1 then
+        util.set_qflist(util.locations_to_items(result))
+        api.nvim_command("copen")
+        api.nvim_command("wincmd p")
+      end
+    else
+      util.jump_to_location(result)
+    end
+  end
+
+  return handler
 end
 
 vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
