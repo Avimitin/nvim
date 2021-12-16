d3248c2fb11ff61cb916f2d2b38ee66e2b86ee2a
Avimitin <avimitin@gmail.com>
2021-12-16 21:48:29 +0800

    F/lsp: replace all deprecated function
    vim.lsp.diagnostic is now displaced by vim.diagnostic
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: c4256285f064cd262088a1d3423e0bfc4e386aab [formerly c4256285f064cd262088a1d3423e0bfc4e386aab [formerly ea0276096abda34162ef8af3291c3ccedb420fa3]]
    Former-commit-id: 1585ccf2243f8fd5ed4fb20d349b3fa9dcc862bc
    Former-commit-id: 2b88c9b140bcb66218cbfd1280c6ecf2ac115ac6

--- lua/utils.lua
+++ lua/utils.lua
@@ -121,11 +121,11 @@ M.lsp_attach = function (client, bufnr)
                    opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '<space>e',
-                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+                   '<cmd>lua vim.diagnostic.show_line_diagnostics()<CR>',
                    opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
+    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>',
                    opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
+    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>',
                    opts)
     buf_set_keymap('n', '<space>q',
                    '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
