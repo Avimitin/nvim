0375ac6b6418c42ad7cad2afd9bd165d578fa2de
Avimitin <avimitin@gmail.com>
2021-12-23 16:57:12 +0800

    N/colorscheme: add new colorscheme kanagawa
    Plugin: https://github.com/rebelot/kanagawa.nvim
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,7 +1,9 @@
 -- theme
 vim.opt.termguicolors=true
 vim.opt.background="dark"
-local theme = "deus"
+-- Available theme value:
+-- "ayu", "kanagawa", "deus"
+local theme = "kanagawa"
 
 local function ayu_setup()
   require('ayu').setup({
@@ -14,9 +16,27 @@ local function deus_setup()
   vim.g.deus_background = "hard"
 end
 
+local function kanagawa_setup()
+  require('kanagawa').setup({
+      undercurl = true,           -- enable undercurls
+      commentStyle = "italic",
+      functionStyle = "bold",
+      keywordStyle = "italic",
+      statementStyle = "bold",
+      typeStyle = "NONE",
+      variablebuiltinStyle = "italic",
+      specialReturn = true,       -- special highlight for the return keyword
+      specialException = true,    -- special highlight for exception handling keywords
+      transparent = false,        -- do not set background color
+      colors = {},
+      overrides = {},
+  })
+end
+
 local theme_opt = {
   ["ayu"] = ayu_setup,
   ["deus"] = deus_setup,
+  ["kanagawa"] = kanagawa_setup,
 }
 
 theme_opt[theme]()
