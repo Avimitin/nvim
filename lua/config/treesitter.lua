c5fcd4b77805df4ee901a8eadfde01d73fadf96d
Avimitin <avimitin@gmail.com>
2021-12-31 12:01:29 +0800

    new,plugin: add new plugin neorg
    This plugin can help using neovim to write org-mode file.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,4 +1,30 @@
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_configs.norg = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg",
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "main"
+    },
+}
+
+parser_configs.norg_meta = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+        files = { "src/parser.c" },
+        branch = "main"
+    },
+}
+
+parser_configs.norg_table = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+        files = { "src/parser.c" },
+        branch = "main"
+    },
+}
+
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"cpp", "toml", "rust", "go", "json", "lua", "comment"},
+    ensure_installed = {"cpp", "toml", "rust", "go", "json", "lua", "comment", "norg", "norg_meta", "norg_table"},
     highlight = {enable = true}
 }
