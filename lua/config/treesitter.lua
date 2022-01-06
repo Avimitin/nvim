810a2a850c837fca56efc75becf18cac64d918c0
Avimitin <avimitin@gmail.com>
2022-01-06 17:27:02 +0800

    rwt!plugin: remove plugin neorg
    I finally know that this is not the Emacs org mode. This is just an
    extend version but not complete yet. And all I use is just the multiple
    task marker. I can do this in MarkDown, I don't need it. So I deem to
    remove it.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 471ff6dd81f47ef95b192003377531c4dbbe5d25 [formerly 471ff6dd81f47ef95b192003377531c4dbbe5d25 [formerly f2ca0cd319ae68b9e2b2bb68a084a9e6db0e4b1f]]
    Former-commit-id: bb64ada7db14ce2ea9ad762fb4e14302783a28cd
    Former-commit-id: b34a6dfb4cb923ee73d40976935ef47cc5b0fb3e

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,30 +1,11 @@
-local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
-parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg",
-        files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main"
-    },
-}
-
-parser_configs.norg_meta = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
-
-parser_configs.norg_table = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
-
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"cpp", "toml", "rust", "go", "json", "lua", "comment", "norg", "norg_meta", "norg_table"},
-    highlight = {enable = true}
+  ensure_installed = {
+    "cpp", "toml",
+    "rust", "go",
+    "json", "lua",
+    "comment"
+  },
+  highlight = {
+    enable = true
+  }
 }
