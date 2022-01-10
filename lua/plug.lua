1b8e9d41a69f98b0df083bf39bfbb4e9bbd25f1b
Avimitin <avimitin@gmail.com>
2022-01-10 10:29:24 +0800

    rwt,lspsaga: replace icon to nerd font icon
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 6407cb8b60e10aed762e1529361a34912572552c [formerly 6407cb8b60e10aed762e1529361a34912572552c [formerly 2fb1bc3a31ee99b959a33bdce2c39f3bde1e10a6]]
    Former-commit-id: 13cb9ea212cc167b886b38489b903acbbbff55c0
    Former-commit-id: 4d0e067b7cd925d9d119e2043392eb8d774d562f

--- lua/plug.lua
+++ lua/plug.lua
@@ -177,15 +177,15 @@ return require('packer').startup(function(use)
         infor_sign = "",
         diagnostic_header_icon = "   ",
         -- code action title icon
-        code_action_icon = " ",
+        code_action_icon = " ",
         code_action_prompt = {
           enable = true,
           sign = true,
           sign_priority = 40,
           virtual_text = true
         },
-        finder_definition_icon = "  ",
-        finder_reference_icon = "  ",
+        finder_definition_icon = "  ",
+        finder_reference_icon = "  ",
         max_preview_lines = 10,
         finder_action_keys = {
           open = "o",
@@ -203,9 +203,9 @@ return require('packer').startup(function(use)
           quit = "<C-c>",
           exec = "<CR>"
         },
-        definition_preview_icon = "  ",
+        definition_preview_icon = "  ",
         border_style = "single",
-        rename_prompt_prefix = "➤",
+        rename_prompt_prefix = "",
         server_filetype_map = {},
         diagnostic_prefix_format = "%d. "
       }
