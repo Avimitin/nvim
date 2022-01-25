b39526ce3b2d7861638509257398bfab84acb9f7
Avimitin <avimitin@gmail.com>
2022-01-25 23:55:23 +0800

    reft/plugin: move all the packer setup script into plugins.lua
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,29 +1,114 @@
-require('packer').init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float {
-        border = "single"
-      }
-    end
-  },
-  git = {
-    clone_timeout = 60 -- Timeout, in seconds, for git clones
-  }
-}
+local utils = require('utils')
+local M = {}
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use {
-    "wbthomason/packer.nvim",
-    event = "VimEnter"
-  }
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 
-  local component = {'colors', 'cmp', 'coding', 'mkd', 'git_tools', 'enhance'}
-  for _, compo in ipairs(component) do
-    for _, plugin in ipairs(require("partial."..compo)) do
-      use(plugin)
-    end
+-- has_packer return the packer install status
+local function has_packer()
+  return vim.fn.empty(vim.fn.glob(install_path)) == 0
+end
+
+-- install_packer will use git to install packer to the install_path
+local function install_packer()
+  -- detecting plugin manager
+  local fn = vim.fn
+
+  utils.log_info("Installing packer to " .. install_path)
+  fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+end
+
+-- add_packer will add packer into the optional plugin directory
+local function add_packer()
+  local packer_call, error_msg = pcall(vim.cmd, [[packadd packer.nvim]])
+  if not packer_call then
+    utils.log_err(error_msg, "load plugin")
+    return
   end
-end)
 
+  vim.cmd([[autocmd BufWritePost plug.lua source <afile> | PackerCompile]])
+end
+
+-- init_packer will setup the packer style
+local function init_packer()
+  require('packer').init {
+    display = {
+      open_fn = function()
+        return require("packer.util").float {
+          border = "single"
+        }
+      end
+    },
+    git = {
+      clone_timeout = 60 -- Timeout, in seconds, for git clones
+    }
+  }
+end
+
+-- setup_plugins will get list of plugins definition and use them
+local function setup_plugins()
+  require('packer').startup(function(use)
+    -- Packer can manage itself
+    use {
+      "wbthomason/packer.nvim",
+      event = "VimEnter"
+    }
+
+    local component = {'colors', 'cmp', 'coding', 'mkd', 'git_tools', 'enhance'}
+    for _, compo in ipairs(component) do
+      for _, plugin in ipairs(require("partial."..compo)) do
+        use(plugin)
+      end
+    end
+  end)
+end
+
+-- prehook setup global variables before plugins loaded
+local function prehook()
+  if not vim.fn.has("nvim-0.6") then
+    -- for filetype.nvim
+    -- If using a Neovim version earlier than 0.6.0
+    vim.g.did_load_filetypes = 1
+  end
+
+  -- for vsnip
+  vim.g.vsnip_snippet_dir = vim.fn.expand("~/.config/nvim/vsnip")
+
+  -- for wildfire
+  vim.g.wildfire_objects = {"i'", 'i"', "i)", "i]", "i}", "ip", "it", "i`"}
+end
+
+-- posthook run script after plugins loaded
+local function posthook()
+  -- Run rooter when it is the first time enter the neovim
+  vim.cmd [[autocmd VimEnter * Rooter]]
+end
+
+-- load will try to detect the packer installation status.
+-- It will automatically install packer to the install_path.
+-- Then it will called the setup script to setup all the plugins.
+M.load = function ()
+  if not has_packer() then
+    install_packer()
+    add_packer()
+    setup_plugins()
+    require('packer').sync()
+    vim.cmd("au User PackerComplete echom 'Plugins are installed successfully, please use :qa to restart the neovim'")
+    return
+  end
+
+  add_packer()
+  init_packer()
+  prehook()
+  setup_plugins()
+  posthook()
+end
+
+return M
 -- vim: foldmethod=marker
