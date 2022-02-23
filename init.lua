4ea10f41e7f0da332ed03b49b5497218e121f4cc
Avimitin <avimitin@gmail.com>
2022-02-23 23:13:15 +0800

    [NEW] plugins: add impatient.nvim to optimize the start up time
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- init.lua
+++ init.lua
@@ -14,6 +14,12 @@ License: MIT License
 -- load basic configuration
 local utils = require("utils")
 
+-- Try to call the cache plugin
+local cache_ok, cache_err = pcall(require, "impatient")
+if not cache_ok then
+  utils.errorL(cache_err, "cache")
+end
+
 for _, module_name in ipairs({ "options", "mappings", "commands", "autocmd" }) do
   local ok, err = pcall(require, module_name)
   if not ok then
