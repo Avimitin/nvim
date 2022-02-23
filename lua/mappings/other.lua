80655ae97f06b03a38125680a9cbe87392d8992f
Avimitin <avimitin@gmail.com>
2022-02-23 22:31:55 +0800

    [RFT] keymap: separate the editor keymap and plugin keymap
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 4c44a4de1d9120a77fbc71913a4efb4f549fe717 [formerly 4c44a4de1d9120a77fbc71913a4efb4f549fe717 [formerly 87709236b5f687166509e128b6d28e1dc59d7bc0]]
    Former-commit-id: d3f1a4a23b72eeffee3c07691363fd8ea1dfbc07
    Former-commit-id: 96944846806058d74ea9e91800da0873083bb347

---
+++ lua/mappings/other.lua
@@ -0,0 +1,32 @@
+local map = require("utils").map
+-- EasyAlign
+map("v", "<leader>e", ":EasyAlign<CR>")
+
+-- nvim-tree
+map("n", ";t", ":NvimTreeToggle<CR>")
+
+map("n", "<C-\\>", [[:FTermToggle<CR>]])
+map("t", "<C-\\>", [[<C-\><C-n>:FTermToggle<CR>]])
+map("t", "<C-n>", [[<C-\><C-n>]])
+
+map("n", ";f", [[:lua require('telescope.builtin').find_files{}<CR>]])
+map("n", "<LEADER>tg", [[:lua require('telescope.builtin').live_grep{}<CR>]])
+
+map("n", ";g", [[<CMD>Git<CR>]])
+map("n", "<LEADER>l", [[<CMD>LazyGit<CR>]])
+
+map("i", "<A-;>", "<ESC>")
+
+-- bufferline tab stuff
+map("n", "<C-c>", ":BufferLinePickClose<CR>") -- close tab
+
+-- move between tabs
+map("n", ";n", [[<Cmd>BufferLineCycleNext<CR>]])
+map("n", ";p", [[<Cmd>BufferLineCyclePrev<CR>]])
+
+-- move tabs
+map("n", "<A->>", [[<CMD>BufferLineMoveNext<CR>]])
+map("n", "<A-<>", [[<CMD>BufferLineMovePrev<CR>]])
+map("n", "<A-p>", [[<CMD>:BufferLinePick<CR>]])
+
+map("n", ";d", ":Dispatch ", { noremap = true, silent = false })
