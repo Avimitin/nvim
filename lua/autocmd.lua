1f43bc91411a7e5586463c3c2b445c4b48a462cb
Avimitin <avimitin@gmail.com>
2022-02-23 23:01:40 +0800

    [RFT] autocmd: move packer compile autocmd to file
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 7df6a470eff35ee50d0a122a4928398c0ceb4fcb [formerly 7df6a470eff35ee50d0a122a4928398c0ceb4fcb [formerly ede5cad294d0d04f4422c509ee3f6d0f1c34c1a6]]
    Former-commit-id: d8b00620a9a667326f578558845b9dd64810a1f0
    Former-commit-id: 713530f183d7412965f1caf5bff58987e6e54b25

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,3 +1,5 @@
 -- use relativenumber when editing
 vim.cmd[[ au InsertEnter * set norelativenumber ]]
 vim.cmd[[ au InsertLeave * set relativenumber ]]
+
+vim.cmd([[autocmd BufWritePost load.lua source <afile> | PackerCompile]])
