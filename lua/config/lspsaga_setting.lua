7378eb045848c45015eff797be8f5074218a7e4a
Avimitin <avimitin@gmail.com>
2022-02-23 17:29:21 +0800

    [RFT] lsp: update sign icon
    * Use lspsaga to display the icons
    * Close virtual text hint for code action
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/config/lspsaga_setting.lua
+++ lua/config/lspsaga_setting.lua
@@ -3,18 +3,19 @@ lspsaga.setup { -- defaults ...
   debug = false,
   use_saga_diagnostic_sign = true,
   -- diagnostic sign
-  error_sign = "",
-  warn_sign = "",
-  hint_sign = "",
-  infor_sign = "",
+  use_diagnostic_virtual_text = true,
+  error_sign = "",
+  warn_sign = "",
+  hint_sign = "",
+  infor_sign = "",
   diagnostic_header_icon = "   ",
   -- code action title icon
-  code_action_icon = " ",
+  code_action_icon = " ",
   code_action_prompt = {
     enable = true,
     sign = true,
     sign_priority = 40,
-    virtual_text = true
+    virtual_text = false
   },
   finder_definition_icon = "  ",
   finder_reference_icon = "  ",
@@ -35,7 +36,7 @@ lspsaga.setup { -- defaults ...
     quit = "<C-c>",
     exec = "<CR>"
   },
-  definition_preview_icon = "  ",
+  definition_preview_icon = "  ",
   border_style = "single",
   rename_prompt_prefix = "",
   server_filetype_map = {},
