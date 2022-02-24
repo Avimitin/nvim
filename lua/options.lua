4441fe09898d7f1141846b0aab6fde4a53815bc5
Avimitin <avimitin@gmail.com>
2022-02-24 12:02:47 +0800

    [DOC] options: add comment for options
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -10,13 +10,13 @@ opt.fileencoding = "utf-8"
 opt.number = true
 opt.rnu = true
 
--- line behind cursor
+-- highlight the current line
 opt.cursorline = true
 
--- [[
 -- TAB SETTING
--- Use a mix of tab and space to avoid mix up file
--- ]]
+-- Use 2 spaces forcely. But don't worry, vim-sleuth will handle the indent
+-- gracefully. See <https://github.com/tpope/vim-sleuth> for more.
+--
 -- Use the appropriate number of spaces to insert a <Tab>.
 opt.expandtab = true
 -- Number of spaces that a <Tab> in the file counts for.
@@ -25,7 +25,7 @@ opt.shiftwidth = 2
 opt.softtabstop = 2
 
 -- Copy indent from current line when starting a new line
--- opt.autoindent=true
+opt.autoindent=true
 
 -- A List is an ordered sequence of items.
 opt.list = true
@@ -43,16 +43,23 @@ opt.timeout = false
 -- remember where to recover cursor
 opt.viewoptions = "cursor,folds,slash,unix"
 
--- [[
 -- lines longer than the width of the window will wrap and displaying continues
 -- on the next line.
--- ]]
 opt.wrap = true
+
+-- set text width to zero to use the wrap functionality
 opt.tw = 0
+
 opt.cindent = true
+
+-- set windows split at bottom-right by default
 opt.splitright = true
 opt.splitbelow = true
+
+-- don't show the "--VISUAL--" "--INSERT--" text
 opt.showmode = false
+
+-- show chars, selected block in visual mode
 opt.showcmd = true
 
 -- auto completion on command
@@ -70,11 +77,20 @@ opt.lazyredraw = true
 opt.visualbell = true
 opt.updatetime = 100
 opt.virtualedit = "block"
+
+-- highlight a column at the 100 chars
 opt.colorcolumn = "100"
+
+-- screen will not redraw when exec marcro, register
 opt.lazyredraw = true
+
+-- always draw signcolumn, with 1 fixed space to show 2 icon at the same time
 opt.signcolumn = "yes:1"
+
+-- enable all the mouse functionality
 opt.mouse = "a"
 
+-- use indent as the fold method
 opt.foldmethod = "indent"
 opt.foldlevel = 99
 opt.foldenable = true
