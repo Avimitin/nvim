73a1f8fdea3e1565223d5793540f5914244909ee
Avimitin <avimitin@gmail.com>
2022-02-26 10:46:45 +0800

    [RFT!] lspconfig: move lsp_attach to lspconfig
    Move the core.utils.lsp_attach to plugins.config.lspconfig_cfg.set_lsp_key
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: feb194bbf9d0455de63300cc36c5b58396db7eb9 [formerly feb194bbf9d0455de63300cc36c5b58396db7eb9 [formerly bcc3ca706a28d26bec0e8c7eaa3718c1850b634d]]
    Former-commit-id: 8a88008fab03899c08231d65784b6c34b7b92770
    Former-commit-id: edf34995b6b1c7dd2a72f55eae7b20b6c2b88994

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -22,57 +22,6 @@ M.infoL = function(msg, title)
   })
 end
 
--- this function serve the LSP
-M.lsp_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-  -- Mappings.
-  local opts = {
-    noremap = true,
-    silent = true,
-  }
-  buf_set_keymap("n", "gd", "<Cmd>Lspsaga preview_definition<CR>", opts)
-  buf_set_keymap("n", "gh", "<Cmd>Lspsaga hover_doc<CR>", opts)
-  buf_set_keymap(
-    "n",
-    "<C-u>",
-    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
-    opts
-  )
-  buf_set_keymap(
-    "n",
-    "<C-d>",
-    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>",
-    opts
-  )
-  buf_set_keymap("n", "gs", "<cmd>Lspsaga signature_help<CR>", opts)
-  buf_set_keymap("n", "go", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-  buf_set_keymap("n", "gj", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-  buf_set_keymap("n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
-  buf_set_keymap("n", "gx", "<cmd>Lspsaga code_action<CR>", opts)
-
-  buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gm", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("n", "gq", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
-
-  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap("x", "gf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-  end
-end
-
 M.fek = function(key, mode)
   vim.fn.feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode)
 end
--- lua/plugins/config/lspconfig_cfg.lua
+++ lua/plugins/config/lspconfig_cfg.lua
@@ -7,7 +7,56 @@ if not (present1 or present2) then
   return
 end
 
-local on_attach = require("core.utils").lsp_attach
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- Mappings.
+  local opts = {
+    noremap = true,
+    silent = true,
+  }
+  buf_set_keymap("n", "gd", "<Cmd>Lspsaga preview_definition<CR>", opts)
+  buf_set_keymap("n", "gh", "<Cmd>Lspsaga hover_doc<CR>", opts)
+  buf_set_keymap(
+    "n",
+    "<C-u>",
+    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
+    opts
+  )
+  buf_set_keymap(
+    "n",
+    "<C-d>",
+    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>",
+    opts
+  )
+  buf_set_keymap("n", "gs", "<cmd>Lspsaga signature_help<CR>", opts)
+  buf_set_keymap("n", "go", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+  buf_set_keymap("n", "gj", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+  buf_set_keymap("n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
+  buf_set_keymap("n", "gx", "<cmd>Lspsaga code_action<CR>", opts)
+
+  buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("n", "gm", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("n", "gq", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
+
+  -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("x", "gf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
+end
+
 
 -- Gets a new ClientCapabilities object describing the LSP client
 -- capabilities.
@@ -144,3 +193,8 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "single",
 })
+
+
+return {
+  set_lsp_key = on_attach
+}
