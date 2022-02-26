66be2a1a79a67506c226b90f15848fdf061b4526
Avimitin <avimitin@gmail.com>
2022-02-26 17:15:57 +0800

    [MSC] benchmark: implement all the benchmark functionality
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 2e67f8734e4c9ac51666f630bc08ad5fc0675a3c [formerly 2e67f8734e4c9ac51666f630bc08ad5fc0675a3c [formerly 30c4393d077e03ea7785373e8677e7258888361f]]
    Former-commit-id: df1db58e9abe2a03d02d31e9f2dba1fdb3116dfe
    Former-commit-id: c30e8b32647e99d3d31d4b20c3db6a38b5a166df

--- fixtures/benchmark.pl
+++ fixtures/benchmark.pl
@@ -1,54 +1,89 @@
 #!/usr/bin/perl
+use warnings;
+use strict;
 
+# which file to write the result
 my $output_file = "./fixtures/benchmark.txt";
+
+# where to get the neovim startup time data
 my $sttime_file = "/tmp/nvim-startuptime";
+
+# how to run the neovim
 my $nvim_cmd = "nvim --headless --startuptime ".$sttime_file." -c 'au VimEnter * quitall'";
 
+# get the last line of the result
 sub read_last_line {
   return `tail -n1 /tmp/nvim-startuptime`;
 }
 
+# split and get the first number of the result
 sub split_score {
   return split(' ', shift);
 }
 
+# run neovim with file if the first argument is not nil
 sub run_nvim {
-  if (@_[0]) {
-    system($nvim_cmd, shift);
+  if ($_[0]) {
+    system($nvim_cmd." ".shift);
   } else {
     system($nvim_cmd);
   }
 }
 
+# run the test
 sub run_test {
-  my $times = shift;
+  # get the params
+  my $n = shift;
   my $file = shift;
+
+  # initialize records
   my @records = ();
 
-  for ($i = 0; $i < $times; $i++) {
+  # test n times
+  for (my $i = 0; $i < $n; $i++) {
     if ($file) {
       run_nvim($file);
     } else {
       run_nvim();
     }
     my @scores = split_score(read_last_line());
-    my $score = @scores[0];
+    my $score = $scores[0];
     push @records, $score;
   }
 
-  my @records = sort @records;
+  # sort the array
+  my @sorted_records = sort @records;
+
+  # remove the smallest score
+  shift @sorted_records;
+  # remove the largest score
+  pop @sorted_records;
+
+  # calculate the average score
   my $sum = 0;
-  foreach $n (@records) {
+  foreach my $n (@sorted_records) {
     $sum = $sum + $n
   }
-  my $avg = $sum / @records;
-  print "Max: @records[-1] ms\n";
-  print "Min: @records[0] ms\n";
-  print "Avg: $avg ms\n";
+  my $avg = $sum / @sorted_records;
+
+
+  # return the hashmap in the below structure
+  # {
+  #   "max": 0.00,
+  #   "min": 0.00,
+  #   "avg": 0.00
+  # }
+  return (max => $sorted_records[-1],
+          min => $sorted_records[0],
+          avg => $avg);
 }
 
+# write the result
 sub write_result {
+  # open the $output_file
   open(fh, ">", $output_file);
+
+  # result format
   my $hint = << "EOF";
 BENCHMARK (TEST 10 Times) (Unit: millisecond)
 =============================================
@@ -68,6 +103,28 @@ Max elapse time: %.2f
 Min elapse time: %.2f
 Avg elapse time: %.2f
 EOF
-  print fh "$hint\n";
+
+  # get the input params
+  my ($ia, $ib, $ic) = @_;
+
+  # print the result to the file
+  printf fh $hint,
+    $ia->{max}, $ia->{min}, $ia->{avg},
+    $ib->{max}, $ib->{min}, $ib->{avg},
+    $ic->{max}, $ic->{min}, $ic->{avg};
+
+  # close the file
   close(fh);
 }
+
+# run empty buffer test
+my %buffer  = run_test(10);
+
+# run markdown file test
+my %md  = run_test(10, "README.md");
+
+# run lua file test
+my %lua  = run_test(10, "init.lua");
+
+# and finally write the result
+write_result(\%buffer, \%md, \%lua);
