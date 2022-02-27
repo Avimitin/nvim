ec252dfe2fa232cb945a75d53055f5a97171230b
Avimitin <avimitin@gmail.com>
2022-02-27 16:08:56 +0800

    [MSC] clog_gen: finish changelog generator
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 890168e0c3b1c1b968250219051dcf3173afd6a8 [formerly 890168e0c3b1c1b968250219051dcf3173afd6a8 [formerly a06d1972a9b555c4d8f7ba270ee792e3f0f38434]]
    Former-commit-id: d5cea94506e978c14e914dc2442eb418a5dfa20a
    Former-commit-id: 5cfb02d807fc464761455f65f71326a64a33d367

--- fixtures/clog_generate.pl
+++ fixtures/clog_generate.pl
@@ -48,6 +48,13 @@ sub get_log {
   return @output;
 }
 
+sub beautify {
+  my @in = shift;
+  foreach my $cmt (@in) {
+    print Dumper $cmt;
+  }
+}
+
 sub parse_log {
   my $in = shift;
 
@@ -59,6 +66,8 @@ sub parse_log {
   my @rc = ();
   # breaking change collector
   my @bc = ();
+  # normal commit format
+  my @other = ();
 
   for my $line (@$in) {
     if ($line =~ m/\[([A-Z!]{3})(!?)\] ([a-zA-z\/-_]+): (.*)/) {
@@ -68,14 +77,63 @@ sub parse_log {
         module => $3,
         summary => $4,
       );
-      print Dumper \%cmt;
+
+      if ($cmt{break}) {
+        push @bc, \%cmt;
+        next;
+      }
+
+      SWITCH:
+      for ($cmt{type}) {
+        if (/NEW|FET/) { push @nc, \%cmt; last SWITCH; }
+        if (/FIX/) { push @fc, \%cmt; last SWITCH;}
+        if (/RFT|RWT/) { push @rc, \%cmt; last SWITCH;}
+        if (/DOC|MSC/) { push @other, \%cmt; last SWITCH;}
+      }
     } else {
       print "WARN: commit not in valid format: $line\n";
     }
   }
+
+  return (
+    feat     => \@nc,
+    fix      => \@fc,
+    refactor => \@rc,
+    breaking => \@bc,
+    other    => \@other,
+  )
 }
 
+sub parse {
+  my $title = shift;
+  my (@inp) = @_;
+  # if there is nothing in the input
+  if ( !@inp ) {
+    return;
+  }
+
+  my $temp = "$title:\n";
+  $temp = $temp.'-' x (length($title) + 1);
+  $temp = $temp."\n";
+
+  foreach my $cmt (@inp) {
+    $temp = $temp."  * $cmt->{module}: $cmt->{summary}\n";
+  }
+
+  return $temp;
+}
 
 my %cliargs = parse_cmdline();
 my @output = get_log(\%cliargs);
-parse_log(\@output);
+my %result = parse_log(\@output);
+
+print parse("BREAKING CHANGES!", @ { $result{breaking} });
+print "\n";
+print parse("New Features", @ { $result{feature} });
+print "\n";
+print parse("Fix", @ { $result{fix} });
+print "\n";
+print parse("Changes", @ { $result{refactor} });
+print "\n";
+print parse("Others", @ { $result{other} });
+print "\n";
