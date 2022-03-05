58295deb685ba6a7b72dc0b875993907d6824080
Avimitin <avimitin@gmail.com>
2022-03-05 17:35:06 +0800

    [RFT!] plugins: move prehook to options.lua
    Currently the prehook function is only serving for setting option before
    plugins loading. I want to expose them to the top dir init.lua file.
    So options are now move to plugins.options directory.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: bd93218502c84df38ae37f95d15f054aa42eac8b [formerly bd93218502c84df38ae37f95d15f054aa42eac8b [formerly b91fec41558914159b49da8c4368680be3f86512]]
    Former-commit-id: d7fc91862a043600a7efa3bd0cb090006e23ea71
    Former-commit-id: a6e90c308d5b62d1946654a2d2f3f156e154b6bd

---
+++ lua/plugins/options.lua
@@ -0,0 +1,15 @@
+if not vim.fn.has("nvim-0.6") then
+  -- for filetype.nvim
+  -- If using a Neovim version earlier than 0.6.0
+  vim.g.did_load_filetypes = 1
+end
+
+-- for vsnip
+vim.g.vsnip_snippet_dir = vim.fn.expand("~/.config/nvim/vsnip")
+
+-- for wildfire
+vim.g.wildfire_objects = { "i'", 'i"', "i)", "i]", "i}", "ip", "it", "i`" }
+
+-- for vim-markdown
+vim.g.vim_markdown_conceal_code_blocks = 0
+vim.g.vim_markdown_strikethrough = 1
