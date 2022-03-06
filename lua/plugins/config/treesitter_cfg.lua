36526ed4cbfce286ab2fb1e02d853739555645e9
Avimitin <avimitin@gmail.com>
2022-03-06 15:18:50 +0800

    [NEW] plugins: extend text object by treesitter
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/config/treesitter_cfg.lua
+++ lua/plugins/config/treesitter_cfg.lua
@@ -26,4 +26,42 @@ require("nvim-treesitter.configs").setup({
   matchup = {
     enable = true,
   },
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+  },
 })
