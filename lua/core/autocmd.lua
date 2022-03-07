30ed4da16cebd588a9b780807996387270cfa0a1
Avimitin <avimitin@gmail.com>
2022-03-07 21:48:45 +0800

    [RFT] autocmd: move TermOpen command to autocmd.lua file
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: e53b6b357f6ae38f4ab8c786864b07663bfd0ddb [formerly e53b6b357f6ae38f4ab8c786864b07663bfd0ddb [formerly e80d81efa58350b5cc23a01a824fb24cfc3ff158]]
    Former-commit-id: 10d78f495d9a18c3d1b0d9eb5abba20e1ab17996
    Former-commit-id: fc1d79c55d307808cf663943319d215fbe24cd30

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -2,4 +2,8 @@
 vim.cmd([[ au InsertEnter * set norelativenumber ]])
 vim.cmd([[ au InsertLeave * set relativenumber ]])
 
+-- auto compile when editing the load.lua file
 vim.cmd([[autocmd BufWritePost load.lua source <afile> | PackerCompile]])
+
+-- start insert when enter the terminal
+vim.cmd("autocmd TermOpen term://* startinsert")
