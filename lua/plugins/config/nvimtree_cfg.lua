29ca6e38d49344eb1d1ee1f1c523fdae47a47404
Avimitin <avimitin@gmail.com>
2022-03-31 16:34:02 +0800

    F! nvim-tree: remove outdated options
    Nvim-tree is no more support auto_close functionality.
    
    Ref:
    https://github.com/kyazdani42/nvim-tree.lua/commit/524758a207f9c5bf3888b446d9f93192a837b8a7
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/config/nvimtree_cfg.lua
+++ lua/plugins/config/nvimtree_cfg.lua
@@ -48,7 +48,6 @@ require("nvim-tree").setup({
   hijack_netrw = true,
   open_on_setup = false,
   ignore_ft_on_setup = { "startify", "dashboard" },
-  auto_close = true,
   open_on_tab = false,
   hijack_cursor = true,
   update_cwd = true,
