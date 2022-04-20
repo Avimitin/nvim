33b38da0b165bfbdf9f83a8c3cd5b5ca6ce49ab4
Avimitin <avimitin@gmail.com>
2022-04-20 15:47:35 +0800

    N. keys: add more description for keys
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: f574f764a139ecd8f3f7518c5a6d64ad5b4ce10f [formerly f574f764a139ecd8f3f7518c5a6d64ad5b4ce10f [formerly 8a6372469137e9228724a0fb2806e6c35edea1b8]]
    Former-commit-id: 07c2f83934e8e6dbb93367927f7a7100f23984a2
    Former-commit-id: 56437890ecb38c583d687aa17574057e6e66cda4

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -35,34 +35,34 @@ xmap("<", "<gv", d("Increase indent"))
 xmap(">", ">gv", d("Decrease indent"))
 
 -- create tab like window
-nmap("<C-T>h", ":tabprevious<CR>")
-nmap("<C-T>l", ":tabnext<CR>")
-nmap("<C-T>n", ":tabnew<CR>")
+nmap("<C-T>h", ":tabprevious<CR>", d("Goto previous tab"))
+nmap("<C-T>l", ":tabnext<CR>", d("Goto next tab"))
+nmap("<C-T>n", ":tabnew<CR>", d("Create a new tab"))
 
 -- save quickly
 nmap(";w", ":w<CR>", d("Save buffer"))
 
 -- do thing like ctrl c and ctrl v
-xmap("<C-y>", [["+y]])
-nmap("<C-p>", [["+p]])
-map("i", "<C-p>", [[<ESC>"+pa]])
+xmap("<C-y>", [["+y]], d("Copy to system clipboard"))
+nmap("<C-p>", [["+p]], d("Paste from system clipboard"))
+map("i", "<C-p>", [[<ESC>"+pa]], d("Paste from system clipboard"))
 
 -- shut down the search high light
-nmap("<ESC>", ":nohlsearch<CR>")
+nmap("<ESC>", ":nohlsearch<CR>", d("Close search highlight"))
 -- no more finger expansion
 map("i", "<A-;>", "<ESC>", d("Exit the insert mode"))
 
 -- move around the window
-nmap(";k", "<C-w>k")
-nmap(";j", "<C-w>j")
-nmap(";l", "<C-w>l")
-nmap(";h", "<C-w>h")
+nmap(";k", "<C-w>k", d("Jump to window above"))
+nmap(";j", "<C-w>j", d("Jump to window below"))
+nmap(";l", "<C-w>l", d("Jump to the left window"))
+nmap(";h", "<C-w>h", d("Jump to the righ window"))
 
 -- resize the window
-nmap("<C-S-up>", ":res +5<CR>")
-nmap("<C-S-down>", ":res -5<CR>")
-nmap("<C-S-right>", ":vertical resize-5<CR>")
-nmap("<C-S-left>", ":vertical resize+5<CR>")
+nmap("<C-S-up>", ":res +5<CR>", d("Extend the upper boundary of the current window"))
+nmap("<C-S-down>", ":res -5<CR>", d("Extend the lower boundary of the current window"))
+nmap("<C-S-right>", ":vertical resize-5<CR>", d("Extend the right boundary of the current window") )
+nmap("<C-S-left>", ":vertical resize+5<CR>", d("Extend the right boundary of the current window"))
 
 -- center editing line
-map("i", "<C-c>", "<ESC>zzi")
+map("i", "<C-c>", "<ESC>zzi", d("Place the current line to the middle of the screen"))
