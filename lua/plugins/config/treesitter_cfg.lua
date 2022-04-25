2bbb86138740b68db2bafb198ece119a86cb8546
Avimitin <avimitin@gmail.com>
2022-04-25 08:51:06 +0800

    R! treesitter: remove `move` settings
    Those keymap are inefficient. I barely use them since I make those
    settings.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/config/treesitter_cfg.lua
+++ lua/plugins/config/treesitter_cfg.lua
@@ -25,25 +25,5 @@ require("nvim-treesitter.configs").setup({
         ["il"] = "@call.inner",
       },
     },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
-    },
   },
 })
