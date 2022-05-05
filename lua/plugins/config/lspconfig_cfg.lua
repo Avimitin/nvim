c8f5741bcaf1d238473e03aa3bfb7e92c152dc4c
Avimitin <avimitin@gmail.com>
2022-05-05 11:45:18 +0800

    M. format: fix linting
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/config/lspconfig_cfg.lua
+++ lua/plugins/config/lspconfig_cfg.lua
@@ -110,20 +110,20 @@ end
 -- provide analyse, completion for neovim runtime file
 -- @return table
 local function neovim_lua_setting()
-  local runtime_path = vim.split(package.path, ';')
+  local runtime_path = vim.split(package.path, ";")
   table.insert(runtime_path, "lua/?.lua")
   table.insert(runtime_path, "lua/?/init.lua")
   return {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
+        version = "LuaJIT",
         -- Setup your lua path
         path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { "vim" },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
