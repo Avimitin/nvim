c42ed597916e52c848b2306c2ce119176e015b80
Avimitin <avimitin@gmail.com>
2022-05-06 11:35:27 +0800

    N. autocmd: add fcitx5 auto switcher
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: a8f0b12c15a04e11d5ad2939761977ac2b5c2513 [formerly a8f0b12c15a04e11d5ad2939761977ac2b5c2513 [formerly 6e0495d93a5ac1bcd50ea6941b742b0b0b67d85e]]
    Former-commit-id: 558cf849aa349568a4c20e6678ab4bc421e18558
    Former-commit-id: e1a0e2a3c0e1bda02b3dcb82e8e3be005be8c870

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -16,3 +16,46 @@ au("TextYankPost", {
     vim.highlight.on_yank({ higroup = "HighLightLineMatches", timeout = 200 })
   end,
 })
+
+-- Automatically close and activate fcitx5
+-- Enable this feature in `~/.config/lua/custom.lua` file
+-- If you don't have one, just create it.
+local ok, custom = pcall(require, "custom")
+if not ok or not custom.has_fcitx5 then
+  return
+end
+
+-- In archlinux, fcitx5-remote is contained in fcitx5 packages.
+-- If fcitx5-remote executable is not found on your machine, remove this autocommand.
+--
+-- This autocommand will not always activate fcitx5 in insert mode.
+-- My approach is to activate fcitx5 only when fcitx5 is manually triggered last
+-- time in insert mode.
+
+au({ "InsertLeave" }, {
+  callback = function()
+    -- If the fcitx5 is manually activated in insert mode
+    vim.g.fcitx5_is_active = vim.fn.system("fcitx5-remote"):gsub("%s+", "") == "2"
+
+    -- if fcitx5 is active, we close it when we leave insert mode
+    if vim.g.fcitx5_is_active then
+      vim.fn.system("fcitx5-remote -c")
+
+      -- when we inactive the fcitx5 in autocommand, we might want to reactive it
+      -- in insert mode. This can help InsertEnter autocommand know the fcitx5 is
+      -- close by neovim, not user.
+      vim.g.fcitx5_should_reactivate = true
+      vim.g.fcitx5_is_active = false
+    end
+  end,
+})
+
+au({ "InsertEnter" }, {
+  callback = function()
+    if vim.g.fcitx5_should_reactivate then
+      vim.fn.system("fcitx5-remote -o")
+
+      vim.g.fcitx5_should_reactivate = false
+    end
+  end,
+})
