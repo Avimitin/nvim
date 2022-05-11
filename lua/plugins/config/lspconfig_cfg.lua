02d14c9a5965c4c5afe2b86147492d2562b05a78
Avimitin <avimitin@gmail.com>
2022-05-11 16:01:14 +0800

    M. lspconfig: increase code readability
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/config/lspconfig_cfg.lua
+++ lua/plugins/config/lspconfig_cfg.lua
@@ -8,7 +8,7 @@ if not (present1 or present2) then
 end
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
+  local function bmap(...)
     vim.api.nvim_buf_set_keymap(bufnr, ...)
   end
 
@@ -24,53 +24,43 @@ local on_attach = function(client, bufnr)
     noremap = true,
     silent = true,
   }
-  buf_set_keymap("n", "gd", "<Cmd>Lspsaga preview_definition<CR>", opts)
-  buf_set_keymap("n", "gh", "<Cmd>Lspsaga hover_doc<CR>", opts)
-  buf_set_keymap(
-    "n",
-    "<C-u>",
-    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
-    opts
-  )
-  buf_set_keymap(
-    "n",
-    "<C-d>",
-    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>",
-    opts
-  )
-  buf_set_keymap("n", "gs", "<cmd>Lspsaga signature_help<CR>", opts)
-  buf_set_keymap("n", "go", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-  buf_set_keymap("n", "gj", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-  buf_set_keymap("n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
-  buf_set_keymap("n", "ga", "<cmd>Lspsaga code_action<CR>", opts)
+  bmap("n", "gd", "<Cmd>Lspsaga preview_definition<CR>", opts)
+  bmap("n", "gh", "<Cmd>Lspsaga hover_doc<CR>", opts)
+  bmap("n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>", opts)
+  bmap("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>", opts)
+  bmap("n", "gs", "<cmd>Lspsaga signature_help<CR>", opts)
+  bmap("n", "go", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+  bmap("n", "gj", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+  bmap("n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+  bmap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
+  bmap("n", "ga", "<cmd>Lspsaga code_action<CR>", opts)
 
-  buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gm", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("n", "gq", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
+  bmap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  bmap("n", "gm", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  bmap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  bmap("n", "gq", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
 
   -- add rust specific keymappings
   if client.name == "rust_analyzer" then
-    buf_set_keymap("n", "<leader>rr", "<cmd>RustRunnables<CR>", opts)
-    buf_set_keymap("n", "<leader>ra", "<cmd>RustHoverAction<CR>", opts)
+    bmap("n", "<leader>rr", "<cmd>RustRunnables<CR>", opts)
+    bmap("n", "<leader>ra", "<cmd>RustHoverAction<CR>", opts)
   end
 
   -- Set some keybinds conditional on server capabilities
   -- 0.8.0
   if vim.fn.has("nvim-0.8.0") then
     if client.server_capabilities.documentFormattingProvider then
-      buf_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
+      bmap("n", "gf", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
     elseif client.server_capabilities.documentRangeFormattingProvider then
-      buf_set_keymap("x", "gf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+      bmap("x", "gf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 
     -- 0.6.0 - 0.7.0
   else
     if client.resolved_capabilities.document_formatting then
-      buf_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+      bmap("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     elseif client.resolved_capabilities.document_range_formatting then
-      buf_set_keymap("x", "gf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+      bmap("x", "gf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
   end
 end
