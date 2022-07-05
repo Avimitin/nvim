4eb90476054ab9f6a331c4459e9042947dbb21ee
Avimitin <avimitin@gmail.com>
2022-07-05 12:01:54 +0800

    R. git: move config function to single file
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 33dc835d76432bed936cb4efb759ef6547b7f2a7 [formerly 33dc835d76432bed936cb4efb759ef6547b7f2a7 [formerly 568831fbf4beb24ab2e8f1d24c5f2c2373983243]]
    Former-commit-id: e47579bae783ee4c2b2f80dd77fdb52c095c507f
    Former-commit-id: 5ff72cba7b402b5158aff4d76d3ca46046006d21

--- lua/plugins/modules/git/init.lua
+++ lua/plugins/modules/git/init.lua
@@ -1,4 +1,7 @@
 local register = require("plugins").register
+local config = require("plugins.modules.git.config")
+
+config.pre()
 
 local repos = {
   -- A git tool like magit in Emacs
@@ -16,21 +19,6 @@ local repos = {
   -- Call the lazygit inside neovim, relies on lazygit executable
   {
     "kdheepak/lazygit.nvim",
-    setup = function()
-      vim.g.lazygit_floating_window_winblend = 0
-      vim.g.lazygit_floating_window_scaling_factor = 1
-      vim.g.lazygit_floating_window_corner_chars = {
-        "╭",
-        "╮",
-        "╰",
-        "╯",
-      }
-      vim.g.lazygit_floating_window_use_plenary = 0
-      vim.g.lazygit_use_neovim_remote = 1
-      if vim.g.lazygit_use_neovim_remote == 1 and vim.fn.executable("nvr") then
-        vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
-      end
-    end,
     cmd = "LazyGit",
   },
 
@@ -41,85 +29,7 @@ local repos = {
       "nvim-lua/plenary.nvim",
     },
     event = "BufRead",
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add = {
-            hl = "GitSignsAdd",
-            text = "▎",
-            numhl = "GitSignsAddNr",
-            linehl = "GitSignsAddLn",
-          },
-          change = {
-            hl = "GitSignsChange",
-            text = "░",
-            numhl = "GitSignsChangeNr",
-            linehl = "GitSignsChangeLn",
-          },
-          delete = {
-            hl = "GitSignsDelete",
-            text = "_",
-            numhl = "GitSignsDeleteNr",
-            linehl = "GitSignsDeleteLn",
-          },
-          topdelete = {
-            hl = "GitSignsDelete",
-            text = "‾",
-            numhl = "GitSignsDeleteNr",
-            linehl = "GitSignsDeleteLn",
-          },
-          changedelete = {
-            hl = "GitSignsChange",
-            text = "▒",
-            numhl = "GitSignsChangeNr",
-            linehl = "GitSignsChangeLn",
-          },
-        },
-        on_attach = function(bufnr)
-          local function map(mode, lhs, rhs, opts)
-            opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
-            vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
-          end
-
-          -- Navigation
-          map("n", "gij", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-          map("n", "gik", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
-
-          -- Actions
-          map("n", "gis", ":Gitsigns stage_hunk<CR>")
-          map("v", "gis", ":Gitsigns stage_hunk<CR>")
-          map("n", "gir", ":Gitsigns reset_hunk<CR>")
-          map("v", "gir", ":Gitsigns reset_hunk<CR>")
-          map("n", "giS", "<cmd>Gitsigns stage_buffer<CR>")
-          map("n", "giu", "<cmd>Gitsigns undo_stage_hunk<CR>")
-          map("n", "giR", "<cmd>Gitsigns reset_buffer<CR>")
-          map("n", "gip", "<cmd>Gitsigns preview_hunk<CR>")
-          map("n", "giB", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-          map("n", "gib", "<cmd>Gitsigns toggle_current_line_blame<CR>")
-          map("n", "gid", "<cmd>Gitsigns diffthis<CR>")
-          map("n", "giD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-          map("n", "gih", "<cmd>Gitsigns toggle_deleted<CR>")
-
-          -- Text object
-          map("o", "ih", ":<C-U>Gitsigns select_hunk<CR>")
-          map("x", "ih", ":<C-U>Gitsigns select_hunk<CR>")
-        end,
-        numhl = true,
-        linehl = false,
-        watch_gitdir = { interval = 1000, follow_files = true },
-        current_line_blame = false,
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
-        },
-        sign_priority = 6,
-        update_debounce = 100,
-        status_formatter = nil, -- Use default
-        word_diff = false,
-        diff_opts = { internal = true },
-      })
-    end,
+    config = config.gitsigns_config,
   },
 
   -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
