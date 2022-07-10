fa10e670ce81f67ad6a1e5a27c1e64bcc2e3a13d
Avimitin <avimitin@gmail.com>
2022-07-10 17:09:45 +0800

    colorscheme: move colorscheme into sub-directory
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 9c1309dfe6c04e151b2bba711688c171b6ca5e91 [formerly 9c1309dfe6c04e151b2bba711688c171b6ca5e91 [formerly f503720b87f794b34acb8b5aad99aadde2b368b8]]
    Former-commit-id: c9a917fe791421ea4d24a74d2c2684960a7a80d7
    Former-commit-id: 22ee291f6918a88a6801d2e26033bc212e130f42

---
+++ lua/plugins/colorscheme/repos.lua
@@ -0,0 +1,47 @@
+local colorscheme = {
+  -- dark green color scheme
+  {
+    "Avimitin/neovim-deus",
+    cond = function()
+      return require("plugins.colorscheme.config").theme == "deus"
+    end,
+    config = function()
+      require("plugins.colorscheme.config").deus_setup()
+    end,
+  },
+
+  -- dark purple color scheme
+  {
+    "rebelot/kanagawa.nvim",
+    cond = function()
+      return require("plugins.colorscheme.config").theme == "kanagawa"
+    end,
+    config = function()
+      require("plugins.colorscheme.config").kanagawa_setup()
+    end,
+  },
+
+  -- GitHub light and dark colorscheme
+  {
+    "projekt0n/github-nvim-theme",
+    cond = function()
+      local select = require("plugins.colorscheme.config").theme
+      for _, avail in ipairs({
+        "github_dark",
+        "github_dimmed",
+        "github_light",
+        "github_light_default",
+      }) do
+        if select == avail then
+          return true
+        end
+      end
+      return false
+    end,
+    config = function()
+      require("plugins.colorscheme.config").github_setup()
+    end,
+  },
+}
+
+return colorscheme
