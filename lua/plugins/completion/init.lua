e10dcc50be07d9e60ba58821e03ac486d45c9b02
Avimitin <avimitin@gmail.com>
2022-07-14 11:44:01 +0800

    cmp: fix completion directory not set issue
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -1,3 +1,6 @@
+-- load config that should be initialize before plugins
+require("plugins.completion.config").pre()
+
 -- register plugin repos
 local repo = require("plugins.completion.repos")
 require("plugins").register(repo)
