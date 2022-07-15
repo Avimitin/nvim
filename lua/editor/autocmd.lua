bbdaf6c0aa723c19f23af6eac7d443c8a1a591f1
Avimitin <avimitin@gmail.com>
2022-07-15 12:58:34 +0800

    autocmd: add async fcitx5 toggle implementation
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 4d013c5e0526c7196a755cedc8d445e931038a90 [formerly 4d013c5e0526c7196a755cedc8d445e931038a90 [formerly 8210180f3b6d9584d1c18260e2eb5dc22d21b620]]
    Former-commit-id: be2f5b14a1bd69e81fed1803d3aa9e3e1756b365
    Former-commit-id: 7d36b41df5eb9affd9b9ffe65047b1be60d0a5d2

--- lua/editor/autocmd.lua
+++ lua/editor/autocmd.lua
@@ -23,45 +23,119 @@ au("TextYankPost", {
   end,
 }) ]]
 
--- Automatically close and activate fcitx5
--- Enable this feature in `~/.config/lua/custom.lua` file
--- If you don't have one, just create it.
+--
+-- [fcitx5 auto toggle]
+--
+-- In archlinux, fcitx5-remote is already contained in fcitx5 packages.
+-- If fcitx5-remote executable is not found on your machine, try install one.
+--
+-- This autocommand will not always activate fcitx5 in insert mode.
+-- My approach is to activate fcitx5 only when fcitx5 is manually triggered last
+-- time in insert mode.
+--
+-- You can disable this feature in `~/.config/lua/custom.lua` file
+--
 local ok, custom = pcall(require, "custom")
 if not ok or not custom.has_fcitx5 then
   return
 end
 
--- In archlinux, fcitx5-remote is contained in fcitx5 packages.
--- If fcitx5-remote executable is not found on your machine, remove this autocommand.
---
--- This autocommand will not always activate fcitx5 in insert mode.
--- My approach is to activate fcitx5 only when fcitx5 is manually triggered last
--- time in insert mode.
+local function async_close_fcitx5()
+  local task = require("plenary.job")
+  task
+    :new({
+      command = "fcitx5-remote",
+      args = { "-c" },
+      on_exit = function(_, ret)
+        if ret ~= 0 then
+          vim.notify("fail to close fcitx5")
+          return
+        end
+
+        vim.g.fcitx5_should_reactivate = true
+      end,
+    })
+    :start()
+end
+
+local function async_toggle_fcitx5()
+  local task = require("plenary.job")
+  task
+    :new({
+      command = "fcitx5-remote",
+      on_exit = function(j, ret)
+        if ret ~= 0 then
+          vim.notify("Fail to execute fcitx5-remote command")
+          return
+        end
+
+        local stdout = j:result()
+        -- Grievance: why Lua array index start with 1?
+        if #stdout > 0 and stdout[1] == "2" then
+          async_close_fcitx5()
+        end
+      end,
+    })
+    :start()
+end
+
+local function async_open_fcitx5()
+  local task = require("plenary.job")
+  task
+    :new({
+      command = "fcitx5-remote",
+      args = { "-o" },
+      on_exit = function(_, ret)
+        if ret ~= 0 then
+          vim.notify("fail to open fcitx5")
+          return
+        end
+
+        vim.g.fcitx5_should_reactivate = false
+      end,
+    })
+    :start()
+end
 
 au({ "InsertLeave" }, {
   callback = function()
+    -- plenary is a wrapper for libuv API, it can help us asynchronously execute commands.
+    -- But it is not included in Neovim API, so we have to make sure that it **does** exist.
+    local has_plenary, _ = pcall(require, "plenary")
+
+    if has_plenary then
+      async_toggle_fcitx5()
+      return
+    end
+
+    --
+    -- use neovim API as a fallback solution
+    --
     -- If the fcitx5 is manually activated in insert mode
-    vim.g.fcitx5_is_active = vim.fn.system("fcitx5-remote"):gsub("%s+", "") == "2"
+    local is_active = vim.fn.system("fcitx5-remote"):gsub("%s+", "") == "2"
 
-    -- if fcitx5 is active, we close it when we leave insert mode
-    if vim.g.fcitx5_is_active then
+    if is_active then
       vim.fn.system("fcitx5-remote -c")
-
-      -- when we inactive the fcitx5 in autocommand, we might want to reactive it
-      -- in insert mode. This can help InsertEnter autocommand know the fcitx5 is
-      -- close by neovim, not user.
       vim.g.fcitx5_should_reactivate = true
-      vim.g.fcitx5_is_active = false
     end
   end,
 })
 
 au({ "InsertEnter" }, {
   callback = function()
-    if vim.g.fcitx5_should_reactivate then
-      vim.fn.system("fcitx5-remote -o")
-
-      vim.g.fcitx5_should_reactivate = false
+    if not vim.g.fcitx5_should_reactivate then
+      return
     end
+
+    -- use plenary as primary solution
+    local has_plenary, _ = pcall(require, "plenary")
+    if has_plenary then
+      async_open_fcitx5()
+      return
+    end
+
+    -- use neovim API as a fallback solution
+    vim.fn.system("fcitx5-remote -o")
+    vim.g.fcitx5_should_reactivate = false
   end,
 })
