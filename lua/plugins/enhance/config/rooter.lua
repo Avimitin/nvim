c67c275f73800c27063290bb5530d423b9b6de40
Avimitin <avimitin@gmail.com>
2022-07-15 20:27:33 +0800

    nvim-rooter: add bootstrap guard and cwd change guard
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: ae0b276c0c2dfd15b4f694e9027b35f0f00c87e3 [formerly ae0b276c0c2dfd15b4f694e9027b35f0f00c87e3 [formerly 2b515fb5d3e87c7723a0e477c1ba514ee9e4f86e]]
    Former-commit-id: 49660b22ad28d10ed46c3c0e34db6d599916a66a
    Former-commit-id: 32d6f94949e186a3e09a6bf946a08a7ac2247b1d

--- lua/plugins/enhance/config/rooter.lua
+++ lua/plugins/enhance/config/rooter.lua
@@ -1,12 +1,25 @@
+-- nvim-rooter is not present when user first install the configuration
+local has_rooter, rooter = pcall(require, "nvim-rooter")
+if not has_rooter then
+  return
+end
+
 local opts = {
   rooter_patterns = { ".git", ".hg", ".svn", "Cargo.toml", "go.mod", "package.json" },
   -- Trigger manually
   manual = false,
 }
 
-require("nvim-rooter").setup(opts)
+local uv = vim.loop
 
+local old_cwd = uv.cwd()
+
+rooter.setup(opts)
 -- trigger it once
-require("nvim-rooter").rooter()
+rooter.rooter()
 
-vim.notify("Dir Change: " .. vim.fn.getcwd())
+local new_cwd = uv.cwd()
+
+if new_cwd ~= old_cwd then
+  vim.notify("Dir Change: " .. new_cwd)
+end
