9d15d06e64af48fb9f319873cbfb999e2cc49801
Avimitin <avimitin@gmail.com>
2022-07-16 15:34:19 +0800

    scripts: add nix-os like install prompt
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,11 +1,37 @@
 #!/bin/bash
 
+# exit when any of the command fail
+set -e
+
+ok_or_exit ()
+{
+  if [[ "$1" != "y" && "$1" != "Y" ]]; then
+    echo
+    echo "exit"
+    exit 0
+  fi
+}
+
+# Create tmp directory
+read -p "I am going to create a directory under /tmp directory.
+This directory is used for storing temparary download files.
+
+Do you agree? [y/N] " confirm && ok_or_exit $confirm
+
 temp_download_dir=$(mktemp -d -t 'NVIM_CFG_DOWNLOAD_DIR_XXX')
+echo
+echo "* tmpdir: $temp_download_dir"
+echo
+
 
 # The "latest" tag will be tagged multiple time
 url="https://github.com/Avimitin/nvim/archive/refs/tags/latest.tar.gz"
+read -p "I am going to download the neovim source file from $url
+
+Do you agree? [y/N]" confirm && ok_or_exit $confirm
 
 # download the files
+echo
 echo "Downloading..."
 curl -sSL $url | tar xz -C $temp_download_dir
 
@@ -17,5 +43,13 @@ if [[ -d "$target_dir" ]]; then
   mv -r $target_dir "$HOME/.config/nvim.backup-$(date +%Y%m%d-%H-%M)"
 fi
 
+read -p "I am going to install the neovim configuration to $target_dir.
+
+Do you agree? [y/N]" confirm && ok_or_exit $confirm
+
+echo
+mkdir -p $target_dir
 # move the downloaded file to the .config/nvim directory
 mv -f $temp_download_dir/nvim-latest $target_dir
+
+echo "Process done!"
