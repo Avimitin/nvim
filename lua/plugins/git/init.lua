80663d4c5691acd427d024f1bafbd1269b449392
Avimitin <avimitin@gmail.com>
2022-07-26 12:45:26 +0800

    !git: clean up configuration
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -1,9 +1,6 @@
 -- initialize key mappings
 require("plugins.git.keymap")
 
--- initialize git configuration
-require("plugins.git.config").pre()
-
 -- register plugin repos
 local repos = require("plugins.git.repos")
 require("plugins").register(repos)
