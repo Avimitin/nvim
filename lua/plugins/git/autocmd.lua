7ceb9687d0e91f451c175524cdc49cfa582a20c2
Avimitin <avimitin@gmail.com>
2022-07-26 14:00:44 +0800

    git: add auto command to load staged change when commiting
    Signed-off-by: Avimitin <avimitin@gmail.com>

---
+++ lua/plugins/git/autocmd.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+local function setup_auto_diff()
+  local au = vim.api.nvim_create_autocmd
+  au("FileType", {
+    pattern = "gitcommit",
+    callback = function(opts)
+      if opts == nil then
+        return
+      end
+
+      local win_width = vim.fn.winwidth("%")
+      if win_width < 160 then
+        return
+      end
+
+      vim.cmd("vertical Git diff --staged")
+      vim.cmd("setlocal filetype=git")
+      vim.cmd("wincmd h")
+    end,
+  })
+end
+
+M.setup = function()
+  setup_auto_diff()
+end
+
+return M
