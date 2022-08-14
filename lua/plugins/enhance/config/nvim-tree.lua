a6ee993053b61a7a126b52c916d2149853ead3ff
Avimitin <avimitin@gmail.com>
2022-08-14 23:33:47 +0800

    nvim-tree: turn to default key mappings
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: fcbcf9d86ff8e2055faf390951afe6389319038f [formerly fcbcf9d86ff8e2055faf390951afe6389319038f [formerly b7627cb27fb37d68d1323af4463a1a96eb802429]]
    Former-commit-id: f83b95636db05c613b7a6546bbb9b830dd7d16fa
    Former-commit-id: 3b23bf672ccdba94fea40493285576423c01b2b9

--- lua/plugins/enhance/config/nvim-tree.lua
+++ lua/plugins/enhance/config/nvim-tree.lua
@@ -5,8 +5,6 @@ return function()
     return
   end
 
-  local tree_cb = tree_c.nvim_tree_callback
-
   -- following options are the default
   require("nvim-tree").setup({
     disable_netrw = true,
@@ -103,40 +101,9 @@ return function()
       -- height = 30,
       side = "left",
       mappings = {
-        custom_only = true,
+        custom_only = false,
         list = {
-          { key = { "<CR>", "<2-LeftMouse>" }, cb = tree_cb("edit") },
-          { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
-          { key = "<C-v>", cb = tree_cb("vsplit") },
-          { key = "<C-x>", cb = tree_cb("split") },
-          { key = "<C-t>", cb = tree_cb("tabnew") },
-          { key = "<", cb = tree_cb("prev_sibling") },
-          { key = ">", cb = tree_cb("next_sibling") },
-          { key = "P", cb = tree_cb("parent_node") },
-          { key = "<BS>", cb = tree_cb("close_node") },
-          { key = "<S-CR>", cb = tree_cb("close_node") },
-          { key = "<Tab>", cb = tree_cb("preview") },
-          { key = "K", cb = tree_cb("first_sibling") },
-          { key = "J", cb = tree_cb("last_sibling") },
-          { key = "I", cb = tree_cb("toggle_git_ignored") },
-          { key = "H", cb = tree_cb("toggle_dotfiles") },
-          { key = "R", cb = tree_cb("refresh") },
-          { key = "a", cb = tree_cb("create") },
-          { key = "d", cb = tree_cb("remove") },
-          { key = "r", cb = tree_cb("rename") },
-          { key = "<C-r>", cb = tree_cb("full_rename") },
-          { key = "x", cb = tree_cb("cut") },
-          { key = "c", cb = tree_cb("copy") },
-          { key = "p", cb = tree_cb("paste") },
-          { key = "y", cb = tree_cb("copy_name") },
-          { key = "Y", cb = tree_cb("copy_path") },
-          { key = "gy", cb = tree_cb("copy_absolute_path") },
-          { key = "[c", cb = tree_cb("prev_git_item") },
-          { key = "]c", cb = tree_cb("next_git_item") },
-          { key = "-", cb = tree_cb("dir_up") },
-          { key = "o", cb = tree_cb("system_open") },
-          { key = "q", cb = tree_cb("close") },
-          { key = "?", cb = tree_cb("toggle_help") },
+          { key = "?", action = "toggle_help" },
         },
       },
     },
