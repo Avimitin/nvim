74a66a81dfcddf85022e74a62fe85d0315d5c77a
Avimitin <avimitin@gmail.com>
2022-08-23 12:54:35 +0800

    plugin: add dial.nvim to inc/decrement number/date/boolean
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/enhance/keymap.lua
+++ lua/plugins/enhance/keymap.lua
@@ -117,3 +117,31 @@ nmap(";d", ":Dispatch ", { noremap = true, silent = false })
 -- vim-matchup
 --
 map({ "n", "x", "o" }, ",", "<Plug>(matchup-%)")
+
+--
+-- dial.nvim
+--
+local function _cmd_sequence(direction, mode)
+  local function cmd(body)
+    local cmd_sequences = "<Cmd>"
+    local cr_sequences = "<CR>"
+    return cmd_sequences .. body .. cr_sequences
+  end
+
+  local function if_expr(cond, branch_true, branch_false)
+    if cond then
+      return branch_true
+    end
+    return branch_false
+  end
+
+  local select = cmd([[lua require"dial.command".select_augend_]] .. mode .. "()")
+  local setopfunc = cmd([[let &opfunc="dial#operator#]] .. direction .. "_" .. mode .. [["]])
+  local textobj = if_expr(mode == "normal", cmd([[lua require("dial.command").textobj()]]), "")
+  return select .. setopfunc .. "g@" .. textobj
+end
+
+nmap("=", _cmd_sequence("increment", "normal"))
+nmap("-", _cmd_sequence("decrement", "normal"))
+map("v", "=", _cmd_sequence("increment", "visual"))
+map("v", "-", _cmd_sequence("decrement", "visual"))
