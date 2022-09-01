686e01c0c732d3c9111a0aef491f351aebd07b86
Avimitin <avimitin@gmail.com>
2022-09-01 14:41:14 +0800

    Revert "lspsaga: close winbar"
    This reverts commit 49b1d79b172c23bb781f5c2c7401802b0b0a019a.

--- lua/plugins/coding/config/init.lua
+++ lua/plugins/coding/config/init.lua
@@ -205,6 +205,7 @@ config.lspconfig_ft = lspconfig_ft()
 -- lspsaga
 --
 config.lspsaga_config = function()
+  local enable_winbar = require("editor.utils").vhas("nvim-0.8.0")
   local saga = require("lspsaga")
   local themes = require("lspsaga.lspkind")
   themes[12][2] = " "
@@ -244,7 +245,7 @@ config.lspsaga_config = function()
     -- close it until neovim 0.8.0 become stable
     symbol_in_winbar = {
       in_custom = false,
-      enable = false,
+      enable = enable_winbar,
       separator = "  ",
       show_file = false,
       click_support = false,
