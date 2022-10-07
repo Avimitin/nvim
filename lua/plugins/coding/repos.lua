f8de9aabcee06ccde88d9d85cd0b9d51a7f643d5
Avimitin <avimitin@gmail.com>
2022-10-07 15:21:57 +0800

    lsp: dim unused variable
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/coding/repos.lua
+++ lua/plugins/coding/repos.lua
@@ -133,6 +133,26 @@ local repos = {
     end,
     module = "lsp_lines",
   },
+
+  {
+    "zbirenbaum/neodim",
+    event = "LspAttach",
+    config = function()
+      require("neodim").setup({
+        alpha = 0.7,
+        blend_color = "#000000",
+        update_in_insert = {
+          enable = false,
+          delay = 100,
+        },
+        hide = {
+          virtual_text = false,
+          signs = true,
+          underline = true,
+        },
+      })
+    end,
+  },
 }
 
 return repos
