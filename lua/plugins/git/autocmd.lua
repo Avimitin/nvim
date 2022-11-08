751401c41fc238e19420c6e739de2c65e25cd966
Avimitin <avimitin@gmail.com>
2022-11-08 10:38:54 +0800

    git: fix the messy diff preview auto command
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: df43900358ba649f66545c8101b8050220e712bd [formerly df43900358ba649f66545c8101b8050220e712bd [formerly 43cd63a0272ea902ecbdbe0c0e23b14f5e912bd5]]
    Former-commit-id: ec6b423d6c1c0492ea4b1a1bffed69d94af46c3d
    Former-commit-id: 84877281d16246c94be5f9720677635b345af09d

--- lua/plugins/git/autocmd.lua
+++ lua/plugins/git/autocmd.lua
@@ -16,7 +16,17 @@ local function setup_auto_diff()
 
       vim.cmd("vertical Git diff --staged")
       vim.cmd("setlocal filetype=git")
-      vim.cmd("wincmd h")
+      local win_id = vim.api.nvim_get_current_win()
+      local buf_id = vim.api.nvim_win_get_buf(win_id)
+      vim.api.nvim_create_autocmd("BufWinLeave", {
+        buffer = opts.buf,
+        callback = function()
+          local id = win_id
+          vim.api.nvim_buf_delete(buf_id, { force = true })
+          vim.api.nvim_win_close(id, true)
+        end
+      })
+      vim.api.nvim_set_current_win(win_id)
     end,
   })
 end
