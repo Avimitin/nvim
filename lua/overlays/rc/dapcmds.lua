544b411fe8666bf4ae49b45971b906b0f5b92f57
Avimitin <avimitin@gmail.com>
2022-12-06 17:32:15 +0800

    hydra: setup hydra for dap
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: a1269ecf43b216fd9de8072bd35ea249fb4c93c3 [formerly a1269ecf43b216fd9de8072bd35ea249fb4c93c3 [formerly 24103de193808f2adf79136ceba02e78d71bf33f]]
    Former-commit-id: 29e301bd1e70feedda395ed926f39f392122f75a
    Former-commit-id: 19c0f70f0b9a058061974d2312028dcd7e12816d

--- lua/overlays/rc/dapcmds.lua
+++ lua/overlays/rc/dapcmds.lua
@@ -1,30 +1,41 @@
-local alias = require("editor.utils").alias
+local alias = function(cmd, exec, opt)
+  local o = opt or {}
+  vim.api.nvim_create_user_command(cmd, exec, o)
+end
 
 --
 -- debug ui
 --
 alias("DapUIToggle", function()
+  local Hydra = require("hydra")
+  local dap = require("dap")
   require("dapui").toggle()
-end)
 
---
--- debug function
---
--- TODO: setup the Hydra mode
-alias("DapBreakpoint", function()
-  require("dap").toggle_breakpoint()
-end)
-alias("DapBp", function()
-  require("dap").toggle_breakpoint()
-end)
+  local hint = [[
+ _B_: Breakpoint 
+ _C_: Continue
+ _S_: Step over
+ _q_: exit
+]]
 
-alias("DapContinue", function()
-  require("dap").continue()
-end)
-alias("DapC", function()
-  require("dap").continue()
-end)
-
-alias("DapStepOver", function()
-  require("dap").step_over()
+  Hydra({
+    name = "Dap Debugger",
+    hint = hint,
+    config = {
+      color = "pink",
+      invoke_on_body = true,
+      hint = {
+        border = "rounded",
+        position = "middle-right",
+      },
+    },
+    mode = { "n" },
+    body = "<leader>dp",
+    heads = {
+      { "B", dap.toggle_breakpoint, { silent = true } },
+      { "C", dap.continue, { silent = true } },
+      { "S", dap.step_over, { silent = true } },
+      { "q", nil, { exit = true, nowait = true, desc = "exit" } },
+    },
+  })
 end)
