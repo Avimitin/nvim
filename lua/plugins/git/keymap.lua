0aeec459c268914b97b3a71e082b2cc75d3e2015
Avimitin <avimitin@gmail.com>
2022-12-06 12:48:57 +0800

    git: move git plugins into overlays
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/plugins/git/keymap.lua
+++
@@ -1,8 +0,0 @@
-local nmap = require("editor.utils").nmap
-
--- fugitive
--- keep the same prefix as the git sign
--- See git-sign keymap in lua/plugins/config/gitsign_cfg.lua
-nmap("gic", ":Git commit -sS<CR>")
-nmap("giP", ":Dispatch! git push ", { silent = false })
-nmap("<leader>g", [[<CMD>vertical Git<CR>]])
