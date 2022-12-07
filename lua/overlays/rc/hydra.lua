5490c19cb7da21b053a4cc7df59c6697545e1e6c
Avimitin <avimitin@gmail.com>
2022-12-07 17:50:20 +0800

    windows: enhance windows management
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 15d4b3a0372161502cbb0a74a40e7ed1f022e68b [formerly 15d4b3a0372161502cbb0a74a40e7ed1f022e68b [formerly e0cac17c0565bce137463631c0989be3c03bccdf]]
    Former-commit-id: 51375891be3b8b3ddfffc4e38d1eebfb18d53068
    Former-commit-id: 43745bcafbf36610da53ab885dfc9f018eb98347

--- lua/overlays/rc/hydra.lua
+++ lua/overlays/rc/hydra.lua
@@ -3,41 +3,65 @@ if not ok then
   return
 end
 local cmd = require("hydra.keymap-util").cmd
+local pcmd = require("hydra.keymap-util").pcmd
+
 local window_hint = [[
-                   WINDOWS OPERATION
-
-    _h_:  Left | _j_:  Down  _k_:  Up | _l_:  Right
-
-  _<Up>_:     Increse Height  _<Down>_:  Decrese Height
-  _<Right>_:  Increase Width  _<Left>_:  Decrese Width
-
-    _v_:  Horizontal Split      _x_:  Vertical Split
+ ^^^^^^^^^^        Move      ^^           Size   ^^   ^^          Split
+ ^^^^^^^^^^   --------------  ^^     --------------^^   ^^  ---------------
+  ^^ ^    ^ ^    ^^^ ^   ^ ^
+   ^ ^ _k_ ^ ^      ^ ^ _K_ ^ ^   ^       _<Up>_   ^       _s_:  horizontally 
+  _h_ ^ ^ _l_    _H_ ^ ^ _L_   _<Left>_    _<Right>_   _v_:  vertically
+   ^ ^ _j_ ^ ^      ^ ^ _J_ ^ ^   ^      _<Down>_   ^      _c_:  close
+  ^^ ^    ^ ^    ^^^ ^  
+   focus^^^^^^      window
+ ^ ^ ^ ^ ^ ^  ^ ^ ^ ^ ^ ^   ^^ ^    ^^        _=_: equalize  _o_: remain only
+ _q_/_<Esc>_: Quit
 ]]
 
 Hydra({
-  name = "Windows Operation",
+  name = "Windows",
   hint = window_hint,
   config = {
-    color = "pink",
-    invoke_on_body = "true",
-    timeout = 800,
+    invoke_on_body = true,
     hint = {
       border = "rounded",
-      position = "bottom",
+      offset = -1,
     },
   },
   mode = "n",
-  body = "<C-W>",
+  body = "<C-w>",
   heads = {
-    { "h", "<C-W>h" },
-    { "j", "<C-W>j" },
-    { "k", "<C-W>k" },
-    { "l", "<C-W>l" },
-    { "v", "<C-W>v" },
-    { "x", "<C-W>x" },
+    { "h", "<C-w>h" },
+    { "j", "<C-w>j" },
+    { "k", pcmd("wincmd k", "E11", "close") },
+    { "l", "<C-w>l" },
+
+    { "H", cmd("WinShift left") },
+    { "J", cmd("WinShift down") },
+    { "K", cmd("WinShift up") },
+    { "L", cmd("WinShift right") },
+
     { "<Down>", cmd("res -5") },
     { "<Up>", cmd("res +5") },
     { "<Left>", cmd("vertical resize -5") },
     { "<Right>", cmd("vertical resize +5") },
+    { "=", "<C-w>=", { desc = "equalize" } },
+
+    { "s", pcmd("split", "E36") },
+    { "<C-s>", pcmd("split", "E36"), { desc = false } },
+    { "v", pcmd("vsplit", "E36") },
+    { "<C-v>", pcmd("vsplit", "E36"), { desc = false } },
+
+    { "w", "<C-w>w", { exit = true, desc = false } },
+    { "<C-w>", "<C-w>w", { exit = true, desc = false } },
+
+    { "o", "<C-w>o", { exit = true, desc = "remain only" } },
+    { "<C-o>", "<C-w>o", { exit = true, desc = false } },
+
+    { "c", pcmd("close", "E444") },
+    { "<C-c>", pcmd("close", "E444"), { desc = false } },
+
+    { "<Esc>", nil, { exit = true, desc = false } },
+    { "q", nil, { exit = true, desc = "close window" } },
   },
 })
