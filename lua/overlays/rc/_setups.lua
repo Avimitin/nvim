43840c2558842cd3f2bfd45cf65ad570ae86cd77
Avimitin <avimitin@gmail.com>
2022-12-07 19:02:09 +0800

    plugin: add package-info.nvim
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/overlays/rc/_setups.lua
+++ lua/overlays/rc/_setups.lua
@@ -205,6 +205,15 @@ local setups_sets = {
       end,
     })
   end,
+  ["js_deps"] = function()
+    vim.api.nvim_create_autocmd("BufRead", {
+      pattern = "package.json",
+      callback = function(props)
+        require("packer").loader("package-info.nvim")
+        require("overlays.rc.js_deps").setup_hydra(props.buf)
+      end,
+    })
+  end,
 }
 
 return setups_sets
