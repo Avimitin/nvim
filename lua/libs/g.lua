da7803bb406300ea39ba92a3da4212ae97926fbe
Avimitin <avimitin@gmail.com>
2022-12-13 12:02:39 +0800

    libs: add a custom cache validator
    Most of the plugin use `vim.g.<plugin>_loaded` to confirm if their are
    loaded or not. So it is not safe to use the vim global namespace to
    identify plugin load status. This commit add the custom cache validator
    to avoid conflict.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

---
+++ lua/libs/g.lua
@@ -0,0 +1,36 @@
+--
+-- Maintain a self global module load cache to avoid conflict with vim.g.*
+-- User can use the below mechanism to get cache status
+--
+--   * require("libs.g")["index"]
+--   * require("libs.g").index
+--   * require("libs.g")("index")
+--   * require("libs.g").loaded("index")
+--
+
+local M = {
+  __cache = {},
+}
+
+-- function loaded will insert the given module name into cache when it is not exist.
+---@param mod string A unique module name
+---@return boolean true if the given mod is loaded, false if the given mod is not exist.
+function M.loaded(mod)
+  if M.__cache[mod] then
+    return true
+  end
+
+  M.__cache[mod] = 1
+  return false
+end
+
+setmetatable(M, {
+  __call = function(self, a)
+    return self.loaded(a)
+  end,
+  __index = function(self, a)
+    return self.loaded(a)
+  end,
+})
+
+return M
