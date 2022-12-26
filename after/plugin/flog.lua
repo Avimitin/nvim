e2c6e756792af95ee43beb7ad33bbc02d2f2f00f
Avimitin <avimitin@gmail.com>
2022-12-26 11:21:37 +0800

    flog: fix command line mods
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- after/plugin/flog.lua
+++ after/plugin/flog.lua
@@ -13,11 +13,20 @@ local opts = {
 vim.api.nvim_create_user_command("Flog", function(args)
   require("packer").loader("vim-fugitive")
   require("packer").loader("vim-flog")
-  vim.cmd(("Flog %s"):format(args.args))
+
+  if args.mods and args.mods ~= "" then
+    vim.cmd(("%s Flog %s"):format(args.mods, args.args))
+  else
+    vim.cmd(("Flog %s"):format(args.args))
+  end
 end, opts)
 
 vim.api.nvim_create_user_command("Flogsplit", function(args)
   require("packer").loader("vim-fugitive")
   require("packer").loader("vim-flog")
-  vim.cmd(("Flogsplit %s"):format(args.args))
+  if args.mods and args.mods ~= "" then
+    vim.cmd(("%s Flogsplit %s"):format(args.mods, args.args))
+  else
+    vim.cmd(("Flogsplit %s"):format(args.args))
+  end
 end, opts)
