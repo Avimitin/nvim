b5fd7d092e16d12ffca34cea6f0695a7d7c6735b
Avimitin <avimitin@gmail.com>
2022-12-29 17:08:16 +0800

    nvim-tree: add auto command to open nvim-tree on directory argument
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- after/plugin/nvim_tree.lua
+++ after/plugin/nvim_tree.lua
@@ -3,3 +3,31 @@ if require("libs.g")["nvim_tree"] then
 end
 
 require("libs.keymaps").nmap("<leader>t", ":NvimTreeToggle<CR>")
+
+vim.api.nvim_create_autocmd("UIEnter", {
+  pattern = "*",
+  callback = function()
+    if vim.fn.argc() == 0 then
+      return
+    end
+    local first_arg = vim.fn.argv(0)
+    if not first_arg or #first_arg == 0 then
+      return
+    end
+
+    vim.loop.fs_stat(
+      first_arg,
+      vim.schedule_wrap(function(err, stat)
+        if err then
+          return
+        end
+
+        if stat.type ~= "directory" then
+          return
+        end
+
+        require("nvim-tree").open(first_arg)
+      end)
+    )
+  end,
+})
