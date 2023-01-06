a13f7fac4af7b1c89fcbc8a12a2e92a400a20d75
Avimitin <avimitin@gmail.com>
2023-01-06 14:25:30 +0800

    wildfire: add missing text object
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- after/plugin/wildfire.lua
+++ after/plugin/wildfire.lua
@@ -2,4 +2,4 @@ if require("libs.g").wildfire then
   return
 end
 
-vim.g.wildfire_objects = { "i'", 'i"', "i)", "i]", "i}", "ip", "it", "i`" }
+vim.g.wildfire_objects = { "i'", 'i"', "i)", "i]", "i}", "ip", "it", "i`", "i>" }
