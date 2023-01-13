073f26f76b911c030b4844984978ca7a2c9a4283
Avimitin <avimitin@gmail.com>
2023-01-13 11:22:19 +0800

    scroll: add scrolling for normal key mappings
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- after/plugin/cinnamon.lua
+++ after/plugin/cinnamon.lua
@@ -13,3 +13,16 @@ map({ "n", "x" }, "J", scroll("5j", 1, 1))
 map({ "n", "x" }, "K", scroll("5k", 1, 1))
 map({ "n", "x" }, "gg", scroll("gg", 1, 1))
 map({ "n", "x" }, "G", scroll("G", 1, 1))
+
+vim.keymap.set("n", "<C-o>", [[<Cmd>lua require("cinnamon.scroll").scroll('<C-o>', 1)<CR>]])
+vim.keymap.set("n", "<C-i>", [[<Cmd>lua require("cinnamon.scroll").scroll('<C-i>', 1)<CR>]])
+vim.keymap.set(
+  { "n", "x" },
+  "<C-u>",
+  [[<Cmd>lua require("cinnamon.scroll").scroll('<C-u>', 1, 1)<CR>]]
+)
+vim.keymap.set(
+  { "n", "x" },
+  "<C-d>",
+  [[<Cmd>lua require("cinnamon.scroll").scroll('<C-d>', 1, 1)<CR>]]
+)
