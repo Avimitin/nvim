c56da818504c6ac814f4ed4955feff056dde50fe
Avimitin <avimitin@gmail.com>
2023-01-17 13:27:52 +0800

    lspsaga: update settings
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- lua/overlays/rc/lspsaga.lua
+++ lua/overlays/rc/lspsaga.lua
@@ -1,44 +1,29 @@
 local saga = require("lspsaga")
-local themes = require("lspsaga.lspkind")
-themes[12][2] = " "
 
 -- use custom config
-saga.init_lsp_saga({
+saga.setup({
   -- when cursor in saga window you config these to move
   move_in_saga = { prev = "k", next = "j" },
   diagnostic_header = { " ", " ", " ", " " },
-  scroll_in_preview = {
+  scroll_preview = {
     scroll_down = "<C-d>",
     scroll_up = "<C-u>",
   },
-  code_action_icon = "ﯦ ",
   -- same as nvim-lightbulb but async
-  code_action_lightbulb = {
+  lightbulb = {
     sign = false,
+    enable_in_insert = false,
     virtual_text = true,
   },
-  finder_icons = {
-    def = "  ",
-    ref = "  ",
-    link = "  ",
-  },
-  finder_action_keys = {
-    open = "<CR>",
-    vsplit = "s",
-    split = "i",
-    tabe = "t",
-    quit = "q",
-    scroll_down = "<C-f>",
-    scroll_up = "<C-b>", -- quit can be a table
-  },
-  -- show symbols in winbar must be neovim 0.8.0,
-  -- close it until neovim 0.8.0 become stable
   symbol_in_winbar = {
-    in_custom = false,
     enable = true,
     separator = "  ",
     show_file = true,
-    click_support = false,
+  },
+  ui = {
+    code_action = "ﯦ",
+    diagnostic = "",
+    preview = "",
   },
 })
 
