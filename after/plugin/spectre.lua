e444d50260f1028333bc27de25c0af781b120ade
Avimitin <avimitin@gmail.com>
2023-01-26 17:10:25 +0800

    stylua: format
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- after/plugin/spectre.lua
+++ after/plugin/spectre.lua
@@ -2,6 +2,6 @@ if require("libs.g").spectre then
   return
 end
 
-vim.api.nvim_create_user_command("Sed", function ()
+vim.api.nvim_create_user_command("Sed", function()
   require("spectre").open()
 end, {})
