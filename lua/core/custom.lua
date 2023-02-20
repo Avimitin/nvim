df5419450b380437c086420788085b0d178fd005
Avimitin <avimitin@gmail.com>
2023-02-20 16:36:36 +0800

    custom: allow project/root custom injection
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 5d2ab764e41142c4c21c1f8d45b5ec4fa866864f [formerly 5d2ab764e41142c4c21c1f8d45b5ec4fa866864f [formerly fd9732ba68f48426beff0b7feb9a4a43834e28eb]]
    Former-commit-id: 345c256e0f22da1a704a88459a4e022874940212
    Former-commit-id: 77a78d457dbad6157b42d96d29c5747b3c187cf8

--- lua/core/custom.lua
+++ lua/core/custom.lua
@@ -18,14 +18,32 @@ local default = {
       find_project_root = true,
     },
   },
-  -- Append key mappings
-  keymap = {
-    n = {},
-    i = {},
-    x = {},
-    t = {},
-  },
-  -- Control plugin spec
+  -- *Append* key mappings
+  --
+  -- Spec:
+  --    [1]: left key mapping
+  --    [2]: right key mapping
+  --    mode?: optional, default "n"
+  --    option: see :h nvim_set_keymap
+  --
+  -- Example:
+  --
+  -- ```lua
+  -- keymaps = {
+  --    { "jk", "<ESC>", mode = "i", desc = "Exit insert mode" },
+  -- }
+  -- ```
+  keymaps = {},
+  -- Override plugin spec, support extra key `disable` to remove plugin
+  --
+  -- Example:
+  --
+  -- ```lua
+  -- plugins = {
+  --    { "tpope/dot-repeat" },
+  --    { "neovim/nvim-lspconfig", disable = true }, -- then this plugin will never be installed
+  -- }
+  -- ```
   plugins = {},
   -- Nerd font icon list
   icons = {
@@ -77,8 +95,8 @@ local default = {
   markdown = {
     previewer = "firefox",
   },
-  lsp = {},
   treesitter = {
+    -- require gcc
     ensure_installed = {
       "lua",
       "rust",
@@ -113,4 +131,55 @@ local default = {
   },
 }
 
-vim.cfg = default
+--- Path to custom file in neovim root dir, return nil if it is not exist
+---@return string|nil path
+local function get_root_custom()
+  local filepath = vim.fn.stdpath("config") .. "/.neovim.lua"
+  if vim.loop.fs_stat(filepath) then
+    return filepath
+  end
+end
+
+--- Path to custom file in project root dir, return nil if it is not exist
+---@return string|nil
+local function get_project_custom()
+  local root_dir_pattern = {
+    "Cargo.toml",
+    ".git",
+    "package.json",
+    "yarn.lock",
+    ".hg",
+    ".svn",
+  }
+  local root_dir = require("libs.find_root").get_root(root_dir_pattern)
+  if not root_dir then
+    return nil
+  end
+  local filepath = root_dir .. "/.neovim.lua"
+  if vim.loop.fs_stat(filepath) then
+    return filepath
+  end
+end
+
+local function new_cfg()
+  local final_config = default
+  local extend = function(ext)
+    final_config = vim.tbl_deep_extend("force", final_config, ext)
+  end
+
+  local root_file = get_root_custom()
+  local ok, mod = pcall(dofile, root_file)
+  if ok then
+    extend(mod)
+  end
+
+  local project_file = get_project_custom()
+  ok, mod = pcall(dofile, project_file)
+  if ok then
+    extend(mod)
+  end
+
+  return final_config
+end
+
+vim.cfg = new_cfg()
