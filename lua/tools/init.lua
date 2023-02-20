647d2240bb9cb1ea9db5a0090d864a717dd8ec3f
Avimitin <avimitin@gmail.com>
2023-02-20 11:44:41 +0800

    tools: add nvim-tree
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 900f6c3911000c81cd2a71ab770d12d7512db7af [formerly 900f6c3911000c81cd2a71ab770d12d7512db7af [formerly bc3696d8622373808c6e4b4f4cf3e01eda635f7e]]
    Former-commit-id: b790bb16b1839d8800a9032cf03946d7fe41446c
    Former-commit-id: f5f0f690871bc20a823ba65475f9cf5b9f363791

--- lua/tools/init.lua
+++ lua/tools/init.lua
@@ -13,3 +13,60 @@ register("hrsh7th/nvim-insx", {
 register("ii14/emmylua-nvim", {
   lazy = true,
 })
+
+-- Neovim Library wrapper
+register("nvim-lua/plenary.nvim", {
+  lazy = true,
+})
+
+-- Tree-like file browser
+register("kyazdani42/nvim-tree.lua", {
+  lazy = true,
+  keys = {
+    {
+      "<leader>t",
+      function()
+        if vim.bo.filetype == "NvimTree" then
+          require("nvim-tree").toggle()
+        else
+          require("nvim-tree").focus()
+        end
+      end,
+    },
+  },
+  --
+  init = function()
+    -- Auto load nvim-tree when neovim is started with directory
+    vim.api.nvim_create_autocmd("UIEnter", {
+      pattern = "*",
+      callback = function()
+        if vim.fn.argc() == 0 then
+          return
+        end
+        local first_arg = vim.fn.argv(0)
+        if not first_arg or #first_arg == 0 then
+          return
+        end
+
+        vim.loop.fs_stat(
+          first_arg,
+          vim.schedule_wrap(function(err, stat)
+            if err then
+              return
+            end
+
+            if stat.type ~= "directory" then
+              return
+            end
+
+            require("nvim-tree").open(first_arg)
+          end)
+        )
+      end,
+    })
+  end,
+  --
+  config = function()
+    require("tools.nvim_tree")
+  end,
+}) -- End of nvim-tree
