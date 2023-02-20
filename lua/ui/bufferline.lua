36436a17db3e9a7e6124957272a915b5111f2311
Avimitin <avimitin@gmail.com>
2023-02-20 11:30:51 +0800

    bufferline: add key mappings
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 362c04d1a754dba2d7a930766d0e70daacca1aea [formerly 362c04d1a754dba2d7a930766d0e70daacca1aea [formerly 485ffcb5fcd13567403b13d18239b2122c07d514]]
    Former-commit-id: a9e8c84e7506e001e21c45b051985fdce2581ac8
    Former-commit-id: 8a59cf18cfb52857b8666a14a46cebdbe914ca89

--- lua/ui/bufferline.lua
+++ lua/ui/bufferline.lua
@@ -18,3 +18,14 @@ require("bufferline").setup({
     always_show_bufferline = true,
   },
 })
+
+local cmd = require("libs.keymap").wrap_cmd
+require("libs.keymap").nmap({
+  { "<C-c>", cmd("BufferLinePickClose"), desc = "Close buffer" },
+  { "<Tab>", cmd("BufferLineCycleNext"), desc = "Goto next buffer" },
+  { "<S-Tab>", cmd("BufferLineCyclePrev"), desc = "Goto previous buffer" },
+  { "<Leader>p", cmd("BufferLinePick"), desc = "Pick a buffer" },
+  -- This might pollute user tmux/terminal settings
+  { "<M-S-right>", cmd("BufferLineMoveNext"), desc = "Move buffer to right" },
+  { "<M-S-left>", cmd("BufferLineMovePrev"), desc = "Move buffer to left" },
+})
