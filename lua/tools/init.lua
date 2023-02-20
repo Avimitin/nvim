5970a566aec505dfc9cb597b05d42e64b51c775c
Avimitin <avimitin@gmail.com>
2023-02-20 11:31:36 +0800

    lua: add null-ls sources
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 7a93e78cdebe57b1dbe557f8873d72545101d679 [formerly 7a93e78cdebe57b1dbe557f8873d72545101d679 [formerly e9cba19cd15cc2771fd3e43a43462721106af7c1]]
    Former-commit-id: 0b1487526ad09e3987b6b0cfa781273e25004f56
    Former-commit-id: 57f08f05474d6e0bd14094c490472a9ab4bead6f

--- lua/tools/init.lua
+++ lua/tools/init.lua
@@ -1,9 +1,15 @@
 local register = require("pack").register
 
+-- Auto-pairs key mappings
 register("hrsh7th/nvim-insx", {
   event = "InsertEnter",
   branch = "main",
   config = function()
     require("insx.preset.standard").setup()
   end,
+})
+
+-- Neovim API completion sources
+register("ii14/emmylua-nvim", {
+  lazy = true,
 })
