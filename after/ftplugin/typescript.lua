d34232dead64e12015f477a892d24c34012f7b47
Avimitin <avimitin@gmail.com>
2023-02-23 11:59:22 +0800

    typescript: allow user choosing deno as lsp client
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 2b43ffa98342105c1bd7299acefa9446ea40bdec
    Former-commit-id: dc60c055cf82efbb68b83976a15589090510345d
    Former-commit-id: 0c8ce8a6519c65d45546f9b45a2cecd70270af08

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -2,4 +2,14 @@ if require("libs.cache")["typescript_lsp"] then
   return
 end
 
-require("lsp.javascript").setup()
+if vim.cfg.typescript.server == "denols" then
+  -- fix highlight on codefences
+  vim.g.markdown_fenced_languages = {
+    "ts=typescript",
+  }
+
+  local config = require("lsp.deno")
+  require("lsp").start(vim.cfg.typescript.server, config)
+elseif vim.cfg.typescript.server == "tsserver" then
+  require("lsp.javascript").setup()
+end
