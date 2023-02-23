b3505742a082a1514fd8c2e73d867857fd5ce3d0
Avimitin <avimitin@gmail.com>
2023-02-23 13:20:34 +0800

    deno: enhance typescript support
    * Move configuration from after/ftplugin to lsp/ directory
    * Add new command `DenoCache` to cache uncached module
    * Add new command `DenoReloadImport` to reload cached response
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 149e839eabc4eeb0a63a6fed28fd3a33b4bb9786
    Former-commit-id: fce180d426dd4d3cb2f229d1e722a94d834bfb4f
    Former-commit-id: aafb2b3168a08c84bbbebf8b1fc75dac1024e296

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -3,13 +3,7 @@ if require("libs.cache")["typescript_lsp"] then
 end
 
 if vim.cfg.typescript.server == "denols" then
-  -- fix highlight on codefences
-  vim.g.markdown_fenced_languages = {
-    "ts=typescript",
-  }
-
-  local config = require("lsp.deno")
-  require("lsp").start(vim.cfg.typescript.server, config)
+  require("lsp.deno").setup()
 elseif vim.cfg.typescript.server == "tsserver" then
   require("lsp.javascript").setup()
 end
--- lua/lsp/deno.lua
+++ lua/lsp/deno.lua
@@ -1,40 +1,115 @@
-local Export = {}
-
-Export.settings = {
-  deno = {
-    enable = true,
-    suggest = {
-      imports = {
-        hosts = {
-          ["https://deno.land"] = true,
-          ["https://crux.land"] = true,
+local extra_config = {
+  settings = {
+    deno = {
+      enable = true,
+      suggest = {
+        imports = {
+          hosts = {
+            ["https://deno.land"] = true,
+            ["https://crux.land"] = true,
+          },
         },
       },
     },
   },
-}
+  handlers = {
+    ["deno/registryState"] = function(_, result, ctx)
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      local config = (client.config.settings.deno.suggest or {}).imports or {}
+      local hosts = config.hosts or {}
+      hosts[result.origin] = true
 
-Export.handlers = {
-  ["deno/registryState"] = function(_, result, ctx)
-    local client = vim.lsp.get_client_by_id(ctx.client_id)
-    local config = (client.config.settings.deno.suggest or {}).imports or {}
-    local hosts = config.hosts or {}
-    hosts[result.origin] = true
-
-    local new_config = {
-      deno = {
-        suggest = {
-          imports = {
-            hosts = hosts,
+      local new_config = {
+        deno = {
+          suggest = {
+            imports = {
+              hosts = hosts,
+            },
           },
         },
-      },
-    }
-    client.config.settings = vim.tbl_deep_extend("force", client.config.settings, new_config)
-    client.notify("workspace/didChangeConfiguration", {
-      settings = new_config,
-    })
-  end,
+      }
+      client.config.settings = vim.tbl_deep_extend("force", client.config.settings, new_config)
+      client.notify("workspace/didChangeConfiguration", {
+        settings = new_config,
+      })
+    end,
+  },
 }
+
+local Export = {}
+
+function Export.setup()
+  -- fix highlight on codefences
+  vim.g.markdown_fenced_languages = {
+    "ts=typescript",
+  }
+
+  local apply_keymaps = require("lsp.keymaps")
+
+  local on_attach = function(client, bufnr)
+    apply_keymaps(client, bufnr)
+
+    ---@return table|nil
+    local function get_deno_client()
+      local clients = vim.lsp.get_active_clients({
+        bufnr = bufnr,
+        name = "denols",
+      })
+
+      local _, deno = next(clients)
+      if deno == nil then
+        vim.notify("No Deno client found")
+        return nil
+      end
+
+      return deno
+    end
+
+    local cmd = function(...)
+      vim.api.nvim_buf_create_user_command(bufnr, ...)
+    end
+
+    cmd("DenoReloadImports", function()
+      local deno = get_deno_client()
+      if deno == nil then
+        return
+      end
+
+      deno.request("deno/reloadImportRegistries", nil, function()
+        vim.notify("Registries reloaded")
+      end)
+    end, {
+      desc = "Reload cached response from import registries",
+    })
+
+    cmd("DenoCache", function()
+      local deno = get_deno_client()
+      if deno == nil then
+        return
+      end
+
+      local api = vim.api
+      local current_window = api.nvim_get_current_win()
+      local current_buffer = api.nvim_win_get_buf(current_window)
+      local current_file = api.nvim_buf_get_name(current_buffer)
+
+      deno.request(
+        "deno/cache",
+        { referrer = { uri = "file:" .. current_file }, uris = {} },
+        function()
+          vim.notify("Dependencies cached")
+        end
+      )
+
+      vim.notify("Cache request sent, downloading dependencies...")
+    end, {
+      desc = "Cache uncached module",
+    })
+  end
+
+  extra_config.on_attach = on_attach
+
+  require("lsp").start("denols", extra_config)
+end
 
 return Export
