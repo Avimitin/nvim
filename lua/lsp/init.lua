443b2e6885acb5bd41093528799895c56a4bc762
Avimitin <avimitin@gmail.com>
2023-02-27 17:38:01 +0800

    lsp: move out signs icon setup
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 8153f0ac949ed9e6394d6d9d8658ef36a54c4a51
    Former-commit-id: 5b9b60c29068922a73560697eddf3788de3a6dd8
    Former-commit-id: 3e8c1d0251438d4ee89cfa61c24c0a89cd85c9d2

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,41 +4,7 @@ local register = require("pack").register
 register("neovim/nvim-lspconfig", {
   lazy = true,
   config = function()
-    -- Setup diagnostic icons and signs
-    vim.diagnostic.config({
-      virtual_text = {
-        prefix = "",
-        spacing = 6,
-        format = function(diagnostic)
-          return string.format(
-            "%s  %s",
-            vim.cfg.icons[vim.diagnostic.severity[diagnostic.severity]],
-            diagnostic.message
-          )
-        end,
-      },
-      signs = true,
-      underline = true,
-      -- update diagnostic in insert mode will be annoying when the output is too verbose
-      update_in_insert = false,
-    })
-
-    local types = {
-      "Error",
-      "Warn",
-      "Hint",
-      "Info",
-    }
-
-    for _, diag_type in ipairs(types) do
-      local hl = "DiagnosticSign" .. diag_type
-      vim.fn.sign_define(hl, {
-        text = "",
-        texthl = hl,
-        numhl = hl,
-        linehl = hl,
-      })
-    end
+    require("lsp.icons").setup()
   end,
 })
 
