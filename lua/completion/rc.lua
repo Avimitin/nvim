11d2aa52cfc5f4483db0b409bd3d3e7d361cd86e
Avimitin <dev@avimit.in>
2023-03-08 13:12:16 +0800

    completion: add custom sortin algorithm
    Signed-off-by: Avimitin <dev@avimit.in>
    
    Former-commit-id: 68c023afd5aa8edfd2bbbe352de3634395bd25b0
    Former-commit-id: c37c9a7268620d031b8c04955bbaca3ac18a273a
    Former-commit-id: 909bc356e6f44cafdb5dd085cab2703ae42d295d

--- lua/completion/rc.lua
+++ lua/completion/rc.lua
@@ -87,6 +87,64 @@ cmp.setup({
     ghost_text = true,
   },
   preselect = cmp.PreselectMode.None,
+  sorting = {
+    comparators = {
+      -- Return true: entry2 -> entry1
+      -- Return false: entry1 -> entry2
+      -- Return nil: entry1 <-> entry2
+      function(entry1, entry2)
+        local lsp_types = require("cmp.types").lsp
+
+        if entry1.source.name ~= "nvim_lsp" then
+          if entry2.source.name == "nvim_lsp" then
+            return false
+          else
+            return nil
+          end
+        end
+
+        local priority = {
+          Field = 11,
+          Property = 11,
+          Constant = 10,
+          Enum = 10,
+          EnumMember = 10,
+          Event = 10,
+          Function = 10,
+          Method = 10,
+          Operator = 10,
+          Reference = 10,
+          Struct = 10,
+          Variable = 9,
+          File = 8,
+          Folder = 8,
+          Class = 5,
+          Color = 5,
+          Module = 5,
+          Keyword = 2,
+          Constructor = 1,
+          Interface = 1,
+          Snippet = 0,
+          Text = 1,
+          TypeParameter = 1,
+          Unit = 1,
+          Value = 1,
+        }
+
+        local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+        local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+
+        local priority1 = priority[kind1] or 0
+        local priority2 = priority[kind2] or 0
+
+        if priority1 == priority2 then
+          return nil
+        end
+
+        return priority2 < priority1
+      end,
+    },
+  },
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
