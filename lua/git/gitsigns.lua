30a53eaceeda8d83c8623beacb8bfda36af32d7f
Avimitin <dev@avimit.in>
2023-03-10 15:37:28 +0800

    git: add missing key mapping document
    Signed-off-by: Avimitin <dev@avimit.in>
    
    Former-commit-id: 5d48bcb7aec0a4625130426823bf92bdda26885c
    Former-commit-id: ec7fd1ed38629cc7df270e2d39afa766eb06046c
    Former-commit-id: 7813252eb49cd5089c486e9094d1516f6ccd3911

--- lua/git/gitsigns.lua
+++ lua/git/gitsigns.lua
@@ -62,24 +62,24 @@ require("gitsigns").setup({
     end, { expr = true, desc = "Go to previous hunk" })
 
     -- Actions
-    map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>")
-    map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>")
-    map("n", "<leader>gS", gs.stage_buffer)
-    map("n", "<leader>gu", gs.undo_stage_hunk)
-    map("n", "<leader>gR", gs.reset_buffer)
-    map("n", "<leader>gp", gs.preview_hunk)
-    map("n", "<leader>gb", function()
+    map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
+    map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+    map("n", "<leader>gS", gs.stage_buffer, { desc = "Stage buffer" })
+    map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+    map("n", "<leader>gR", gs.reset_buffer, { desc = "Reset buffer" })
+    map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
+    map("n", "<leader>gB", function()
       gs.blame_line({ full = true })
-    end)
-    map("n", "<leader>gb", gs.toggle_current_line_blame)
-    map("n", "<leader>gd", gs.diffthis)
+    end, { desc = "Open git blame panel" })
+    map("n", "<leader>gb", gs.toggle_current_line_blame, { desc = "Enter line blame mode" })
+    map("n", "<leader>gd", gs.diffthis, { desc = "Open diff" })
     map("n", "<leader>gD", function()
       gs.diffthis("~")
-    end)
-    map("n", "<leader>gt", gs.toggle_deleted)
+    end, { desc = "Diff all" })
+    map("n", "<leader>gt", gs.toggle_deleted, { desc = "Toggle deleted line" })
 
     -- Text object
-    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select hunk" })
   end,
   numhl = true,
   linehl = false,
