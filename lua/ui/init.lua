5b03319a7c53ebd2720acf83152a21f02bc48f57
Avimitin <dev@avimit.in>
2023-03-10 15:30:45 +0800

    plugin: add dressing.nvim
    Signed-off-by: Avimitin <dev@avimit.in>
    
    Former-commit-id: 2e9e4ed4985116efd43d1a129b1b7679ffa0934d
    Former-commit-id: 633c772c113ede5889b1a34d5b0dc16ebb966b13
    Former-commit-id: 36b6bd69140d1a74dffe73c5364a96dab3e786fb

--- lua/ui/init.lua
+++ lua/ui/init.lua
@@ -158,3 +158,20 @@ register("folke/todo-comments.nvim", {
     })
   end,
 })
+
+-- prettify the input and select ui
+register("stevearc/dressing.nvim", {
+  lazy = true,
+  init = function()
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.select = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.select(...)
+    end
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.input = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.input(...)
+    end
+  end,
+})
