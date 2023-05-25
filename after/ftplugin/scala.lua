5662d8ec6edb8ce5878471d3b4d026cd68466749
Avimitin <dev@avimit.in>
2023-05-25 19:00:40 +0800

    scala: setup metal path manually when in nix env

--- after/ftplugin/scala.lua
+++ after/ftplugin/scala.lua
@@ -7,6 +7,11 @@ scala_config.settings = {
   showImplicitArguments = true,
   excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
+
+if vim.env["NIX_STORE"] then
+  scala_config.settings.metalsBinaryPath = vim.fn.exepath("metals")
+end
+
 scala_config.capabilities = require("lang.config").capabilities
 scala_config.on_attach = function(client, bufnr)
   -- require("metals").setup_dap()
