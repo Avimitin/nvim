af0ed73618453bdbfb37b316c8f899a35a4a50d2
Avimitin <dev@avimit.in>
2023-06-06 11:06:15 +0800

    plugins! remove nvim-orgmode/orgmode
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/note/init.lua
+++ lua/note/init.lua
@@ -19,33 +19,3 @@ pack("iamcco/markdown-preview.nvim", {
 pack("dhruvasagar/vim-table-mode", {
   cmd = "TableModeToggle",
 })
-
-pack("nvim-orgmode/orgmode", {
-  build = ":TSUpdate org",
-  ft = "org",
-  keys = {
-    "<leader>oa",
-    "<leader>oc",
-  },
-  config = function()
-    require("orgmode").setup_ts_grammar()
-    require("orgmode").setup({
-      org_agenda_files = { "~/Documents/schedule/*" },
-      org_default_notes_file = "~/Documents/schedule/todo.org",
-      org_todo_keywords = { "TODO(t)", "TRACKING(p)", "SOMEDAY(s)", "|", "DONE(d)" },
-      mappings = {
-        org = {
-          org_toggle_checkbox = "<leader><space>",
-        },
-      },
-    })
-
-    require("cmp").setup.filetype("org", {
-      sources = {
-        { name = "orgmode" },
-        { name = "path" },
-        { name = "buffer" },
-      },
-    })
-  end,
-})
