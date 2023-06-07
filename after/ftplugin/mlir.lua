a8719f4e5fe1383d813c9588b1cd3433eddd8844
Avimitin <dev@avimit.in>
2023-06-07 10:46:16 +0800

    mlir: use buddy-lsp-server for buddy-mlir
    Signed-off-by: Avimitin <dev@avimit.in>

--- after/ftplugin/mlir.lua
+++ after/ftplugin/mlir.lua
@@ -5,4 +5,9 @@ end
 vim.bo.comments = vim.bo.comments .. "://"
 vim.bo.commentstring = "// %s"
 
-require("lang").run_lsp("mlir_lsp_server", {})
+local is_buddy_mlir = (vim.fn.expand("%:p")):match("buddy.mlir") ~= nil
+if is_buddy_mlir then
+  require("lang").run_lsp("buddy_ls", {})
+else
+  require("lang").run_lsp("mlir_lsp_server", {})
+end
