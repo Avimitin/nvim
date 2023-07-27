e03fb34011cb61bc870405b9e903bbfa671c0890
Avimitin <dev@avimit.in>
2023-07-27 12:02:40 +0800

    mlir: use buddy-mlir in some specific project
    Signed-off-by: Avimitin <dev@avimit.in>

--- after/ftplugin/mlir.lua
+++ after/ftplugin/mlir.lua
@@ -5,8 +5,23 @@ end
 vim.bo.comments = vim.bo.comments .. "://"
 vim.bo.commentstring = "// %s"
 
-local is_buddy_mlir = (vim.fn.expand("%:p")):match("buddy.mlir") ~= nil
-if is_buddy_mlir then
+local function require_buddy_lsp()
+  local buddy_projects = {
+    "vector",
+    "buddy-mlir",
+  }
+
+  local current_pwd = vim.fn.expand("%:p")
+  for _, prj in ipairs(buddy_projects) do
+    if current_pwd:match(prj) ~= nil then
+      return true
+    end
+  end
+
+  return false
+end
+
+if require_buddy_lsp() then
   require("lang").run_lsp("buddy_ls", {})
 else
   require("lang").run_lsp("mlir_lsp_server", {})
