24884979af7b8190f37115d7f64c73358f503856
Avimitin <dev@avimit.in>
2023-08-09 14:58:14 +0800

    core: remove one-level import
    Signed-off-by: Avimitin <dev@avimit.in>

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ require("core")
 require("pack").setup()
 
 require("neovim").setup({
-  keymap = require("neovim.keymap"),
-  option = require("neovim.vim-opts"),
+  keymap = require("keymap"),
+  option = require("options"),
 })
