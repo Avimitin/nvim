49e2bb3b6c5a6ffc4a98ab4153c128073067c6c7
Avimitin <dev@avimit.in>
2023-08-23 18:35:37 +0800

    nix: add flakes
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Flakes for running this configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.neovim
+            pkgs.ripgrep
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
