818fd922db5ab4317dfb3ea6ceac724248dd48bc
Avimitin <dev@avimit.in>
2023-09-11 00:28:31 +0800

    nix: fix that attrValue met on function error
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -10,14 +10,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        toNvimPlug = name: drvs:
-          with builtins;
-          let
-            paths = map toString drvs;
-          in
-          pkgs.writeText "set-rtp-for-${name}.lua" ''
-            vim.opt.rtp:append("${concatStringsSep "," paths}")
-          '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -29,8 +21,14 @@
 
         formatter = pkgs.nixpkgs-fmt;
         packages.treesitter-parsers = let
-          allTreesitterParsers = pkgs.callPackages ./nix/treesitter-parsers.nix {};
+          lib = pkgs.lib;
+          parsersAttrSet = pkgs.callPackage ./nix/treesitter-parsers.nix {};
+          allParsers = lib.pipe parsersAttrSet [
+            (lib.filterAttrs (_: lib.isDerivation))
+            lib.attrValues
+          ];
+          toNvimPlug = pkgs.callPackage ./nix/set-rtp.nix {};
         in
-          toNvimPlug "treesitter-parsers" (builtins.attrValues allTreesitterParsers);
+          toNvimPlug "treesitter-parsers" allParsers;
       });
 }
---
+++ nix/set-rtp.nix
@@ -0,0 +1,10 @@
+{ writeText }:
+
+name: drvs:
+let
+  runtimePath = with builtins;
+    concatStringsSep "," (map toString drvs);
+in
+writeText "set-rtp-for-${name}.lua" ''
+  vim.opt.rtp:append("${runtimePath}")
+''
