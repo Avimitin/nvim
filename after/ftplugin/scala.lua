244ae75a4dac029665b1f9ba67966135d3859234
Avimitin <dev@avimit.in>
2023-11-10 23:49:32 +0800

    scala: clean variable usage
    Signed-off-by: Avimitin <dev@avimit.in>

--- after/ftplugin/scala.lua
+++ after/ftplugin/scala.lua
@@ -4,19 +4,20 @@ scala_config.settings = {
   excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
 
-local exepath = vim.fn.exepath("metals")
-if not exepath or exepath == "" then
+local metals_binary = vim.fn.exepath("metals")
+if not metals_binary or metals_binary == "" then
+  vim.notify("metals not found", vim.log.levels.ERROR)
   return
 end
-scala_config.settings.metalsBinaryPath = exepath
+scala_config.settings.metalsBinaryPath = metals_binary
 
-local exepath = vim.fn.exepath("mill")
-if not exepath or exepath == "" then
+local mill_exe = vim.fn.exepath("mill")
+if not mill_exe or mill_exe == "" then
   vim.notify("mill not found", vim.log.levels.ERROR)
   return
 end
 
-scala_config.settings.millScript = exepath
+scala_config.settings.millScript = mill_exe
 scala_config.capabilities = require("lang.config").capabilities
 scala_config.on_attach = function(client, bufnr)
   -- require("metals").setup_dap()
