2068764868f8e4518b46c6ecf1153529e5d83768
Avimitin <dev@avimit.in>
2023-11-14 14:52:52 +0800

    init: add nix profile source to load lua package from nix store
    Signed-off-by: Avimitin <dev@avimit.in>

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- Add support for loading lua package from nix store
+if vim.fn.expand("$NIX_PROFILES") ~= "$NIX_PROFILES" then
+  local append = function(path)
+    package.path = package.path .. ";" .. path
+  end
+  for _, p in ipairs(vim.split(vim.fn.expand("$NIX_PROFILES"), " ")) do
+    append(p .. "/share/lua/5.1/?/init.lua")
+    append(p .. "/share/lua/5.1/?.lua")
+  end
+end
+
 require("key-mapping")
 
 require("core")
