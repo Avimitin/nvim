7b2b72ea6963fa6ec918d55e36ecb3e020b12c5e
Avimitin <dev@avimit.in>
2023-12-03 01:57:52 +0800

    statusline: remove all useless status
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -42,91 +42,131 @@ local function insert_left(element)
   table.insert(gls.left, element)
 end
 
--- insert_right insert given item into galaxyline.right
-local function insert_right(element)
-  table.insert(gls.right, element)
-end
-
 -----------------------------------------------------
 ----------------- start insert ----------------------
 -----------------------------------------------------
 -- { mode panel start
 insert_left({
-  ViMode = {
+  LeftSpace = {
     provider = function()
-      return " "
+      return "  "
     end,
-    highlight = { colors.black, colors.black },
+    highlight = { colors.bg, colors.bg },
   },
 })
+
+local vim_mode = {
+  alias = {
+    n = " Normal",
+    no = "󰌌 Pending",
+    nov = "󰌌 Pending",
+    noV = "󰌌 Pending",
+    i = " Insert",
+    c = " Command",
+    v = " Visual",
+    V = " Visual",
+    [""] = "󰩬 Region",
+    C = "",
+    ["r?"] = "?",
+    rm = "M",
+    R = " Replace",
+    Rv = " Virtual Replace",
+    s = "",
+    S = "",
+    ["r"] = "HIT-ENTER",
+    [""] = "",
+    t = " Terminal",
+    ["!"] = " Shell",
+    _LineLeap = "󱕘 LineLeap",
+  },
+  color = {
+    n = colors.yellow,
+    i = colors.green,
+    v = colors.blue,
+    [""] = colors.blue,
+    V = colors.blue,
+    c = colors.magenta,
+    no = colors.red,
+    s = colors.orange,
+    S = colors.orange,
+    [""] = colors.orange,
+    ic = colors.yellow,
+    R = colors.purple,
+    Rv = colors.purple,
+    cv = colors.red,
+    ce = colors.red,
+    r = colors.cyan,
+    rm = colors.cyan,
+    ["r?"] = colors.cyan,
+    ["!"] = colors.red,
+    t = colors.red,
+    _LineLeap = colors.green,
+  },
+  is_line_leap = false,
+}
 
 insert_left({
   ViModeIcon = {
     provider = function()
       -- auto change color according the vim mode
-      local alias = {
-        n = "",
-        i = "",
-        c = "",
-        V = "",
-        [""] = "",
-        v = "",
-        C = "",
-        ["r?"] = "?",
-        rm = "M",
-        R = "",
-        Rv = "",
-        s = "",
-        S = "",
-        ["r"] = "HIT-ENTER",
-        [""] = "",
-        t = "T",
-        ["!"] = "",
-      }
-
       local mode = vim.fn.mode()
-      local vim_mode_color = {
-        n = colors.yellow,
-        i = colors.green,
-        v = colors.blue,
-        [""] = colors.blue,
-        V = colors.blue,
-        c = colors.magenta,
-        no = colors.red,
-        s = colors.orange,
-        S = colors.orange,
-        [""] = colors.orange,
-        ic = colors.yellow,
-        R = colors.purple,
-        Rv = colors.purple,
-        cv = colors.red,
-        ce = colors.red,
-        r = colors.cyan,
-        rm = colors.cyan,
-        ["r?"] = colors.cyan,
-        ["!"] = colors.red,
-        t = colors.red,
-      }
-
-      vim.api.nvim_set_hl(0, "GalaxyViMode", { fg = vim_mode_color[mode], bg = colors.bg })
-      return alias[mode]
+      if vim_mode.is_line_leap then
+        mode = "_LineLeap"
+      end
+      vim.api.nvim_set_hl(0, "GalaxyViMode", { fg = vim_mode.color[mode], bg = colors.bg })
+      return vim_mode.alias[mode]
     end,
     highlight = "GalaxyViMode",
   },
 })
 
+local gid = vim.api.nvim_create_augroup("LineLeapStatus", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LeapEnter",
+  group = gid,
+  callback = function()
+    vim_mode.is_line_leap = true
+  end,
+})
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LeapLeave",
+  group = gid,
+  callback = function()
+    vim_mode.is_line_leap = false
+  end,
+})
+
 insert_left({
-  EndingSepara = {
+  LeftBigSpace = {
     provider = function()
-      return " "
+      return "  | "
     end,
-    highlight = function()
-      if should_activate_lsp() then
-        return { colors.bg, colors.black }
-      else
-        return { colors.bg, colors.divider }
-      end
+    highlight = { colors.divider, colors.bg },
+  },
+})
+
+insert_left({
+  LineInfoIcon = {
+    provider = function()
+      return "  "
     end,
+    highlight = { colors.fg, colors.bg },
+  },
+})
+
+insert_left({
+  LineInfo = {
+    provider = "LineColumn",
+    highlight = { colors.fg, colors.bg },
+  },
+})
+
+insert_left({
+  LeftBigSpaceB = {
+    provider = function()
+      return "| "
+    end,
+    highlight = { colors.divider, colors.bg },
   },
 })
 
@@ -138,7 +178,7 @@ insert_left({
     end,
     highlight = {
       require("galaxyline.provider_fileinfo").get_file_icon_color,
-      colors.black,
+      colors.bg,
     },
   },
 })
@@ -147,17 +187,17 @@ insert_left({
   GetLspClient = {
     provider = "GetLspClient",
     condition = should_activate_lsp,
-    highlight = { colors.fg, colors.black },
+    highlight = { colors.fg, colors.bg },
   },
 })
 
 insert_left({
   LspSpace = {
     provider = function()
-      return " "
+      return "  "
     end,
     condition = should_activate_lsp,
-    highlight = { colors.black, colors.black },
+    highlight = { colors.bg, colors.bg },
   },
 })
 
@@ -166,7 +206,7 @@ insert_left({
     provider = "DiagnosticError",
     condition = should_activate_lsp,
     icon = "  ",
-    highlight = { colors.red, colors.black },
+    highlight = { colors.red, colors.bg },
   },
 })
 
@@ -175,7 +215,7 @@ insert_left({
     provider = "DiagnosticWarn",
     condition = should_activate_lsp,
     icon = "  ",
-    highlight = { colors.yellow, colors.black },
+    highlight = { colors.yellow, colors.bg },
   },
 })
 
@@ -183,7 +223,7 @@ insert_left({
   DiagnosticInfo = {
     provider = "DiagnosticInfo",
     condition = should_activate_lsp,
-    highlight = { colors.green, colors.black },
+    highlight = { colors.green, colors.bg },
     icon = "  ",
   },
 })
@@ -192,147 +232,13 @@ insert_left({
   DiagnosticHint = {
     provider = "DiagnosticHint",
     condition = should_activate_lsp,
-    highlight = { colors.white, colors.black },
+    highlight = { colors.white, colors.bg },
     icon = "  ",
   },
 })
 
 insert_left({
-  LeftEnd = {
-    provider = function()
-      return ""
-    end,
-    condition = should_activate_lsp,
-    highlight = { colors.black, colors.divider },
-  },
-})
-
-insert_left({
   MiddleSpace = {
-    provider = function()
-      return " "
-    end,
-    highlight = { colors.black, colors.divider },
-  },
-})
-
-insert_right({
-  RightStart = {
-    provider = function()
-      return " "
-    end,
-    highlight = { colors.black, colors.divider },
-  },
-})
-
-insert_right({
-  GitIcon = {
-    provider = function()
-      return "  "
-    end,
-    condition = require("galaxyline.provider_vcs").check_git_workspace,
-    highlight = { colors.fg, colors.black },
-  },
-})
-
-insert_right({
-  GitBranch = {
-    provider = "GitBranch",
-    condition = require("galaxyline.provider_vcs").check_git_workspace,
-    highlight = { colors.fg, colors.black },
-  },
-})
-
-insert_right({
-  RightSpace = {
-    provider = function()
-      return " "
-    end,
-    highlight = { colors.black, colors.black },
-  },
-})
-
-insert_right({
-  DiffAdd = {
-    provider = "DiffAdd",
-    condition = checkwidth,
-    icon = "  ",
-    highlight = { colors.green, colors.black },
-  },
-})
-
-insert_right({
-  DiffModified = {
-    provider = "DiffModified",
-    condition = checkwidth,
-    icon = "  ",
-    highlight = { colors.yellow, colors.black },
-  },
-})
-
-insert_right({
-  DiffRemove = {
-    provider = "DiffRemove",
-    condition = checkwidth,
-    icon = "  ",
-    highlight = { colors.red, colors.black },
-  },
-})
-
-insert_right({
-  RightEndingSepara = {
-    provider = function()
-      return ""
-    end,
-    highlight = { colors.bg, colors.black },
-  },
-})
-
-insert_right({
-  LineInfo = {
-    provider = "LineColumn",
-    separator = "  ",
-    separator_highlight = { colors.fg, colors.bg },
-    highlight = { colors.fg, colors.bg },
-  },
-})
-
-insert_right({
-  RightSpacing = {
-    provider = function()
-      return "   "
-    end,
-    highlight = { colors.bg, colors.bg },
-  },
-})
-
-insert_right({
-  PerCent = {
-    provider = "LinePercent",
-    condition = checkwidth,
-    highlight = { colors.fg, colors.bg },
-  },
-})
-
-insert_right({
-  RightSpacing = {
-    provider = function()
-      return "   "
-    end,
-    highlight = { colors.bg, colors.bg },
-  },
-})
-
-insert_right({
-  FileFormat = {
-    provider = "FileEncode",
-    condition = checkwidth,
-    highlight = { colors.fg, colors.bg },
-  },
-})
-
-insert_right({
-  RightEnding = {
     provider = function()
       return " "
     end,
@@ -378,27 +284,25 @@ gl.short_line_list = vim.tbl_keys(BufferTypeMap)
 require("galaxyline").section.short_line_left = {
   {
     ShortLineLeftBufferType = {
-      highlight = { colors.black, colors.cyan },
+      highlight = { colors.dimblue, colors.bg },
       provider = function()
         -- return filename for normal file
         local get_file_name = function()
-          return string.format("%s %s", "", vim.fn.pathshorten(vim.fn.expand("%")))
+          return string.format("%s %s", "", vim.fs.basename(vim.fn.expand("%")))
         end
         local name = BufferTypeMap[vim.bo.filetype] or get_file_name()
         return string.format("  %s", name)
       end,
-      separator = " ",
-      separator_highlight = { colors.cyan, colors.bg },
+      separator = " ",
+      separator_highlight = { colors.bg, colors.bg },
     },
   },
   {
-    ShortLineLeftWindowNumber = {
-      highlight = { colors.cyan, colors.bg },
+    WinSeparator = {
+      highlight = { colors.black, colors.black },
       provider = function()
-        return " " .. vim.api.nvim_win_get_number(vim.api.nvim_get_current_win())
+        return " "
       end,
-      separator = "",
-      separator_highlight = { colors.black, colors.bg },
     },
   },
 }
