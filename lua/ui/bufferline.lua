458fe9bd252932e191eef066c9f6d904ff98103a
Avimitin <dev@avimit.in>
2023-12-08 14:30:50 +0800

    bufferline: denose the bufferline
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/ui/bufferline.lua
+++ lua/ui/bufferline.lua
@@ -1,11 +1,14 @@
+local preset = require("bufferline").style_preset
+
 require("bufferline").setup({
   options = {
+    style_preset = {
+      preset.no_italic,
+      preset.no_bold,
+    },
     offsets = { { filetype = "neo-tree", text = " NeoTree" } },
     buffer_close_icon = "",
     modified_icon = "",
-    close_icon = "",
-    left_trunc_marker = "",
-    right_trunc_marker = "",
     max_name_length = 14,
     max_prefix_length = 13,
     tab_size = 20,
@@ -14,8 +17,9 @@ require("bufferline").setup({
     enforce_regular_tabs = false,
     view = "multiwindow",
     show_buffer_close_icons = true,
-    separator_style = "thin",
+    separator_style = { "", "" },
     always_show_bufferline = true,
+    indicator = { style = "none" },
   },
 })
 
