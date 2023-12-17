b83c43128447f0bc3ff263b3943a253aa1b3c015
Avimitin <dev@avimit.in>
2023-12-17 23:10:06 +0800

    nix: add updateScript to treesitter parser
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/nvim-treesitter-parsers.nix
+++ nix/nvim-treesitter-parsers.nix
@@ -1,4 +1,4 @@
-{ lib, runCommand, mkTreesitter }:
+{ lib, runCommand, writeShellApplication, jq, mkTreesitter }:
 
 parsersMetadata:
 
@@ -12,7 +12,46 @@ in
 runCommand "generate-treesitter-rtp"
 {
   inherit tsParsers;
-  passthru = { loaderScript = "/treesitter-parsers.lua"; };
+  passthru = {
+    luaPath = "/treesitter-parsers.lua";
+    updateScript = writeShellApplication {
+      name = "treesitter-hash-batch-updater";
+      runtimeInputs = [ jq ];
+      text = ''
+        [ -z "$*"  ] && echo "No nix file specify" && exit 1
+        nix_file="$1"
+
+        log() {
+          echo ">>> [updater] $*"
+        }
+
+        getVal() {
+          nix derivation show "$1" | jq -r "to_entries|.[0].value$2"
+        }
+
+        declare -a parsersArray
+        parsersArray=( ${toString tsParsers} )
+        for parser in "''${parsersArray[@]}"; do
+          log "Verifing parser $parser"
+
+          src=$(getVal "$parser" ".env.src")
+          oldHash=$(getVal "$src" ".env.outputHash")
+          srcUrl=$(getVal "$src" ".env.urls")
+          newFile=$(nix-prefetch-url "$srcUrl" --print-path --type sha256 | tail -n1)
+          newHash=$(nix hash file --base16 --type sha256 --sri "$newFile")
+
+          if [[ "$newHash" != "$oldHash" ]]; then
+            log "Hash changed for parser $parser"
+            sed -i "s/$oldHash/$newHash/" "$nix_file"
+          else
+            log "Parser $parser is up to date"
+          fi
+        done
+
+        log "All hash are updated in $nix_file"
+      '';
+    };
+  };
 } ''
   mkdir -p $out/parser
 
