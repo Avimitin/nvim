8115fc9f4ee021971562ff34f3308b7186fc8051
Avimitin <dev@avimit.in>
2024-01-16 02:35:51 +0800

    treesitter: load treesitter asynchoronously to avoid blocking render
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/treesitter/init.lua
+++ lua/treesitter/init.lua
@@ -3,7 +3,9 @@ local register = require("pack").register
 register("nvim-treesitter/nvim-treesitter", {
   event = "BufReadPost",
   config = function()
-    require("treesitter.config")
+    vim.schedule(function()
+      require("treesitter.config")
+    end)
   end,
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
