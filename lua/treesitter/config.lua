5ea453ceeb39671b5600e74ec0b68dbe6912114c
Avimitin <dev@avimit.in>
2024-01-16 13:58:34 +0800

    indent: remove useless line count
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/treesitter/config.lua
+++ lua/treesitter/config.lua
@@ -12,7 +12,6 @@ require("nvim-treesitter.configs").setup({
       scope_incremental = false,
       node_decremental = false,
     },
-    disable = disable,
   },
   highlight = {
     enable = true,
