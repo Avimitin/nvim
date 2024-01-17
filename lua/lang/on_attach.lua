a68b1d7b019f8ae61efbbac033e4cf9da9edd4ce
Avimitin <dev@avimit.in>
2024-01-17 21:07:54 +0800

    lsp: disable codelen refresher when LSP doesn't support
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/lang/on_attach.lua
+++ lua/lang/on_attach.lua
@@ -114,13 +114,15 @@ utils.setup_document_highlight_on_cursor = function(client, bufnr)
   end
 end
 
-utils.setup_codelens_refresher = function(bufnr)
-  vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.codelens.refresh()
-    end,
-  })
+utils.setup_codelens_refresher = function(client, bufnr)
+  if client and client.server_capabilities and client.server_capabilities.codeLensProvider then
+    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.codelens.refresh()
+      end,
+    })
+  end
 end
 
 -- LSP's on_attach interface accept two arguments client and bufnr. But we don't use client for now, so it is okay to pass nil here.
@@ -130,7 +132,7 @@ utils.setup_all = function(client, bufnr)
   utils.setup_inlay_hint(bufnr)
   utils.setup_auto_format(bufnr)
   utils.setup_document_highlight_on_cursor(client, bufnr)
-  utils.setup_codelens_refresher(bufnr)
+  utils.setup_codelens_refresher(client, bufnr)
 end
 
 return utils
