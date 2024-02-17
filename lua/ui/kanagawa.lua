82e09ea94818dff2b64b03f2f6efbb6e6f5c931d
Avimitin <dev@avimit.in>
2024-02-17 13:06:33 +0800

    kanagawa: remove workaround
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/ui/kanagawa.lua
+++ lua/ui/kanagawa.lua
@@ -62,28 +62,6 @@ local function override(colors)
       fg = default.springBlue,
       italic = true,
     },
-    ["@text.uri"] = {
-      link = "Comment",
-    },
-    ["@string.regexp"] = { link = "@string.regex" },
-    ["@variable.parameter"] = { link = "@parameter" },
-    ["@exception"] = { link = "@exception" },
-    ["@string.special.symbol"] = { link = "@symbol" },
-    ["@markup.strong"] = { link = "@text.strong" },
-    ["@markup.italic"] = { link = "@text.emphasis" },
-    ["@markup.heading"] = { link = "@text.title" },
-    ["@markup.raw"] = { link = "@text.literal" },
-    ["@markup.quote"] = { link = "@text.quote" },
-    ["@markup.math"] = { link = "@text.math" },
-    ["@markup.environment"] = { link = "@text.environment" },
-    ["@markup.environment.name"] = { link = "@text.environment.name" },
-    ["@markup.link.url"] = { link = "Special" },
-    ["@markup.link.label"] = { link = "Identifier" },
-    ["@comment.note"] = { link = "@text.note" },
-    ["@comment.warning"] = { link = "@text.warning" },
-    ["@comment.danger"] = { link = "@text.danger" },
-    ["@diff.plus"] = { link = "@text.diff.add" },
-    ["@diff.minus"] = { link = "@text.diff.delete" },
   }
 
   if enable_transparent then
