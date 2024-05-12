7c6655ed27782065531f3540c68f14a3a1de7614
Avimitin <dev@avimit.in>
2024-05-12 12:02:32 +0800

    nix: add nvfetcher to automatically bump nightly neovim
    Signed-off-by: Avimitin <dev@avimit.in>

--- overlay.nix
+++ overlay.nix
@@ -3,4 +3,13 @@ final: prev:
   mkTreesitter = final.callPackage ./nix/mkTreesitter.nix { };
   fetchNvimTsLockFile = final.callPackage ./nix/nvim-treesitter-lock-file.nix { };
   generate-nvim-treesitter-parsers = final.callPackage ./nix/nvim-treesitter-parsers.nix { };
+
+  neovim-nightly-bin =
+    let
+      srcInfo = final.callPackage ./nix/_sources/generated.nix { };
+    in
+    final.neovim-unwrapped.overrideAttrs {
+      inherit (srcInfo.neovim-nightly) version src;
+    };
+  neovim = final.wrapNeovim final.neovim-nightly-bin { };
 }
