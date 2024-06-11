c3446ea4331b439a2651727f9060a84844ed8b1c
Avimitin <dev@avimit.in>
2024-06-12 02:38:07 +0800

    ccls: allow ccls to run in subdirectory
    Signed-off-by: Avimitin <dev@avimit.in>

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -4,6 +4,12 @@ end
 
 local bufnr = vim.api.nvim_get_current_buf()
 require("lang").run_lsp(bufnr, "ccls", {
+  root_dir = require("lspconfig").util.root_pattern(
+    "build",
+    "compile_commands.json",
+    ".ccls",
+    ".git"
+  ),
   init_options = {
     compilationDatabaseDirectory = "build",
     index = {
