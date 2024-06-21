fe0f2a176234bbb2d808688020ae8c9cd15ee2b4
Avimitin <dev@avimit.in>
2024-06-21 13:25:00 +0800

    oil: use float as default
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/git/init.lua
+++ lua/git/init.lua
@@ -1,6 +1,7 @@
 local pack = require("pack").register
 
 pack("lewis6991/gitsigns.nvim", {
+  commit = "0dc886637f9686b7cfd245a4726f93abeab19d4a",
   config = function()
     require("git.gitsigns")
   end,
--- lua/tools/init.lua
+++ lua/tools/init.lua
@@ -27,7 +27,7 @@ register("MunifTanjim/nui.nvim", {
 register("stevearc/oil.nvim", {
   config = function()
     require("oil").setup()
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
   end,
 })
 
