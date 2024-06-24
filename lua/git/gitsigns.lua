378e5a0556e7acfad2d9ef0a198d923a8d5c36fc
Avimitin <dev@avimit.in>
2024-06-24 16:46:51 +0800

    gitsigns: bump
    Signed-off-by: Avimitin <dev@avimit.in>

--- lua/git/gitsigns.lua
+++ lua/git/gitsigns.lua
@@ -1,40 +1,22 @@
 require("gitsigns").setup({
   signs = {
     add = {
-      hl = "GitSignsAdd",
       text = "┃",
-      numhl = "GitSignsAddNr",
-      linehl = "GitSignsAddLn",
     },
     change = {
-      hl = "GitSignsChange",
       text = "┃",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
     },
     untracked = {
-      hl = "GitSignsUntracked",
       text = "┃",
-      numhl = "GitSignsUntracked",
-      linehl = "GitSignsUntracked",
     },
     delete = {
-      hl = "GitSignsDelete",
       text = "┃",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
     },
     topdelete = {
-      hl = "GitSignsDelete",
       text = "󱨉",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
     },
     changedelete = {
-      hl = "GitSignsDelete",
       text = "┃",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
     },
   },
   on_attach = function(bufnr)
