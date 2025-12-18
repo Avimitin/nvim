16f0cdd5cac66dccf526ada9a47903bedd007aef
Avimitin <dev@avimit.in>
2025-12-18 23:02:43 +0800

    lang: add ty
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ lsp/ty.lua
@@ -0,0 +1,12 @@
+return {
+  cmd = { "ty", "server" },
+  filetypes = { "python" },
+  root_markers = {
+    "ty.toml",
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    ".git",
+  },
+}
